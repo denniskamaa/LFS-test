9d7da2d89a5239cf453e692a556476f53314b107
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2015-12-01 10:10:19 +0100

    Reenable local build

--- rally/mechanic/builder.py
+++ rally/mechanic/builder.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec9b8dbb375c9195422186b7b6d8f0b0621f516b37cc5bb81a510a5cbe9fe4b3
-size 1802
+oid sha256:639f7c0eb3c5e2a9c646a0cb9b973785ad713f01bfe9c13d1143d093c2060010
+size 1800
