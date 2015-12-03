e4679ca60830bd5126610f0886bb24bcc3183a89
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2015-12-03 21:01:15 +0100

    Log an error when a  build task failed

--- rally/mechanic/builder.py
+++ rally/mechanic/builder.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80adb4628ae95a828a9fbaa52bb3730658e386969a033e96b8919a685729cee3
-size 2073
+oid sha256:85859364d082d1e0d746de174fdc484cab2a88bf68efd22252ea511873160433
+size 1789
