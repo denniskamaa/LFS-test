1bc1e71c260f6e3426cf8402abda987bd96c96f0
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2015-12-04 10:15:20 +0100

    Reenable pickling of previous benchmark results

--- rally/reporter.py
+++ rally/reporter.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a07099e7937fa8fce864464ef4454b83ece15e1eee7a18473170df48b9a4c267
-size 43849
+oid sha256:7feeceeb90811c6a67aa0f6ffe9b56f0dddbdf6dd06f09321948a933fb3f1927
+size 43806
