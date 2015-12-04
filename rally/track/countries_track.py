44a810703a84b6c5dadf953b244ce610e4e1988f
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2015-12-04 16:56:23 +0100

    Use full data set again

--- rally/track/countries_track.py
+++ rally/track/countries_track.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:316a41fde7f73b7c3b8198679c999dcb8a63aac5c43238e043ca32ba7f7043f9
+oid sha256:7e4bc29b44a9e9db9a817aa975d7198767227d270ac3c352262df70d596ddb36
 size 19542
