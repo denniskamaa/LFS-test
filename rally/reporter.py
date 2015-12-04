4e06b09f1b851d68bdd2c7788c25274fd824db72
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2015-12-04 08:01:43 +0100

    Amend KNOWN_CHANGES from dev repo

--- rally/reporter.py
+++ rally/reporter.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:813fcaac46e18d8b8a80a9d20980a63d695d6392f54fd0bceabdefec740c41b5
-size 43140
+oid sha256:37c96326ebd5ebb38388f6f580e7f481cebdfa1d279eaa56ad387bdcd7584ada
+size 43398
