f457950c51c0544a4f1c40504529eb03b487abab
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2015-12-04 08:01:00 +0100

    Issue a warning instead of an error when a build task fails

--- rally/mechanic/builder.py
+++ rally/mechanic/builder.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6dab6e4c0527e0761e660d327d80632134e157038d4111a816da30836c1cf1b9
-size 1790
+oid sha256:76c9468986fcf8242f36e0d415c2d79697ba5be9adcddbaca54918ed597a21dc
+size 1789
