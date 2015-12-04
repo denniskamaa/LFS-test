e317097a6e0e90d81c723204017b05bcc2116fcc
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2015-12-04 16:55:46 +0100

    Add geonames specific queries

--- rally/reporter.py
+++ rally/reporter.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6e624afb5b1ddb77536756e9bbacac7bbaa4d8bd59d617925e243db064dc18e
-size 43015
+oid sha256:f592e057a161104cb8d9fef3b742f09bf2ebfb6db4b07259d906a6a5816db60e
+size 43076
--- rally/track/countries_track.py
+++ rally/track/countries_track.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3664303646c73d2dee301ad11206eafde54bb72943a3dac358cadddedc794547
-size 19370
+oid sha256:316a41fde7f73b7c3b8198679c999dcb8a63aac5c43238e043ca32ba7f7043f9
+size 19542
