958971c6d9a6e6b2c7f2b0500d56ca1c0a4d68e1
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2015-12-07 10:30:06 +0100

    Correct exit code check in supplier

--- rally/mechanic/supplier.py
+++ rally/mechanic/supplier.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99bcd37c3fc933059e15a46effcdb269de9e840008d2b401e8816eb60700b439
-size 1677
+oid sha256:0748727904abe67c00e16e20ceffef425986f2960e3f0b08d809133e90da0946
+size 1669
