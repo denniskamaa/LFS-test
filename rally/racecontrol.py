75e86c5443b4e26316660e29497da3c9509be7b4
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2015-12-08 09:20:30 +0100

    Minor user feedback fixes

--- rally/racecontrol.py
+++ rally/racecontrol.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e75e5c4a289b755c8dd781c27680422100e24472ff8170797b24acb031c329d7
-size 2026
+oid sha256:8bbadd82f3a436b76b2febe0716d419c8a3f1387a9a2ffa12d69d2586dbd9b81
+size 2014
--- rally/rally.py
+++ rally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b0429c46452e910e7dc422e44b8feffc1bc322fdb92852a6a8f96fbfe41729f
-size 3137
+oid sha256:34ca4f3420b2a74917aaf2b52a3aa8fc34a98a3687a3b0625b38d85ad77fdd78
+size 3214
