9c2f01a1cf24778e00d7fbc631d9f89d0dca9c89
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2015-12-08 09:19:57 +0100

    Only run 'defaults' for now (hardcoded)

--- rally/reporter.py
+++ rally/reporter.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f592e057a161104cb8d9fef3b742f09bf2ebfb6db4b07259d906a6a5816db60e
-size 43076
+oid sha256:c7e3d3a5eba17d37f84e866df6623bf69a4668aabb3badbf916752c61c5bcf8f
+size 43095
--- rally/track/countries_track.py
+++ rally/track/countries_track.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9886ebc6f58271b1e63c055f926566917811e12bd42f440f03ea422198457105
-size 19668
+oid sha256:b11dc7cbf3b42dfa7ceaf29a2179f78d42ff4cf1c8a03132486e3a5a81ebcf86
+size 19922
