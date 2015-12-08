b4ea3b383e491c973c3fb508bbb35073fa777204
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2015-12-08 14:10:16 +0100

    Redirect server logs to Rally's log directory

--- rally/mechanic/launcher.py
+++ rally/mechanic/launcher.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe9a0e46a397bade9ebb88729045a76876a4125951b26c56afd113a1e4a78c34
-size 3965
+oid sha256:f5840597753747a2989ce8b0332e6b4dbdc8d3888fe23a2d701bfefec5f5a60a
+size 4089
