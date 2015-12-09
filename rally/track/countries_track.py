50622dab180fde1ee9389acdc5056c3bfb707e91
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2015-12-09 15:44:37 +0100

    Change normalization factor to number of scrolls instead of batch size

--- rally/track/countries_track.py
+++ rally/track/countries_track.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c20489233716d0f8824b6cea39c7d5ac3bb8acf19c1c895df7f2d4c2a12902b
-size 5270
+oid sha256:c64255d5420e2d4665a334d81b5f0c6070bdd96a73ee780b157f92158bed2339
+size 5268
