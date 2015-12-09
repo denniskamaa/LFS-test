ca19241548f93172a6bfe6a42e165d9ea44798f6
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2015-12-09 11:03:47 +0100

    Fix: Honor es.processors only if more than 1 are specified

--- rally/mechanic/launcher.py
+++ rally/mechanic/launcher.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f5840597753747a2989ce8b0332e6b4dbdc8d3888fe23a2d701bfefec5f5a60a
+oid sha256:0e49c992ef82a1e6534b63f47637c9dd16187e0b2147df52bee81a8eefd0a637
 size 4089
