1e135f41984e8a61f9184ee1d749b75106ad90b5
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2015-12-09 11:57:33 +0100

    Remove wait period in search benchmark

--- rally/driver.py
+++ rally/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:86277de3026e182e69a5ee399cef3a676794efcd1b5d8d9bb2c43bd6e02c6ff0
-size 15681
+oid sha256:930b469d62c658ae15d52e53722c19be03864d5b497717091a4b9ff480a7433d
+size 15657
