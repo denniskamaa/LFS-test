7182feb23c2222352760ebedf89a518e7c7df2db
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2015-12-09 15:13:28 +0100

    Rename scroll_all -> scroll

--- rally/summary_reporter.py
+++ rally/summary_reporter.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27e2562e18c89a0419bb3f7e3c6d29f00f6949a4a7e069b039f117f7e48f611c
-size 20254
+oid sha256:58136952e46f6864cdc282a16e9fc9e126504da218949e8fcb3bab0563cdd2c7
+size 20246
--- rally/track/countries_track.py
+++ rally/track/countries_track.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aec28f7c1efdd4d22e7600386c3598f49e71dbec272096800dbfdc4804848d7c
-size 5274
+oid sha256:2c20489233716d0f8824b6cea39c7d5ac3bb8acf19c1c895df7f2d4c2a12902b
+size 5270
