c3271bfc3a263c0b970b3e8d5036279c92712d1d
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2015-12-10 15:04:42 +0100

    Port minor scroll related changes

--- rally/reporter.py
+++ rally/reporter.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:905ddfc04811bd4be4c7ce200f0cc310145154505974acc9c697d2622826e879
-size 43039
+oid sha256:dfe5bdee08ab01b980c26075d7f5043acd03f16ac0085cc77cb7af37748a3f5a
+size 43370
