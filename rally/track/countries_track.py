6599a556a916c33beaedc6fe596a6835b6c2f5d5
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2015-12-11 08:30:00 +0100

    Correct description of country benchmark

--- rally/track/countries_track.py
+++ rally/track/countries_track.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c64255d5420e2d4665a334d81b5f0c6070bdd96a73ee780b157f92158bed2339
-size 5268
+oid sha256:6ad73e306240eec86419def12e4ae53b95aac2dba3a90f3740b4f82083357048
+size 5263
