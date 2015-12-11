99832b33a38ba7bc7ca2fc98d8b3aeed2df073f7
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2015-12-11 14:52:40 +0100

    Allow to specify a specific revision
    With this commit we allow to specify either
    a git revision number or a timestamp which is
    used to derive a revision.
    
    Closes #10.

--- rally/mechanic/supplier.py
+++ rally/mechanic/supplier.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28dfe206c757216f84cf1edba0fd79be69a3d1e9fbcd7785f6f1eb67324db331
-size 1747
+oid sha256:4e59825abe8dbf8d24380ac4967d126cb3c34dd64d4c38b2ea23c10187b603d5
+size 2537
--- rally/rally.py
+++ rally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75c713e457c9c25aab724b9ee5f70b8bbe062c43f3b23995ba0014f810b576e3
-size 3478
+oid sha256:9079cbe18d33e8a56f811a687fa4e8969fee6620c27835f2e2a13c31ef4f8427
+size 3636
