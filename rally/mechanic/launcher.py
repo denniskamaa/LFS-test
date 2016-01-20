fe3beaa50375b3234ec8babea0521ba3712e34ac
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-01-20 10:06:47 +0100

    Check whether the benchmark candidate has started successfully
    With this commit we add a safeguard check whether the benchmark
    candidate could be started successfully. If not, we abort the
    benchmark run and the user gets a helpful error message.
    
    Closes #24

--- rally/mechanic/launcher.py
+++ rally/mechanic/launcher.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba2cbc0e708c059cc5e126a11b016caad434b47fa3eca90197b2a3b65a4a9a89
-size 5095
+oid sha256:6ff819dbed78ebc690e68dbab329b0f842db07c44856a25c4a55aa54c9a5aded
+size 5545
