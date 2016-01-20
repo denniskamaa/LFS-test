9fa8d35feb20db1317ad4daca0c3ddd2d5f98c35
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-01-20 10:51:26 +0100

    Don't provision benchmark candidate with obsolete settings

--- rally/mechanic/provisioner.py
+++ rally/mechanic/provisioner.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63d4a35cb1b869b51c9876c847bf8ef321081328e6a94eaad2b89161ca0cfb4c
-size 3694
+oid sha256:4d8edaaa1951bee363ae8eb975818988bdd21fc662ddcc79500834b8ea9a8a5b
+size 3756
