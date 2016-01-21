98a573ff0a1e9ee02f92624913f8cc544c87d2c5
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-01-21 14:42:07 +0100

    Minor cleanups

--- rally/rally.py
+++ rally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:992efa844dfb74414b662ee3b40dc7dadfac806d518c55ffdb0d4c54943f479e
-size 8520
+oid sha256:4203e399629416c6477d21c9859c949ad65832b9481effa728807592966dc8ab
+size 8589
--- rally/utils/paths.py
+++ rally/utils/paths.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:948afbfd9fe507aaf55a8431c20a3d5d1e7ed1bfd882a4113d4c77a3a862b2ff
-size 1355
+oid sha256:3b39c70c08b69e6d5c673fd7d615acb6b457d8fb7395ed8a12e6d1e4b854b2f8
+size 746
