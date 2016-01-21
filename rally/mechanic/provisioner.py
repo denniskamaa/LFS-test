1d9ecec76c279adedbf61479255b2a0e6275eefe
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-01-21 14:59:30 +0100

    Account for multiple machines in cluster name
    With this commit we don't use the host name in the cluster name
    during provisioning. This is a first (tiny) step towards benchmarks
    using multiple machines.
    
    Closes #23

--- rally/mechanic/provisioner.py
+++ rally/mechanic/provisioner.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a4f33658d192cc0e2ae932cd0bea1e48dddb33dd1d99187f2e23a4a08ce03059
-size 4147
+oid sha256:df0590de8ba96fe9df0d225c19c43a2753607d2ca6a3ac52c0db2069957425f4
+size 3775
