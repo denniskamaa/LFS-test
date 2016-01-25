ce259d9aa525f376743ba05b57984d3a1feebe0a
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-01-25 10:28:32 +0100

    Compress log files
    With this commit we compress all log files generated by Rally to
    allow for more verbose logging configurations without filling up
    the disk.
    
    Closes #17

---
+++ rally/sweeper.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1a3902ce831f8225dc1d4510f72e5681b245e9ed23058ed9529176d67f02dc08
+size 396
--- rally/utils/io.py
+++ rally/utils/io.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4cb300a32210451f02ad5da2b7b3104ed420a1397efff5fb43c65cfcbdf0b56c
-size 3881
+oid sha256:efc6c14d7ecb4be47e04d79f55da123d47b0cd4c87a32fca1d82ac8933ba4d46
+size 4591
