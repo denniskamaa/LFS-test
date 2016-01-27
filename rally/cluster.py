d1d5ec3afb2e594ccff6e4a7a1de6a42b2a356ac
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-01-27 13:50:26 +0100

    apply longer request timeouts to increase stability

--- rally/cluster.py
+++ rally/cluster.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff1f2a6b27ff219bb99986b4a07fa8f51ae09dcbb08f5574053948029f9f40f3
-size 2862
+oid sha256:fe6a441eac9615279790e3c5ae18e122fe74d6afe7e1de6ff569d18a0306a79d
+size 2892
