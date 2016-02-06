6997017fc2af9f651f42b0a62c099717bf2aadd0
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-02-06 15:23:16 +0100

    Add JIT profiler
    With this commit we add a new JIT profiler which allows to write a
    log file of the JIT compiler activity (including assembler dumps in
    case the hsdis library is on the load path). JIT compiler logs are
    compatible with JITWatch.
    
    Closes #43.

--- rally/telemetry.py
+++ rally/telemetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff559620152570f3eba9881f7671975597642c653b6928e1e151d9d28ec20ca0
-size 8446
+oid sha256:7778ba79c6c5b9e873d0bd78dc13c5a8e4457ce3974bce0e557fa771ac228384
+size 9781
--- tests/telemetry_test.py
+++ tests/telemetry_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52d3315506c2be111ad2083dcbd7f5099a5eac1453b9cb896b20ae3ef87f1e99
-size 3313
+oid sha256:235f173d2377e0f102299c2fa4477cec2a59d8373a9e5c46d608d7305ad9486a
+size 4244
