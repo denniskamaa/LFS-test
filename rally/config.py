439a313a507c2223b15f18db46dc7bf7444a029c
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-02-12 15:42:02 +0100

    Log config migrations on INFO level

--- rally/config.py
+++ rally/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d86cfc28561baea484238c56b662b0d575836f40165ba5d6dc35edda6966133
-size 15114
+oid sha256:73138583cfe5796672bf9a29a3e6d65b81aa700ea5f618b889542fc00a3fefe9
+size 15181
