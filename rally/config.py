84e551d185cb0cc33bbeb1287d947a884a849b9b
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-02-15 10:06:10 +0100

    Simplify initial source directory configuration

--- rally/config.py
+++ rally/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73138583cfe5796672bf9a29a3e6d65b81aa700ea5f618b889542fc00a3fefe9
-size 15181
+oid sha256:fc7bc1b3bc12db6436e8f2d7b50ce77d804e5520da5a3d043a49d446be9d35de
+size 15310
