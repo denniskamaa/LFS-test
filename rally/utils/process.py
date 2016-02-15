3322de4a0a1c49d8624f9a65f64c0f7f2d063275
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-02-15 09:40:06 +0100

    Make kill of lingering benchmark cluster processes portable

--- rally/utils/process.py
+++ rally/utils/process.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e493ccdea9d16f5778949448a6429162bb7301cfa452a80dac0e19ff175c1ce2
-size 1925
+oid sha256:f37402cc7ac1de0aea4988922b8778970d9682aa57dedab23d1ca253042de668
+size 1926
