bd66a31c1f8e994059bf4a2eb41be63e302de110
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-02-15 14:44:21 +0100

    Set config defaults in Config constructor already

--- rally/config.py
+++ rally/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a51b048c22aa3c05c4296a9d8fa7f1b9cb2b38fa49bc275350742d262d93728
-size 15362
+oid sha256:831f109ca44badf2bd8f7b7f878ebc29957c53a12c6745a3787522de1ffa2d28
+size 15391
