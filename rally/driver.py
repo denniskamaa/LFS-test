001aec011c54ab3b1e7ceb35c0df4c3c3b94d3fe
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-03-02 12:46:21 +0100

    Increase number of warmup iterations for search benchmark
    With this commit we increase the number of warmup iterations for
    the search benchmark to avoid that the JIT compiler is still
    running when we start the search benchmark.
    
    Relates #9

--- rally/driver.py
+++ rally/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9005ba0268feaff5c9b6f987222cc9f506004caf4674af670542c1c5287c28a0
-size 13834
+oid sha256:dd289eface2318abff2e1c2a589ff67398393863d2e0358d216febc498ef94eb
+size 13826
