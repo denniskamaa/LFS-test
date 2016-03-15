14eb390f92ee399ef1a42e8a9cc8a9df2a84850e
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-03-15 14:05:58 +0100

    Fetch sources at defined UTC timestamp from origin/master
    With this commit we check for timestamp-based revisions the changes
    to origin/master instead of master and we also restrict timestamps
    to valid ISO 8601 timestamps (thus specifying also a timezone).

--- rally/mechanic/supplier.py
+++ rally/mechanic/supplier.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f3df213aa2ba116a375379733b43a04be4e7918fcfa7f7b6d31253d374b8bbe
-size 3365
+oid sha256:2b7de29efa105a08bead98f277af6b75b72960d4cbe4e5a735222bb8f91bd01a
+size 3413
--- rally/rally.py
+++ rally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8a95fc73c8dfbbd32cb4fd46d7cf7284b42434698fe55b78056f8335e0af58b
-size 9020
+oid sha256:0ca4af32cad3c26e0f3e5c511cfa151d987a61d9ddd1a09b6c46d19df5fa8863
+size 9022
