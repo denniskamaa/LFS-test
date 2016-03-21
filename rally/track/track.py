8dfd43c4625decbfd234f932463bc7e13ed16e7e
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-03-21 14:00:31 +0100

    Force download of benchmark mapping file
    With this commit we always download the mapping file. In the past
    it happened that we had to update the mapping file due to changes
    in Elasticsearch but Rally users had to manually remove the file
    in order to get a working benchmark environment again.
    
    By always downloading the mapping file we ensure that it is
    current. While this is not an ideal solution, users are at least
    not blocked when a mapping change has been made.

--- rally/track/track.py
+++ rally/track/track.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c9dd41f4abc75649578b4069c743cbb6baa2f8e98c1ecb22feca68562214c94
-size 15512
+oid sha256:1b470dc3a5d7ca57e0c1446ce5e0420ec33aa9029f544c7188b7c0755bf2a1f9
+size 15966
