c83d449f9d817e006a606c3af2b8c50debf33cb6
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-03-24 07:16:05 +0100

    Report 50, 95, 99, 99.9 percentile on command line

--- rally/metrics.py
+++ rally/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2a48484210e25e04adc883b3e2521b9d2a7de7ff2dcf7fa43fc07c14429c580
-size 7637
+oid sha256:f0d6fd7385490daf011006eaf73b44e93688aff657a8c0bd0baafd848badcddb
+size 7781
