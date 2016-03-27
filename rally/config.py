e6335021b2fdad77cdd2892fb787548b6fddaaac
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-03-27 11:06:11 +0200

    Remove old reporter

--- rally/config.py
+++ rally/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c8e91e9f7fdd480bb27a6c27927081e9b1606c70cf4b2fcf6c22c57b47e2be0
-size 16774
+oid sha256:f533bc3a68d98a68ebb4a46b1a062e4166e66b20c023866128b17b12fa366126
+size 17004
--- rally/reporter.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1d3ee2a983c54692e37aeaa333cd5c2871630f085304000ff86568c6b87b59b3
-size 51968
