f6b37ecd1640d7776fe6582a4e4a3595a9cc18cf
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-03-27 10:47:42 +0200

    Add integration test

--- rally/rally.py
+++ rally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b20461fe6f266a6bb52a953c36a2e848e0f4b32deb5234989fcea933dcbef60
-size 9460
+oid sha256:12adfb0dea1df821a968569735bb0b0ba9f77f4e8456de191c828196e76d55a8
+size 9783
---
+++ rally/track/tiny_track.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7fceff931ae47c28989e1f05d35ecb2f238c5a40ead2b4ab04485318510ef84d
+size 2828
