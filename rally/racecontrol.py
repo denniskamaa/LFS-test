2ca55286a703c4019aae866046f01fa1676104c3
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-04-02 17:39:10 +0200

    Show error message on unknown pipelines, tracks or track setups
    Previously it could happen that we silently go on although no
    track setup matches. This produces weird output and is puzzling.
    
    Now we show a clear error message and indicate how the user can
    check for the correct name in each case.
    
    Closes #75

--- rally/racecontrol.py
+++ rally/racecontrol.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3af2d95e14094317201a8b1d06be0407056dd407c8d3f21889ceef4b08850c2e
-size 14055
+oid sha256:cd0add81ab7f7e18ab650d26db21e3f1556d42cd7f107d81646dbd62fe779854
+size 14814
