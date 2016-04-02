08546f5d47c8e503efd02780a0379984033fcebd
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-04-02 16:51:30 +0200

    Retrieve mappings for all types in an index

--- rally/track/track.py
+++ rally/track/track.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff89dd9fea15ea114c527efa73fa589e6cc53d033aa1bd039992cbccc96c455a
-size 18956
+oid sha256:0a51489904b22b28bf82438c7669e1668e220f1644e4b401a6fd4e958b1a4d7a
+size 18976
