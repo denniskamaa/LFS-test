059857572cd5735471f5eae6f78d555e14b59e0a
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-04-14 08:22:38 +0200

    Get Rally version for docs from package config

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1aaf824afe72993e150c0912ad40fc6d0f01723c0f7ebd2d71c9407515cbe931
-size 9433
+oid sha256:a1ac6a1ae43edde166c3a491042969da9c365c0fd06ec73dd4158c4084e34ee9
+size 9490
