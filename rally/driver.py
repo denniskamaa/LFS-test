d523ecff3ccbd0910441ccf73d3113b21f0596a3
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-04-14 16:27:27 +0200

    Gather multiple samples for stats latencies

--- rally/driver.py
+++ rally/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18c01dcbe283f73ac7e6f0b1fa2ea5c4d35b98b9a860fa41293dae27df4322ff
-size 15991
+oid sha256:afe0b3f49f1e41930c732f1bd0b59a552c27a398c148b477343436743ea644b4
+size 16848
--- rally/summary_reporter.py
+++ rally/summary_reporter.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89a63d0a9312bb4c59c022d0cb2c9aa4eee01105d6c6d5b9f6b4d91fedb70bec
-size 7574
+oid sha256:8788c0d4c0710cbf8fe303910665bb3855803f35528209b3a0b5742bdaf9b936
+size 7685
