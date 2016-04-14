8e2dca1155863ce3582579b6af29cedf5fb4c9eb
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-04-14 14:20:04 +0200

    Fix log statement

--- rally/driver.py
+++ rally/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:654b36333ab26dbc86f0906b9de2d09038201f4b4592ec1a42f051742ba09a91
+oid sha256:a98c72ef8a7401ab76e903330da61d28eb4170da5615ea0c7d3beadeb2d3357a
 size 15444
