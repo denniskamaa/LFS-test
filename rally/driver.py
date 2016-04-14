95787d423240e22647cf778a05c81659191dd8e0
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-04-14 14:21:07 +0200

    Remove MB/s output

--- rally/driver.py
+++ rally/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a98c72ef8a7401ab76e903330da61d28eb4170da5615ea0c7d3beadeb2d3357a
-size 15444
+oid sha256:3e2e9ca0bfafabbfd3ca85039ae72b2b7728c436f8b3554fc3586ee28a1c985e
+size 15327
