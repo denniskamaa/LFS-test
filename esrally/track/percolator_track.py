ed0358a333d6281b037fe11ca3d340b555421f6f
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-04-15 12:19:01 +0200

    Fix query cache configuration in percolator track

--- esrally/track/percolator_track.py
+++ esrally/track/percolator_track.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6bcdf4c77a4f5c9bbae6860d6de66194104f3efb9b5e5b3ba9468918ccbd5381
+oid sha256:64bbf358a4b3b69f1320875f41ac62f546caf6ffc132ee8c29962ef7371ac620
 size 3752
