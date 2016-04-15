242dc523fa70079444d966fbb91323d218919e27
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-04-15 12:30:23 +0200

    Write dev config

--- esrally/config.py
+++ esrally/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce1e94d256a65dbdbb2e745845b8649345a88626eaddbeed2a609271a17ded27
-size 17006
+oid sha256:ef4278eb9b4a60d6b8e3d01a71159da135675e6805fccf8d7aa7635d316fd9b7
+size 17023
