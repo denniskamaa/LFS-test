b7c3a95ee075ef973aca18e1a0b431bc0395db0e
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-04-15 09:33:27 +0200

    Minor formatting fix in reporter

--- rally/summary_reporter.py
+++ rally/summary_reporter.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8788c0d4c0710cbf8fe303910665bb3855803f35528209b3a0b5742bdaf9b936
-size 7685
+oid sha256:13e0d5179fce17ff73dc14c7c4cd0e557c2896f2f0fd4fcbba50ed7c1a5fc8d4
+size 7687
