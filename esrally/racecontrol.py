5023f4857137b01333902b65b94e61aec5f8b12c
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-04-16 16:58:45 +0200

    rename summary_reporter.py to reporter.py

--- esrally/racecontrol.py
+++ esrally/racecontrol.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ea753f1bced9cb8fcc64a699b8c4d4579c7d5ab33e273399f5d546c6a7bc000
-size 14842
+oid sha256:4d20dfe5dbd15668c3d3dbc96bac5e0fd72663b68be73bd02ef0861f30bb8a44
+size 14826
