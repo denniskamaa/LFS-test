3ac60afdb6bbf600c71b984394574777262d5264
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-04-18 10:38:19 +0200

    Add support to store also warmup times
    With this commit we prepare the metrics store to store also warmup
    samples. As Rally is nearing a public release it is easier to add
    this now instead of adding changes later.
    
    Note that we still only store normal samples. Support for also
    storing warmup samples will come in #80.
    
    Relates #80

--- esrally/metrics.py
+++ esrally/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e674b395c7aeab6cb1197721eb10fa14c164ec9cdcc6312ab125f1c189c3efe3
-size 14801
+oid sha256:541c4461edd938c39a16497a3a92783c802d396b0030baa1fc1c8d70d7d0e600
+size 15418
--- tests/metrics_test.py
+++ tests/metrics_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9fb60deef5b27e70704b760b0365c75a8a3e72e31bbc70a4708fea239b75143f
-size 5628
+oid sha256:4bd6e2c4a197c446a053fe73aee6678141ffaeca3d263cd33d3d8998453189dd
+size 6021
