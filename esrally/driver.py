0cf7781a997311b8bbb3053815d9899ba0117884
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-04-18 10:35:47 +0200

    Simplify indexer log statement

--- esrally/driver.py
+++ esrally/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da6b392c00cb3f9b8f0e99fa57010f31b8dc90efc6b9a0732279c679b270ffb0
-size 20091
+oid sha256:d9cb74e8e174825fbb19080fd7d38dc3202e28247e95480c2c6fbe1594f58168
+size 20073
