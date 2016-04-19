e48b6e34c1739fb28f65f245ecc0ba44cc979238
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-04-19 17:56:50 +0200

    Continue 0.2.1.dev

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c2a70cf017e8d6c9aa45a00b9106b6a5f8a07fe78c0b4e7b0cb6cf7d84e771a
-size 1691
+oid sha256:702c40bc8ebe7c487fdaf557b21d90486f8520cfde3b197b3e9cdff5372c508a
+size 1698
