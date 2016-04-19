b69ec557221b632441bd6ba772e6b6409e9d53bf
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-04-19 12:50:40 +0200

    Add required parameters for PyPI

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d78fbfd64f2daae97d2c9b952ce04206034090541ff26c3d09dcd9ce1f2924b0
-size 1603
+oid sha256:8c2a70cf017e8d6c9aa45a00b9106b6a5f8a07fe78c0b4e7b0cb6cf7d84e771a
+size 1691
