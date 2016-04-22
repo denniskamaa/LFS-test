36945beea526f8f08326f4cecdd7a53db8fe285c
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-04-22 08:52:52 +0200

    Don't try to report indexing throughput for search only benchmark

--- esrally/reporter.py
+++ esrally/reporter.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54fb91c87063d3d834d22ec8760bc943690f10caead1b9a5af20223c577c5272
-size 7879
+oid sha256:91fd153a375a25c514df4a09bc3eccf6b902a3ded9394cb609a79526eb4e8fa8
+size 8012
