2a8ba45334df35cd0fcc372a8fbe54a24ccf4400
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-04-26 11:21:07 +0200

    Update type mapping for percolator track

--- esrally/track/percolator_track.py
+++ esrally/track/percolator_track.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:577606f2a7c831fe7a52e70d4fef00257de3d7abb8cb17332737c1e104365ad9
-size 4223
+oid sha256:f886c530423b37325ef00a98c6e6ace63a99e52585d2082d7c50aeab4c6a1995
+size 4319
