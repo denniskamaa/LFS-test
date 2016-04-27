dd9d982a1cb798de23a058f4e0b50192ad14c965
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-04-27 09:44:23 +0200

    Make proof of concept perf stat profiler work

--- esrally/telemetry.py
+++ esrally/telemetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e706cbde26c48d51d1f1ed4a744336aa8ccdd2c65efdebc06682d3e5cd1debca
-size 21648
+oid sha256:36609d35e87bf6605dc35027032b932bbd9f0f1dc17c92e3011d1975a1adb7cd
+size 21661
