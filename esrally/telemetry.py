30d1656dafa96eb7856eb34e66f44e00dab1d9cd
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-04-28 10:48:36 +0200

    Add perf telemetry device
    Closes #28

--- esrally/telemetry.py
+++ esrally/telemetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:36609d35e87bf6605dc35027032b932bbd9f0f1dc17c92e3011d1975a1adb7cd
-size 21661
+oid sha256:a2a62a5c2b3854371a6a5e2a210260e0f27ee8e150372a326cb8e53ef4540251
+size 21803
