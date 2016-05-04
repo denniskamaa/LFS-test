fc3ab31921a414796cbc5a52dfdc8e8fe69ccb36
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-05-04 10:59:47 +0200

    Reenable geopoint track
    Relates #82

---
+++ esrally/track/geopoint_track.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:666ddc71b2cd170799985f8f78cb36493b36335be0489eef890e5c0d69932176
+size 2840
--- esrally/track/geopoints_track.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ecb7d3274beeaf3245466b77f7c77f4472c2245b047b9014eded55979abda742
-size 2911
