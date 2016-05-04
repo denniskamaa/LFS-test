4ffd9c536218f48f17c5dead08833ce2947d4952
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-05-04 09:30:54 +0200

    Fix packaging issues with resources

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f303bdd07c941d36475f15aa5136a3f7a6b3a80a07db9608f2eaea7af4b6957c
-size 1721
+oid sha256:8801e9df1da75b30620a5b58969cf02669b6306170f7541a6bdfb24b91b0a799
+size 1791
