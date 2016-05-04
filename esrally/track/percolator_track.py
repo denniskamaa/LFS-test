d86f2fbc2f18ffbbd9d2d3dd131928608af76e5e
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-05-04 08:42:45 +0200

    Add index config in percolator track for ES 5.0.0-alpha2

--- esrally/track/percolator_track.py
+++ esrally/track/percolator_track.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f886c530423b37325ef00a98c6e6ace63a99e52585d2082d7c50aeab4c6a1995
-size 4319
+oid sha256:d6957cb7ad80ccbca43f65c70cd62744e3835c1204e328427ceba3eff9170c17
+size 4432
