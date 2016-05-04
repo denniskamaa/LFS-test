8342c30c4b8b7655d91546a1e33042f64bea5772
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-05-04 15:19:51 +0200

    Continue with 0.3.0.dev

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b140908ddbf0f18a875416d41f9427626a9df60a79d39f2a756d6fc7b56b3921
+oid sha256:decdc51ff9906b10dc5725fb9b4e379f539f1625f7cd197305cd736c3fff2689
 size 9456
--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15115445638873202894ef6bbd27278d8fd2380f6bfad324ab1dc8776a3ce96f
-size 1784
+oid sha256:cbb30ff2b2153488df08c05290bacd8e6e862b4b6e9268fc2e6cf51661e93de7
+size 1791
