676ecd8be15a22d6fedc67eb1a45f452c0025fea
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-05-19 18:51:33 +0200

    Rename RaceStore -> EsRaceStore

--- esrally/metrics.py
+++ esrally/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a9a13c52d15929a6822bfffdcb9a9b04966c3a17c3d9dc65db89ed546614369
-size 23283
+oid sha256:3d79b2d8fa0b0fe1a163b7eeaafeb2d9168368060a473ae29bf13268f6ec4d6b
+size 23291
