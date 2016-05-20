f2e3ab3699328d0bed23e9975eca35e03fd741a5
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-05-20 16:49:29 +0200

    Make in process launcher aware of multiple ES versions

--- esrally/mechanic/launcher.py
+++ esrally/mechanic/launcher.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3fabe0305e9b3a70fe5668a69ebfa1a1f55e17c473aed6e4894485b38dede92
-size 10135
+oid sha256:9f848f50245f46b9a5db26071a1421ca4aa0c2137a78d2e87e9d713feecf14cf
+size 11503
