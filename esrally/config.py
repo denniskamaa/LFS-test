c1c1082f77619c6b3dd999277da2943bad6c043d
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-05-20 17:20:58 +0200

    Refine wording

--- esrally/config.py
+++ esrally/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ce1bf0ef4f373a6e0f9b15f42b56f4b763dc495228b11f68f2b9cfafb303064
-size 20513
+oid sha256:a3e251346b5d1c520e68998bc4e141f7707aa8ffc5c499ca12f8e2e1e8a7f171
+size 20695
