e39cce9417fe5c46d8fe75c20544a33a4267f885
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-05-23 08:48:57 +0200

    Separate tracks from Rally

--- esrally/track/track.py
+++ esrally/track/track.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9322968a21ea3e36fec77eeb5f16d4cba0da788ce41cb89c2822ec8f4cc67839
-size 20211
+oid sha256:8256ce236e700caac9975b6af8795daa726e29022d7577b07827eac17f4b752d
+size 22990
--- tests/track/track_test.py
+++ tests/track/track_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d630c762dd7654916ea91dbdd1a53208cf59ff9dada3122c38512836fdafb716
-size 807
+oid sha256:1e424c0d8d0005707967094cfc9ec4e51d423c398c2fd357e48692a7da7afa40
+size 3709
