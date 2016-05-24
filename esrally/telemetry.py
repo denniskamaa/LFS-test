fb50507a765fc5495772dfc2eb65869307d6ea4f
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-05-24 17:27:22 +0200

    Add support for externally benchmarking older ES versions

--- esrally/telemetry.py
+++ esrally/telemetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:085b2677dbaceb1b4d93af1ac9290bc1263f562c2ee1cc6a8b81ba6235a1b1fe
-size 22571
+oid sha256:e8e0be0afe517f97ee34ad30335c1be016d1935774278d5df3a9b246ab02330b
+size 22593
