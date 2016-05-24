47ba727fc21e6b02fba001b6e191a1d9ef2e3139
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-05-24 20:57:04 +0200

    Correct Rally branch detection

--- esrally.sh
+++ esrally.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dfa230ceae3ac1483acfae5e6e63080ada2f8bf8ffc0307b239ded2ea300e013
-size 1424
+oid sha256:7d525b3ad72c25c51bcf1f399a2995126a37501f1b1d7e38fd9c1dab21276557
+size 1427
