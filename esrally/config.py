505d593d70c51ecf64218bfc58a724522fbbfa32
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-05-27 21:03:12 +0200

    Don't echo password

--- esrally/config.py
+++ esrally/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e0d4be46fdaab48b7f7cd796dcfbcfff4db67a516a85286a7d1f75733422a99
-size 22348
+oid sha256:4e6ffff33bd6cf0dcee5c9221d220f29b478563c1f531764e3bdd4111cf7f95e
+size 22529
