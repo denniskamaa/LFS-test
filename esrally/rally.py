0bc9e33458341ee5ce75bac96e3a91cd43c6ff5b
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2016-05-28 19:50:15 +0200

    Fix bootstrapping

--- esrally/rally.py
+++ esrally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b917dd1a45a9b9192ae18a3c2a721253b4e0532cf2f431dfd0b6ecdc2f3290fe
-size 12256
+oid sha256:b29f498badd07882b04570e45c4c578680bd704a2ce94eaeb49e462b81ad193b
+size 12335
