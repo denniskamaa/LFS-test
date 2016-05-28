53b0e9d1b9c8c4dfb600f31cf6b79d106fc955db
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-05-28 19:04:02 +0200

    Fix parsing of missing document count

--- esrally/track/track.py
+++ esrally/track/track.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:efbd6ffd44bb3e949f05868853441c048b12cb03548819d63820df43bab7a2a2
-size 32114
+oid sha256:4a9aa71fa32866fc71c6d40fddd6f2c860124c1250bbb6de6c40d95861a137f9
+size 32131
