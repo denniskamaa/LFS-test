802aa284054eed13ac5453e98ecf477684ef24c2
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-06-06 16:20:20 +0200

    Don't calculate percentiles for missing raw data

--- esrally/metrics.py
+++ esrally/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d376b0a0bdba90ccc0eca767191711e547f3034b1d0cffe4318293054e900b97
-size 29505
+oid sha256:e7fe66f3628f70704ea481e13dd6a26b0db0c2adb0b2f388ab1c14e4f010e22d
+size 29545
