b98801fb3c38e6b93ea6ff2a1ce9333e97155cc9
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-06-07 11:52:26 +0200

    Improve error reporting in track repo

--- esrally/track.py
+++ esrally/track.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:60d731a9f7bff276d6e8313a77830dd2a0d1814b13626295f8ca6b60e713a8c9
-size 29492
+oid sha256:1ca2ff0cee2b3a0213aa32ce2cc1c3099caac401605c0d05af5126ba9bee845d
+size 29667
