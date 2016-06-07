1dd9d715e712024237b07631d0ee56490eff5f62
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-06-07 10:59:34 +0200

    Correct segment memory report

--- esrally/reporter.py
+++ esrally/reporter.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ffa59b2d9e28751d93d418628786ba9c482823aba29ebf411e14a6d4f1d40fc
-size 25848
+oid sha256:1a4172aa55262e2c732b6cd2f0c12c86fd914b1bd28266100c95201f9b7dc104
+size 25855
