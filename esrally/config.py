c0fd973150dd2abcbfbdadd9cda3a181fdce7a6b
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-06-09 08:10:20 +0200

    Gradle is the only additional requirement to build from sources

--- esrally/config.py
+++ esrally/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9c56ead78ef9818d8703c0b2e03795b4eb4b8fe70c6c3dcef0263e4f410c11c
-size 23016
+oid sha256:9bbc9cf213099c78ad58fc8ad66be186d2d491a4cd78428163d23032b8de7159
+size 23005
