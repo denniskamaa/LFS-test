52d5c85e3baee33862cd379ec9664505d0d62cb6
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-07-01 13:52:20 +0200

    Correct log archive name for external benchmarks

--- esrally/sweeper.py
+++ esrally/sweeper.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b7cfef7e006bc578a8abce2b1ed8360752a1eb78fd5d526ce6a28bd38f909a3
-size 576
+oid sha256:aaff9163075af7eb15875b4b5b4ca550678bca86a095c02082e704148157463d
+size 739
