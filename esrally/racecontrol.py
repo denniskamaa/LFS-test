021565343bb96259ef568c731215b3ef521c6727
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-07-01 13:53:34 +0200

    Earlier detection of distribution version in external benchmarks

--- esrally/racecontrol.py
+++ esrally/racecontrol.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ffe5463849c7a3c8d47bba122ee59a872e315325e6e2b1eb4f78e7fd203e925e
-size 14021
+oid sha256:bdf8066acb356231296fa3235c6abf3ae15bbd7123e245e5821fcb43f26afa64
+size 14129
