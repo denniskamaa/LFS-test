083e1741e6dc570378d18d9f825cf57d8b4a32b2
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-07-15 07:13:41 +0200

    Improve version output and logging

--- esrally/rally.py
+++ esrally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5e58b1ec4589e39a019ccb6332819545803baa8055716b6ecbc9c5694ecdc83
-size 15874
+oid sha256:1f61abe34150d4ac18e0baabd5541bff80259c063d7ca2725ace57ebe264da6c
+size 16409
