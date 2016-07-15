fc0d2b2630f7de973ed03414d76752a784c01b44
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-07-15 08:45:57 +0200

    Use correct property for basic authentication
    Relates #114

--- esrally/cluster.py
+++ esrally/cluster.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:738ed1ce84de6ac5be93f5b35bca8b0de4fc6b7313d2768fb58a3f2021b5f28b
-size 8409
+oid sha256:5a6b3b0b4ea31c4472f0a9f11b4b1ece5c89547e5db4f127790a92bda83fefc8
+size 8414
