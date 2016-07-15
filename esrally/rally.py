3aecfe62ee283a4a8576bbc9eaad64d9c6bcf6c1
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-07-15 07:32:31 +0200

    Improve help on errors

--- esrally/rally.py
+++ esrally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f61abe34150d4ac18e0baabd5541bff80259c063d7ca2725ace57ebe264da6c
-size 16409
+oid sha256:46596694bdc64ddc2b95a15d9ef866bf9e8c660ac424610c362b8da3944cb0e1
+size 17266
