56618dd35b4cc7becd9b17a8f9121166f472ae8a
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-07-27 22:13:26 +0200

    Consider git revision in version check for release script

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dfb620df0f5e739ffcbb97347e2b86420cb543f90479217779416b47eae764fa
-size 1614
+oid sha256:e61672c53ef6eec64d20f1eb9415e012f59a7a5ca6656802d3ecab2401b7ada4
+size 1646
