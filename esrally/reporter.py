0739912a854a373ea058a9e94e214eb588442acc
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-08-09 12:14:57 +0200

    Preserve report structure also for missing records

--- esrally/reporter.py
+++ esrally/reporter.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16e62c9a3f167ed9aa05e2f371fbf1aca99880a61092bd95045d91a856fe68dc
-size 28363
+oid sha256:14581ad53d5222bca8de8be8c0d7036df672044cf3f9674b9bd92dfc4bbecfc3
+size 28991
