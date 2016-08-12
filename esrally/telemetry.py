2548dd3f4c283281c11d0c729e67ef5769f3df75
Adrien Grand <jpountz@gmail.com>
2016-08-12 09:30:07 +0200

    Increase `maxage` of the `jfr` telemetry.
    By default, the `jfr` telemetry only keeps information about recent activity.
    This commit makes it keep all data so that it can be used to analyze the whole
    benchmark.

--- esrally/telemetry.py
+++ esrally/telemetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d000bcac05a8d5829cf4d2ffecb4d1a1fb8d89d777821918faba4322349f10e6
-size 22852
+oid sha256:999e5b5431be52e4c8fcede4bfeb82421e9783a7d6b021ad63860ccfdc03a751
+size 22872
