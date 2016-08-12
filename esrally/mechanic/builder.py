77cd5fa917ed79ce7134523f778a5f427f254626
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-08-12 11:07:45 +0200

    Minor cleanups

--- esrally/mechanic/builder.py
+++ esrally/mechanic/builder.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f37bf76a1c4a8954f1135e2b2ab6faba6c294d3f32acea1fe54ab3259d0d8f1c
-size 2442
+oid sha256:355e84936f755d17b795d163d7df4c478f2ecbd2706a7d66adee9cd983012515
+size 2369
--- tests/mechanic/builder_test.py
+++ tests/mechanic/builder_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1bc919d557bc74c7d79745e4ef21bc46a1a401779ddad369f82767415f1b2bb1
-size 1727
+oid sha256:fdad9fff4d8a7929c337433030d300cbcb9baa560a5b916c14ffb784380ba90f
+size 1697
