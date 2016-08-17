470d335547aa1d24f9191c23df6a59e1d6edc689
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-08-17 16:57:05 +0200

    Support multiple track repositories
    With this commit we add a new command line option `--track-repository`
    which allows to specify where to load tracks from.
    
    Closes #99

--- esrally/rally.py
+++ esrally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ab9c6056ba86626d8d9cde421b9850937f4ecd0e54523394377a25247dfd6d3
-size 18252
+oid sha256:b7aa1f5a82d2d605a0cfd972916ff367698365b28c07bfbe6363f4a41fbe2134
+size 18390
