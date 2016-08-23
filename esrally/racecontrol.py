06ca5a31123e7ee97e030f0ba20353723a3ad6e5
nicholaskuechler <nkuechler@gmail.com>
2016-08-23 09:54:01 -0500

    Fix typo in rally message. (#136)

--- esrally/racecontrol.py
+++ esrally/racecontrol.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db56ce602c2602be43b277c1dd2c4c4c833605cfda8c1053b9f91f629840f520
-size 16548
+oid sha256:8c479b279265023a69f9f8d4c502f9b9e786e101edab18a23803b02fdc7c7e02
+size 16549
