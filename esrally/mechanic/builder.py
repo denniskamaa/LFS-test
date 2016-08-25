a2e57534e61514bed352caafbc52023343641f1b
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-08-25 13:53:10 +0200

    Allow to override the source directory externally

--- esrally/mechanic/builder.py
+++ esrally/mechanic/builder.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:355e84936f755d17b795d163d7df4c478f2ecbd2706a7d66adee9cd983012515
-size 2369
+oid sha256:502d2e1deb80f35fe3f4c46da56afed88d4616d79bd5ce4431be38f4412f102c
+size 2447
--- esrally/rally.py
+++ esrally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:abab17be03fbb3dda17198580f13f5099f075dfee1f6692b6c1b5e15215b281d
-size 18568
+oid sha256:1be8db3ff8a2c8bc47e538d1aa8380616591bfe25d1b9c864a9a22da74da533f
+size 18830
