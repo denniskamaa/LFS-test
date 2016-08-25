3d37d2bad1c0953c06f8f7d0793bc92bcbeb1bb9
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-08-25 10:36:20 +0200

    Create a backup of old log files

--- esrally/rally.py
+++ esrally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7aa1f5a82d2d605a0cfd972916ff367698365b28c07bfbe6363f4a41fbe2134
-size 18390
+oid sha256:abab17be03fbb3dda17198580f13f5099f075dfee1f6692b6c1b5e15215b281d
+size 18568
