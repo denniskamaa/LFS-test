452ea618b7cc2e37bcff45190f53feda8e5cade6
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-08-29 17:24:53 +0200

    Do not override externally provided client options
    Closes #141

--- esrally/driver.py
+++ esrally/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:922ee3f80fc73177e1fd18af152e00259aa3974479e85f322582ebe44a08dc41
-size 44642
+oid sha256:b7e50812fde138edc62b92a5e9e3480203413a0ced160948418f2eccede32c32
+size 44607
--- esrally/rally.py
+++ esrally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b38c73b4255e412ffd2664136bba9984e078f2002f25b123c6cb344b4e7ac47
-size 23656
+oid sha256:a90a02d9afb3570fcf9e06db1f1b671158d59a27ab60d8eda19b3ef9f346cd0f
+size 23668
