b48db0c9930d291ff26e0bf69998c0bdfb255e32
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-08-29 17:13:42 +0200

    Force-merge only indices defined in the track
    Closes #133

--- esrally/track.py
+++ esrally/track.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb593485a485e0895fc26357d43c51242b8e3c6ce7d6bd0e079b5a6c3c427c28
-size 27491
+oid sha256:4c93a555c3c6d87ffc0f20a6e3063a4ee63745661f8ddc2e20a764d5fe08f469
+size 27546
