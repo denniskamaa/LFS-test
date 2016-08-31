8baabd618b2925ad2b282576b657d7ee8010124c
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-08-31 08:02:56 +0200

    Add test for node fallback in telemetry

--- tests/telemetry_test.py
+++ tests/telemetry_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7af038832f9678810df5c9c7430e136b9e5b8c26809d4ecb33c8d4303d3ecf72
-size 11602
+oid sha256:382999e860a0290933e41b1f39e27629e1a7c6915d6b3a6e112eaae097cae0fb
+size 13742
