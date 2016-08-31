20cf33b4421f7eb1f8493105ee103e5bca5a8634
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-08-31 07:35:56 +0200

    Change user forum to Rally's own Discuss forum

--- esrally/rally.py
+++ esrally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a90a02d9afb3570fcf9e06db1f1b671158d59a27ab60d8eda19b3ef9f346cd0f
-size 23668
+oid sha256:55a35c9b598f018c68a4732586b87318a756da277935984a87dbe4e04d0bfb7e
+size 23660
