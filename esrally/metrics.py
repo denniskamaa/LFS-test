268871736a30c15fc78d183290b0bd0d0bdc4c76
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-08-31 12:42:36 +0200

    Adjust representation of target hosts in race store

--- esrally/metrics.py
+++ esrally/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:92a7d5ef891244143674a4ca8e63af5d25598f96413de924bde51ed436e6d1dd
-size 37064
+oid sha256:dccaed64785ba997d7f8324cdf0399d1bb8ac9f3fd986c5ff4ab0e1386656792
+size 37108
--- tests/metrics_test.py
+++ tests/metrics_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0fb2bc86496b50884303672ffd480524436e15ebea331a86ce7846996e60c7a
-size 13117
+oid sha256:159f3c7b4eda9ab209b64654e65cc13e288b4293b8aa8f80b9943efde1e1627e
+size 13170
