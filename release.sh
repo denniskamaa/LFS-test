ba827cf642e5b3cfa3b78d4f98be29657623d15d
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-08-31 10:45:05 +0200

    Fix exit code check in release script

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e679b8751aaee92bed15475851bc10d751f86cde124da002aa4021c27b7693ce
-size 1998
+oid sha256:ae9e290bc12d8c0c7d3a315cfa94550191c9348ea332855938596e71da653304
+size 2000
