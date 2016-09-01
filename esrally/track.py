0de6aa113714681aa12411e72929607dd63796ff
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-09-01 08:44:51 +0200

    Fetch tracks from 'origin' instead of from repo URL

--- esrally/track.py
+++ esrally/track.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c93a555c3c6d87ffc0f20a6e3063a4ee63745661f8ddc2e20a764d5fe08f469
-size 27546
+oid sha256:59c8c2c0393b340c5e1601aa1cb7090af2ffbe2fd25335cbc5be375e9709f576
+size 27529
