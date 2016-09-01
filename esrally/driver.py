5b14ba22f25ea17bbc45acf2474e52bd67e2157c
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-09-01 09:44:56 +0200

    Use cluster health API in a backwards compatible way

--- esrally/driver.py
+++ esrally/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c569787164a2a946923fbe7645ecd56be5f44d2e60f2989620d86f4161d6a6b4
-size 44607
+oid sha256:1c58836c4ae7595f4b1c71e5bc57e233c074c397d391c10a1ec2f010c813bcbd
+size 45295
