3949243db47b11034de4618bb9daa62c65ede25f
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-09-01 14:35:32 +0200

    Reduce visibility of utility classes in tests to avoid pytest warnings

--- tests/driver_test.py
+++ tests/driver_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ecc25cde1b1b12201506f1da69f8a5f1df9fa79a61483b6dfb8e485a134a450
-size 18405
+oid sha256:4020120a6f826e1d8978e7bfdc6ce6e456611536c1b74cf99e747d7d6aa0164c
+size 18821
--- tests/utils/process_test.py
+++ tests/utils/process_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea3ecfe78d309bb6d9588986ec78d25b1da1c3cdf7697347d9d1549196797447
-size 2216
+oid sha256:4b044dbdf5d0ec70c262f4b5d8f1788b9de0ce5eeeb1f6e933a72da0e393351f
+size 2425
