ee7a00395146710acc5809789c86dbd5db20f143
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-09-05 15:23:58 +0200

    Stop reading at either the end of a bulk or the end of the assigned chunk

--- esrally/driver.py
+++ esrally/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4920f3a71bfd955b7d1f178bc880982fbc035eb26d41583972c7ae254ceac04
-size 46263
+oid sha256:e55f0c33cb672576a0d39f06f402b74b3da896be32712b9dc7498805184e3345
+size 46323
--- tests/driver_test.py
+++ tests/driver_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:040cdc203d1104eb3882e771df6ba084ca6fcf367fb68eca7f897c71ab853f1e
-size 19104
+oid sha256:0e05e1b034f80f6d4812b3e0e6d79f4ffa38489a5ea22a7e2f2e07d85c16aa02
+size 20542
