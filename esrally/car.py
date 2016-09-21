c8818df821840d3dfefbc61d7ac3e37d9fd6a17d
Jason Tedor <jason@tedor.me>
2016-09-21 08:19:21 -0400

    Add logger prefixes to log configuration
    In elastic/elasticsearch@fbe27664a65ca7ab5b7dbe1c328ab27423e4313c,
    logger prefixes (e.g., node name, index name, shard IDs) are no longer
    part of the message but are now markers. This commit adapts the layout
    patterns so that these markers are included in log messages.
    
    Relates #156

--- esrally/car.py
+++ esrally/car.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:729e31f3a4a3435e3411d1cee7f72a76c702eb0baa3b706abe1e034d1bab3340
-size 7464
+oid sha256:66dd362b3a4f931b0abe43a31711ba5848f02145acdd634829254ebdc378e325
+size 7499
