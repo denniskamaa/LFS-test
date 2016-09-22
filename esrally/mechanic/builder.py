a1dc1762161ffb0f7da1af3f85c25fca37567ac2
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-09-22 18:18:04 +0200

    Explicitly set JAVA_HOME to defined JDK on build
    With this commit we do not rely anymore on JAVA_HOME being properly
    set by the user but instead set it ourselves to the defined JDK
    before building with Gradle.
    
    Relates #157

--- esrally/mechanic/builder.py
+++ esrally/mechanic/builder.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49c1a1becd6c7dbb6cf138a94bc0323dc0a830907cf07ed7e91beb5c6b7fcd0c
-size 2507
+oid sha256:7d7ebcd33f9c08f0d2109d2c5935bfc5c5562607d271a7dc2fffdb9f013fea97
+size 2603
