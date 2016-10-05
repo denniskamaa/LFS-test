1cb5952c3db8209574a1e5489b46a78b491bdd2e
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-10-05 16:59:56 +0200

    Remove unused 'level=shards' from node stats calls

--- esrally/driver.py
+++ esrally/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3df73b790a16f3ea718d13ab8db3db59cea42898d011e5a5afcae493f74471be
-size 47272
+oid sha256:3e49b549ae9f3222ebd7dd55b91a457c45dc3635607ae805e70d445785e3b0ee
+size 47240
