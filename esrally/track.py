383c766958b4f89d0d0e922adb01d1a9b2e16ba8
Aswin Murugesh <asksm46@gmail.com>
2016-10-06 17:35:27 +0530

    Allow users to search in all types
    Closes #161

--- esrally/track.py
+++ esrally/track.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4b9b541a770534d3f333f08cb7afc0bfa9fd3c913277902e0502ec73f9b1b3b
-size 28341
+oid sha256:dc06cbefc493280fb8b0dd53d8f9d8835a8207f35677270c5872c302bed1c0bb
+size 28313
--- tests/track_test.py
+++ tests/track_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da01254d7dd967bf81809bb86706ed5f0436fb45f8d4e36043f07a03780c3084
-size 4461
+oid sha256:2a6a6a94a337cd79912ba813486c8c40baf863eee3f44f007209434d5dd01ef1
+size 4777
