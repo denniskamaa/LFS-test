eedd343a36e6f5b6552b72e05440fd4141283e7c
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-10-06 15:49:15 +0200

    Improve feedback on errors
    In case an exception occurs in the load test driver, we
    now communicate these problems to the control process so
    Rally can show a better feedback message.
    
    Closes #153

--- esrally/driver.py
+++ esrally/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bee44560a39ebdc292e7550c424498e706c8d5bcd19e5b2ed76703923d305d77
-size 47478
+oid sha256:869a765f4ca813c1e24a8e854cd1248546f9d59f29206f5b895f2f14ccb7ce64
+size 48979
--- esrally/rally.py
+++ esrally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d58335817234ee5bdb3f609246d59fe550016bbec988b564c914f8664ccb027b
-size 25596
+oid sha256:9d191ba1cb26f0f442da602eda1384a5951dd921c03667c54af398fd7159871f
+size 25724
