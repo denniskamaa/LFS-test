75ff7da2d3964efafd5610b4393c10f5bfa6bd43
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-10-06 10:06:41 +0200

    Allow Rally to log to console
    Closes #160

--- esrally/rally.py
+++ esrally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31fa08015d0d7f7d832fbbe90feeaab5d3dbfd856b32f702f4062f3d0dfc7c70
-size 24477
+oid sha256:c1f9ab978dd39d1978b4e79abd792d453bc18df274f790e8f374fde6d13191e3
+size 25328
