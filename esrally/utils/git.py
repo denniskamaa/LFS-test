21f08cd7ac48a9c1e584ac8e741d1cf4da45cfd0
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-10-13 13:09:29 +0200

    Don't check git -C on clone

--- esrally/utils/git.py
+++ esrally/utils/git.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:779d8a3329853e185108caa682ba256d2bae6894acf203fa3ad4ab93333d5956
-size 3770
+oid sha256:691d91985f2ef0c3125a75d42f456e731cf820ad8a5be7ffe1f530e9585bfefd
+size 3762
--- tests/utils/git_test.py
+++ tests/utils/git_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13f20b42d7602cafb212421f507058f761be082d6bc8dd53094e175e0448687a
-size 8900
+oid sha256:12fa953e0d914ebfb7199c5fa6909c5bbb429b14efe97b7def8a1b280744a6e7
+size 8645
