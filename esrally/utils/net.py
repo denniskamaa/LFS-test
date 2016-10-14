b4512effb392f14ebf7a23bc4435e0ed739d9622
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-10-14 08:19:43 +0200

    Guard against unset 'http_proxy' env variable

--- esrally/utils/net.py
+++ esrally/utils/net.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5413b96b84649f1cc63ea33083d7b12616db633ac0f10b5533f237a1c977d6ce
-size 2286
+oid sha256:e29e7273fbc91f2769c3122e7d8d9fb3431625ed224edb3968e3ced714af6927
+size 2300
