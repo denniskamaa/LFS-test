e6ef67251659ada9dc6828bee21da6aefdc800b2
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-10-14 08:25:12 +0200

    Fix logging statement

--- esrally/utils/net.py
+++ esrally/utils/net.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e29e7273fbc91f2769c3122e7d8d9fb3431625ed224edb3968e3ced714af6927
-size 2300
+oid sha256:3371cf90039e32df9e3146d78bc5e2d31c90b24ac0bd88d4f31b959a1ca7e82e
+size 2288
