9e5a3fa23f2be9a95270dde0fa3bdcca33d02393
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-10-17 14:01:28 +0200

    Detect when benchmark candidate fails with OOME
    Closes #169

--- esrally/mechanic/launcher.py
+++ esrally/mechanic/launcher.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20b2f1fa663382f7e228926647dd50f1db961c0888fd39be17d19bf670e2a0a9
-size 11713
+oid sha256:c8fb90bda1eba6603fc12ddc83f874398bc7fc2b6fe8a3aad15ea6353a4824ab
+size 12239
