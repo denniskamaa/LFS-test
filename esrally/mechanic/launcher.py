3a96d46fe5fd97558058f4a4821b55721b6909a8
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-10-21 09:35:14 +0200

    Add GC telemetry device
    With this commit we add a new telemetry device that captures GC
    logs. It can be activated with `--telemetry=gc` on the command
    line.
    
    Caveats: GC-related flags will change with JDK 9 and this
             telemetry device is not yet prepared for that and
             will fail on JDK 9.
    
    Closes #120

--- esrally/mechanic/launcher.py
+++ esrally/mechanic/launcher.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4d32441f8caebc8383b9a0de75bb44566831118b7b0ad7394cb8ef5632e2c63
-size 18745
+oid sha256:34d78b6fc9f5adf66cd26aecddf902973649c4f9da8d61757e6b123ca1cd8173
+size 18801
--- esrally/mechanic/telemetry.py
+++ esrally/mechanic/telemetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b1fb0005d1c923eb646f5d26c6e27b9e246a1e74316f2991b172c2a9692c522
-size 23199
+oid sha256:10c3e1adde318baabadc9297f11e89ea3e8e9223cb97c32172e10f0195471770
+size 24407
