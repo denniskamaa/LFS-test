68fcc2022fd5fdd33f420a456694f3ddaf3e6223
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-10-27 15:08:56 +0200

    Restructure docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:facbd0944a499d8a78c99f6771f643d030845baa8a56b69fb75bf12a1fff344f
-size 9735
+oid sha256:db3e6f24a113d5a866335364360641e24c2938cce169c9506e49bfa27b8ec746
+size 4508
