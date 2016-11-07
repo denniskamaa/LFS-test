e99af1bac9b1e1078e157093ef42cffe8bc6b67e
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-11-07 13:41:53 +0100

    Autodetect Internet connection and warn user if not connected
    Closes #154

--- esrally/rally.py
+++ esrally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d837c00bb6bf56c3dbf3518f6a85b030c83787658a6721a8caf128645e188ca
-size 25889
+oid sha256:76dfdd0bade77800b7e9406da19d1c9820dcacac7ccd133d172ccce4a79e8e5b
+size 26759
--- esrally/utils/net.py
+++ esrally/utils/net.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3371cf90039e32df9e3146d78bc5e2d31c90b24ac0bd88d4f31b959a1ca7e82e
-size 2288
+oid sha256:0afc8a505e6a94e6ba43997419d76295d0bbae07bdf887db44c87cbdd5139b7f
+size 2578
