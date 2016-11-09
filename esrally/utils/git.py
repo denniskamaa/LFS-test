67d8c457e211def16afcb28fb8aa7adec4cce3fd
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-11-09 08:55:32 +0100

    Remove new line in git revision string

--- esrally/utils/git.py
+++ esrally/utils/git.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:691d91985f2ef0c3125a75d42f456e731cf820ad8a5be7ffe1f530e9585bfefd
-size 3762
+oid sha256:7f6d3858ca3af134e4ab8ea83132d513da8c26e71cf346d03181fc0a6599bfb3
+size 3770
