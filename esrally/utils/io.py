4089edc892a69973f747ca8f1f2dd8bb91936dd5
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-11-09 10:30:41 +0100

    Fix JDK detection for Red Hat based distributions

--- esrally/utils/io.py
+++ esrally/utils/io.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f13d7f9164528e04d84b16883f3569e9826ba513a9ec2a45d4076c4c52214d6
-size 9436
+oid sha256:0e971b64ed68b33e6fa0a7105ca31295f3a291e9f5c5e4b95aaaf4174b6216a7
+size 9370
