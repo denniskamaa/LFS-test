b94f487b209fd65895ea37dc7a0509da5a442b23
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2016-11-11 10:33:08 +0100

    Register PartitionBulkIndexParamSource also as 'file-reader'

--- esrally/track/params.py
+++ esrally/track/params.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0034c23175eb87b11bdb0986796674b5fedc35f6228dd089f4943bcb2cd4fb59
-size 16941
+oid sha256:56454fd3685843bb1c681e57d8748d092659d6fa0e6271aeda4decc2e61e8e4a
+size 17059
