21a50f70fa5eea94932bb10e7d298c870bd06451
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2016-11-21 11:34:42 +0100

    Resolve home directory portably

--- esrally/config.py
+++ esrally/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c022f6482f66d63ed62386735f9dc6eb4419576d1c03080400c8f113ac4eed01
-size 23586
+oid sha256:b623c8ea27655a160459f29cef16a03ec02d37bbbc87a9d3d77e23b2f157a8e4
+size 23592
