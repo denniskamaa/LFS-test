b57f4b2b612a88d3ee8b002833b263c77b23cdb7
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2016-11-23 12:54:05 +0100

    Make SchedulerTests more robust

--- tests/driver/driver_test.py
+++ tests/driver/driver_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9dde2a90574994a37a96e77d37a4b7c7562651f46d8d8666153ed9eb0444e6f0
+oid sha256:aaf394ac1603184d82dd8eb1a7fb2ca5831349aced4999758695df5f4553f280
 size 20970
