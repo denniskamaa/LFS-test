fdd897ad256e8c5c984e2ab49a60416da0adcc32
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2016-11-23 13:34:16 +0100

    Encapsulate cluster in mechanic

--- esrally/mechanic/mechanic.py
+++ esrally/mechanic/mechanic.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee109a0a5becac63071346f052497942f595b5f48346138f4bce06a2508610fb
-size 2411
+oid sha256:18c37c78f93ca8fcadb8c2b7bd9082eb9cb4c7889d0a35c07cefdeae2f2c6e01
+size 2762
--- esrally/racecontrol.py
+++ esrally/racecontrol.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59b94cdfd4f6bc64ddd03a17721a758f3741614cef15518b3284d9a99c79999a
-size 9619
+oid sha256:5f8a822cd5c078a82d46685047eedd1e690cea979e978ca46d849fa1c59ca591
+size 9439
