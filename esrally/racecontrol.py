1fc9df534ebcdbbb7936062594cbacd4167df6f7
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2016-11-30 15:20:23 +0100

    Remove pipeline destructor temporarily

--- esrally/racecontrol.py
+++ esrally/racecontrol.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f8a822cd5c078a82d46685047eedd1e690cea979e978ca46d849fa1c59ca591
-size 9439
+oid sha256:2f49169e9becb78a02eeb7bfd148300d46db07b9fa64ebb688f434f3c166e42b
+size 9445
