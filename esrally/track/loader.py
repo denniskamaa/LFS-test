0edbab9db11837ac3f08968e6c6b869a0d9e5176
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2016-12-05 17:49:03 +0100

    Make meta.data-url really optional

--- esrally/track/loader.py
+++ esrally/track/loader.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f5d37c833274a7dfd0fa27a6c015e20d8e7a99025c85992334019e8eb6e6f00
-size 25731
+oid sha256:51dcbe7ee9902ab8ab2f76cc22d5d20f4cc37bb4a23226baa582128fc8f206bc
+size 25748
