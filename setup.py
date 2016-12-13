7db712255a30cb3a9bd496be6d1f32c9f1d4a2c0
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2016-12-13 13:01:51 +0100

    Update TheSpian to 3.5.2

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d7cbef6a0d8694d2a9009eb7ccbbd97ce07bd1809b606700932bf4302e74d12
+oid sha256:7ff18107ade2b7cffee6166bdff0a5aa5b1b2a18c7482af309db71d216ff1e3a
 size 2091
