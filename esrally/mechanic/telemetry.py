2b6191d04023e78e11811e396989840bf85a4d86
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-01-02 09:18:01 +0100

    Warn users before using commercial JDK features

--- esrally/mechanic/telemetry.py
+++ esrally/mechanic/telemetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc8948a6481ead91c98ca3a9d4fcd4a3f6e56c36f7569a9848108f351200c68d
-size 25703
+oid sha256:4b2619d466aa98901cf7156c41fd3ad90b06bb045fda4c063f11d32adffec2be
+size 26513
