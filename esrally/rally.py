6fb0cab79219eb080c50bbf87c24f279dac73031
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-01-10 08:47:23 +0100

    Correct Rally Discuss forum URL

--- esrally/rally.py
+++ esrally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43aaa03d546033225ba2cb33a49696852387d357316502bab67b185e2aacf46d
-size 27114
+oid sha256:3aa90dbbd6dc8c1ebb1260313a8e4b9bf6286bf9bfe9c18f9c0b23e5e299622a
+size 27128
