2d7f3ed21f199ee6c94f24241c4b464442fb38f3
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-01-11 16:43:19 +0100

    Document release requirements

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff3995af336c5159540aced616ec4d47c3d16466b19ec9b90bde892f2c06f7c0
-size 2179
+oid sha256:9f25c7c335d3e4a3325fae5d9eb824e9a15ef1dc261934bc916af861224cb823
+size 2254
