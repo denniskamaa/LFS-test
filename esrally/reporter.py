6d0a2a6cd071d3601da0844a26801d179f7c6008
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-01-11 08:04:25 +0100

    Add string formatting test for ComparisonReporter
    This commits adds a basic test to catch formatting problems in
    ComparisonReporter earlier. It's just added as a basic safeguard
    and we need to increase test coverage significantly.
    
    relates #197

--- esrally/reporter.py
+++ esrally/reporter.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b836f1376e26a00de36824f8105fa9bacfd181b98db076333732feb79348f67e
-size 30784
+oid sha256:628e097246e13fe1ebdd02e83426ea60056c1584ab272d6e9c922b3c92cc1e60
+size 30850
--- tests/reporter_test.py
+++ tests/reporter_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:225770e7d85bce1d33eea1b59fc0001e83da89bfb5ee19301511d11b3be629ea
-size 2686
+oid sha256:572f7eef7a8248aa816f28866650a9683067d736a161fc257d693eec7268fe5c
+size 3519
