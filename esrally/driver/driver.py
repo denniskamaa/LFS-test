5d31fafcce24da86ad4069fa5775e15e681d8a13
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-01-20 09:04:27 +0100

    Provide a proper boolean in cluster health call
    Relates elastic/elasticsearch#22200

--- esrally/driver/driver.py
+++ esrally/driver/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e538c93bb00c5d6a964c1782fe78ce8f62806c41ff0c5fd2cf8cb5ebc7946a74
-size 41911
+oid sha256:8b368495e07f780cc7896a0dc0049db21e0b38f0917dcd559f55805a301b40b2
+size 41913
