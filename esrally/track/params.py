f9106c617f26f7ea7e87cd22bc7af9f0d31ab550
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-01-23 17:07:34 +0100

    Remove obsolete print

--- esrally/track/params.py
+++ esrally/track/params.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0e2e1f21fcd8660058fe1e4cf8ad057d72f0e9067a7e3fc47eadce45102fc53
-size 22954
+oid sha256:3652a20d9bb764fec9434630155ab6a75bada2b9257dd2b0ba6556b47cb8cb0f
+size 22907
