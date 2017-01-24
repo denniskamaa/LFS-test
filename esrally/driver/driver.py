07ce647cbe6a023bfe19cc89100f7160f48ee5c5
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-01-24 10:58:01 +0100

    Improve error message if cluster status could not be reached

--- esrally/driver/driver.py
+++ esrally/driver/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:71c6a1ded0be327ff4583ec077e49b95cb7e3929c39e7da00f02300af4377c36
-size 41911
+oid sha256:d5ecbf36a53e272d85542878fee6759dd9420d0bc8f4e7060a0ee75b7cf7f4fa
+size 42104
