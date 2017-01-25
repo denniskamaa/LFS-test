2f7b4bb42ef6d76ff00d0343b600d2b6a4b066d1
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-01-25 14:11:05 +0100

    Only print if attached to a TTY

--- esrally/utils/console.py
+++ esrally/utils/console.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d33ac8d520300e914515252be3ed497acb315386db2f5b0f377bb8d6367af91
-size 4586
+oid sha256:9ff7b54c64feb147a970bacff6525ad5d093300b0935584ed8d4454f7acf093d
+size 4610
