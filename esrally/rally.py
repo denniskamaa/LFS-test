4270294c542e6d18d7ac652549fd865bebcdc4c3
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-01-26 08:12:45 +0100

    Document the 'test-mode' command line option

--- esrally/rally.py
+++ esrally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d081b4664cde62d18080b8265fdb61bc0d7ad7e820e712c736fef15911b1facb
-size 23319
+oid sha256:551feb0ecddd053cd8377d210f416b0fd8663a8f4c568b3ed2f620ef6bee08db
+size 23375
