76825e730f4387984db831c05eaad59c81e846c9
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-01-27 12:07:20 +0100

    Rotate log files daily instead of size-based

--- esrally/actor.py
+++ esrally/actor.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cac6b31f8f192ddf8dab7caf792b55844432a5c1d7c3295aef726f5177412f02
-size 7091
+oid sha256:1788d29a56fa785cec5b6854ae8fbaf1b3a970cdaf3537a7ed745ca3e4afadba
+size 6880
--- esrally/rally.py
+++ esrally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:551feb0ecddd053cd8377d210f416b0fd8663a8f4c568b3ed2f620ef6bee08db
-size 23375
+oid sha256:7bf6fba9031bc1de28f55e2f683e33b715e7fa65d9fa08614ecefce1400129b8
+size 23364
