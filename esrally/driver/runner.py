5d3501cfe9f32fbd805ae7c54990c07cce82c4a1
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-02-07 18:14:34 +0100

    Remove leftover comment

--- esrally/driver/runner.py
+++ esrally/driver/runner.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:477c3cdc5d080d6594abf98f9eda381b7b5b26de3c170e3282fa15ee6a6153ec
-size 15484
+oid sha256:3faf279dcc8928e7e17f4788f73bc5fa23fbfc30f22ad2f2b2e72cc17a4ee2dd
+size 14974
