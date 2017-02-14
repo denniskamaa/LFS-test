587d071d42a5de0478b9ef4b4f617945055a15c7
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-02-14 10:31:21 +0100

    Properly cancellate benchmarks

--- esrally/driver/driver.py
+++ esrally/driver/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee9edde6fdfb80be09b0505bed39df58c400cace96ff630925cd583f3f7d5b5d
-size 44735
+oid sha256:721705620c46b956899af3af414a5564c93497c3f04d4e73de1881fc57280f2a
+size 44983
