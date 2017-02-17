61961f4c391d55a3bee862b55737bef28f07040a
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-02-17 13:41:42 +0100

    More robust launch checks for Docker launcher

--- esrally/mechanic/launcher.py
+++ esrally/mechanic/launcher.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2208fc870f83406311982dbca858bf1e6696ce12db4e7ec6d69d6585a93756d9
-size 17815
+oid sha256:1530a60031a3194592e4a1ece3055f5e10c236b8d6ef6533a1d1adf0709fa40a
+size 18810
