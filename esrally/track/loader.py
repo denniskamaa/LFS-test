b46c53d4b4fec6afc1d1729141fe7648616c3637
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-02-20 13:43:14 +0100

    Fix test mode for tasks with multiple clients

--- esrally/track/loader.py
+++ esrally/track/loader.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a9d57b3dbc15d81c1962499f75c2de2e8cc70e1c3aaf7d243ccc085161b2e38
-size 35356
+oid sha256:ed26d8aff36bf440a21aed7e3d11e87c2c01f6425ca6df6d05c60dadaf91f46b
+size 35644
