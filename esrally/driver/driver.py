9aeff8bb13467eedc10147bdcc94490868aa5766
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-02-22 08:06:41 +0100

    Significantly speed up test-mode

--- esrally/driver/driver.py
+++ esrally/driver/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb34a0a11b95ed01ce74cce2d60c1c0533deadf07376e8db5afdfa315cd52499
-size 45100
+oid sha256:78cf1478e480fabacddc1617d9c16fbb7a41c1738f5285baa568e5008d311c96
+size 45571
