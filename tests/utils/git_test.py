d45d568dfa71462b9105ab4fe3179c9d79870cc9
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-02-23 08:04:52 +0100

    Prune in tests

--- tests/utils/git_test.py
+++ tests/utils/git_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12fa953e0d914ebfb7199c5fa6909c5bbb429b14efe97b7def8a1b280744a6e7
-size 8645
+oid sha256:45e5e88fb97c7ad982f55f75023735023969c4f3328fd887c6306e1254517ea1
+size 8669
