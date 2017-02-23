59000a5e4aea3c2951fbd070d787341c276801e4
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-02-23 08:16:59 +0100

    Log info/warning/error even if --quiet is specified

--- esrally/utils/console.py
+++ esrally/utils/console.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74f51f3f6101ae4052a31beb736aa6ff0710cbd7069d1a1004bd692a85a78fbc
-size 4738
+oid sha256:c485b10cc0ea48ee2246e14cca23e19b02662478d0336c621f17299898219f8c
+size 4660
