8053ad1484c84b7ba628a6a47a8a04cb251ca6cf
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-02-24 12:25:05 +0100

    Allow user to force index auto-management

--- esrally/rally.py
+++ esrally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3938692bb4fab097df92dde5a284a91a7885742da19740e5dad7fc8d32bad601
-size 25280
+oid sha256:e838ae75426c4ce78723bcdd0dee6afc77a8b29a0b6be0036286981faa8cd68d
+size 25615
--- esrally/track/loader.py
+++ esrally/track/loader.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:052e679dbdf17820d914e707bc84592eb84ec744ece5322622e1290c33273c90
-size 35658
+oid sha256:7210bba5e8f9ed4b018e0a3480a5cfef675cdcf682251b937796288b194fe582
+size 36220
