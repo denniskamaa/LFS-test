9208a0c3c39186513061b550e91259580cbb19a7
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-02-28 08:19:46 +0100

    Accept expected cluster health status or better on startup

--- tests/driver/driver_test.py
+++ tests/driver/driver_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:539d4c09aa5e074a1f03b04ea3617961928bac14221f41559a0f6f82f0393b5f
-size 27855
+oid sha256:65ee7a45d4c710e0420fdcecdb7cf8c9706bee3d37c87535a32d573337b627bd
+size 30331
