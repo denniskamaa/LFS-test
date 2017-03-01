3526de2a49e7ea049a0a12f770e12ca4758507f3
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-03-01 16:21:31 +0100

    Improve logging in load driver

--- esrally/driver/driver.py
+++ esrally/driver/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81b29941be67043867412641a4be9baddd4c05b8488a132761db498674a5e940
-size 46394
+oid sha256:237d52c194e9f351b4e9d9ccf2e50dcbe5a940073e0c173d33eaab789309dd03
+size 46476
