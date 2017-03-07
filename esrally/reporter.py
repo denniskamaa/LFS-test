2f752513f2b3785552568e7a5ba18a70cc57195b
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-03-07 23:56:14 +0100

    Use correct label for stored fields in tournament

--- esrally/reporter.py
+++ esrally/reporter.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a5b286332a94f4fff8dd08f297a397e5c3aa539f9a215a9d0c0dc27a0c3668d
-size 32192
+oid sha256:c7f338fe0cde91576967371b0200e0fbb36beb3ec6ad99d926b632724b5dd33c
+size 32199
