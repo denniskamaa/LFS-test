6e683f36da7909fca3337fad296a52bbe0ef8c05
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-03-27 15:28:29 +0200

    Update py-cpuinfo to 0.2.7

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb1d8c871569021273b41b5a1aa3c1d3218e112ac11a9907c39bf4d35ae3df52
+oid sha256:2503e11526a60dc5326b57f4bad1c1ce5def2a24f5a18f48e083a24ae8a0ac91
 size 2470
