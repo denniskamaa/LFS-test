59ed0d90bdcbf2ab49e1fbd07dbf26e11aad844a
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-04-04 13:41:38 +0200

    Use scheduler_registry for custom schedules

--- esrally/track/loader.py
+++ esrally/track/loader.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f49c8b469abc0acc1b0871ac01340267fd2a7ede1e7dda563fc28a0a0027305
+oid sha256:f9d599f5c7a5c973ac7147419b0a05888d7aef22887cb991483af00c691f6abd
 size 36346
