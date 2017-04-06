7a0886f5e98e126b7826fe57e993265f640d0837
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-04-06 08:51:38 +0200

    Make Rally work in degraded mode when offline
    With this commit we consistently switch to a queue-based implementation
    of Rally internal's actor system when the (default) TCP-based
    implementation cannot be initialized (this happens when the machine is
    offline).
    
    Rally will continue to work as long it is not necessary to distribute
    it across multiple machines (which requires a TCP-based implementation).
    This will most likely be a problem in corporate environments and in
    cases where Rally has to work distributed.
    
    Closes #261

--- esrally/rally.py
+++ esrally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae09a399aa322221704b0af1e60b3ba40c8efb4863e452382085ab51cd5f3f99
-size 26018
+oid sha256:5f5dfcd2e29dd13b086d9b6b5ca526baf6491b800e490fc5f4c8c6f56594f8eb
+size 26097
