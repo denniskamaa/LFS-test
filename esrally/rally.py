54efe12d2a6d1fe4ce8aa813a292c34cee8637f8
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-04-11 09:10:04 +0200

    Write a log file per race
    With this commit we revert to the previous behavior of Rally to write
    one log file per race. Using log file rotation together with multiple
    processes is unsupported in Python and leads to race conditions. A
    self-contained log file per race is also easier to understand.
    
    Closes #263

--- esrally/rally.py
+++ esrally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f5dfcd2e29dd13b086d9b6b5ca526baf6491b800e490fc5f4c8c6f56594f8eb
-size 26097
+oid sha256:e9e47b4bdebeec5d1d1e25b2c6ec69ddc57ade784ff7400a9023a17bc9acde49
+size 26344
