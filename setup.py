3475d025ee0fb74b114cc0bc14e569d4947c29f8
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-04-13 09:09:12 +0200

    Update Elasticsearch client to 5.3.0

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1c2a2bc424700fb42422fff944d80fb975cf1cac76e6fbcb2f92d2443eafb67
+oid sha256:dee4af70d73a67cbc3290be2ddd9e6ec9933d0e2b21aef99e31adbe2249c06f9
 size 2470
