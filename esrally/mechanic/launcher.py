2c8317e3a280488e7db7571eae630c457bf1a43c
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-04-18 11:06:23 +0200

    Improve detection of ES startup failures

--- esrally/mechanic/launcher.py
+++ esrally/mechanic/launcher.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:289d856e4f9810df82d54b4ee57cb1087d39c76a4981f8ff3a88f87449be0db5
-size 18383
+oid sha256:3956a296293eb1dd9e2bb0a41d8d2cffa778c05aa7148d2ae36cdc461f5b855c
+size 18869
