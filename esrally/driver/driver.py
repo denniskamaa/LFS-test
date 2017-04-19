ed4d53294e197fa99797aa32e139a0badd5edec0
Jim Ferenczi <jim.ferenczi@elastic.co>
2017-04-19 17:11:05 +0200

    Mappings should be created on a single call when the index is created
    Mappings for a race are always known beforehand so it is possible to create the index with the final mapping rather than calling
    put_mapping right after the creation of the index.
    This change creates the full mapping and adds it to the call to create_index when the index for a race is created for the first time.
    This is required if we want to benchmark index sorting which requires to create the mapping when the index is created in order to apply the correct
    sort specification extracted from the index settings.

--- esrally/driver/driver.py
+++ esrally/driver/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e7b6de9efc09a3e2fa86ce38f7209b8fb7e93727008ce2fecf366c276f1d1aa
-size 46531
+oid sha256:92fe481dcd30dcf596ebcb86852cf4880ff4a52e6dc6e11203b51b0c2e805d77
+size 46831
--- tests/driver/driver_test.py
+++ tests/driver/driver_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c4267e214a039c5acf5cfaec481a06a52de34d3956256b10de28ef2dd96d6dd
-size 30588
+oid sha256:7474678c10cea16aa79b7a3b7ade5c1fe13fe033cf2d4d871c541007600b16d5
+size 30850
