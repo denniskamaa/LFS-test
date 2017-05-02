b49cbb7a7fb54fea3c2b7f8f37966e7117353621
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-05-02 10:12:43 +0200

    Eliminate unnecessary integration test cases

--- integration-test.sh
+++ integration-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5cd4f3e74c49796351971bd2b82e7a8780a578728bde9d7c869fbd975880d39
-size 5569
+oid sha256:ca3b374a66e5454be07addff17d869841b58dd8239c00357ff74ee66bbeeaba4
+size 4223
