aa4fa053c6f42642717166a20a7835430e8a015f
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-05-02 18:11:11 +0200

    Improve progress logging in load generator

--- esrally/driver/driver.py
+++ esrally/driver/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:92fe481dcd30dcf596ebcb86852cf4880ff4a52e6dc6e11203b51b0c2e805d77
-size 46831
+oid sha256:1dd7c4849be97e6255210dba43f4fdf93d5f07c3e4959654c8fa95b75d2554d1
+size 47939
