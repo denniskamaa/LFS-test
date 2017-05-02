76dd564c544c500c6f7cb213d7565896ca7a7f4b
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-05-02 15:12:27 +0200

    Add additional checks for detailed stats in bulk runner

--- esrally/driver/runner.py
+++ esrally/driver/runner.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e263d7540b355ed2242c6d3cdebe68f751f55501a26cfe499c637c57962b57e3
-size 16607
+oid sha256:5dc7d418c66fb5ae4f6594879950ddd691e73bddd110ed0f72bb85f0825c68e3
+size 16734
