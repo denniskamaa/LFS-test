c34503b7ded01e8c81e85ccb14c88ac63d27c097
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-05-16 12:24:41 +0200

    Store ES major version as dedicated result field
    Relates #283

--- esrally/metrics.py
+++ esrally/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14e0f2b1fc43ff9d11d31814cfb603d3bde65505a86cc71cd50ccc563d2dca85
-size 54245
+oid sha256:1528df6a319c16ec2c142354b17561b8816faba70acfac32e7e87db508ad581f
+size 54356
