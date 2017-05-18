9126e14c96ea2be2dda42efcccafb60eaf18077c
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-05-18 08:01:49 +0200

    Wait for shutdown of local actor system

--- esrally/rally.py
+++ esrally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0b80bb288fa5a360a11fd57c0319a76c4fcddc11f33cd3af2e7cffa685835c3
-size 26697
+oid sha256:fa0d919beef22bfa30936ad04ecdcf09bd848bf5ad508736ceeb7a6771305031
+size 27229
