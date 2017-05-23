29fe884a10d47052e66d8316b75b54f9197caee7
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-05-23 14:14:24 +0200

    Exit race immediately if a load generator dies

--- esrally/driver/driver.py
+++ esrally/driver/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fdcf1cbacbf6a9f444082dd284a635bc1e2b9e2f8a4e2c0cc090db3f4d9113e8
-size 48071
+oid sha256:f1f9a6c6e68c865060e43c4f1440bd194eb240fba6ab38a922671e134a483cc8
+size 48792
