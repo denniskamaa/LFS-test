18d4429eba1dac879c1d90e7e538b7041d5545ab
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-05-23 12:02:13 +0200

    Improve logging

--- esrally/driver/driver.py
+++ esrally/driver/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ceee77761711f0efff613838151b785e00e9041f43728fd4de4fd496b8a2333
-size 47979
+oid sha256:fdcf1cbacbf6a9f444082dd284a635bc1e2b9e2f8a4e2c0cc090db3f4d9113e8
+size 48071
