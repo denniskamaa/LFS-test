4b3f7037ff3d2881c6c8d2fe37165a3ce4fdc3a0
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-05-24 15:26:20 +0200

    Use only ascii-safe characters in setup

--- esrally/config.py
+++ esrally/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4386fcebc3313ea0ac7eab348b1267f00fcb8756eb250558dee781a59c36de63
-size 25239
+oid sha256:b476eb927967b5c1f83af5cc74ae284a327a9699667e408718c40dc6ed4c3e4c
+size 25226
