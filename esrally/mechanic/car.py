c18ae48a79ca103849ee865ef633d62303d8c875
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-05-26 16:15:02 +0200

    Temporarily add a 20g car

--- esrally/mechanic/car.py
+++ esrally/mechanic/car.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dfc56d1663baa4aba2d6b4f64352a457776b852ed5f5cd6fdb3c2c351460e269
-size 12666
+oid sha256:6fa4d8f6911f1c7af44c6ee240fb9fc1fe0ff117b6d0a64486ec481eb807faa3
+size 12703
