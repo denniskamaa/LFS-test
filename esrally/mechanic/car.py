96ef4830df9bd7ab521e251451991e094e0f1968
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-06-19 09:16:21 +0200

    Add 1g car

--- esrally/mechanic/car.py
+++ esrally/mechanic/car.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6fa4d8f6911f1c7af44c6ee240fb9fc1fe0ff117b6d0a64486ec481eb807faa3
-size 12703
+oid sha256:b049df6f5cae6b238c64e9e75c3f33d9d43511d3d44ee64a5b0e326f1a74aac0
+size 12701
