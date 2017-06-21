5a45c0a0c35b0a9ce092493a9b99b09b46450955
Christian Dahlqvist <christian@elastic.co>
2017-06-21 15:08:05 +0100

    Moved bulk size stats to detailed_stats

--- esrally/driver/runner.py
+++ esrally/driver/runner.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c47b2b27800612f7c316228857e6822976ff8cbabd742a55ded25c4f4de506c8
-size 18879
+oid sha256:4a166f59765b50845adc17c4eee36ca2f3553e43765603580eb2eaeb82743b0a
+size 18922
