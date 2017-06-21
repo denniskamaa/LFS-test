882ab016a54cfc55dbb0f55ebd51cf1d036fdb05
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-06-21 10:59:07 +0300

    Remove list facts subcommand
    Closes #282

--- esrally/facts.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5f21371f3b6bb5f49039a1f163f74cae5f393b60921171f3c7e92cade1619505
-size 2819
--- esrally/rally.py
+++ esrally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f38ba8221a0653b2332662d569067741b7a0b23942ca6f1afba19d3b61e9c10
-size 27460
+oid sha256:50ef617a2b82c45d88e312bb45c167e5146f5d2a0ef9f69fef8c2ece5e33ba1f
+size 27083
