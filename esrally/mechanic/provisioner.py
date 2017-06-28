f5ca90d3c1a7ea332602d48bfcca445d8d5348c6
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-06-28 11:30:42 +0200

    Implement docker provisioner

--- esrally/mechanic/provisioner.py
+++ esrally/mechanic/provisioner.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d11942bbd1fc0c9cecb2b1618db9d348ae82017200545b3371092bd580358736
-size 8921
+oid sha256:3cc895d0d854cf2e31812ca0729ba9dca0d3d9ed2959f08b7630584e0298477f
+size 10430
