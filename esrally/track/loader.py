52358f1254d056be744ad399315fddec4b71aa87
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-06-29 10:26:58 +0200

    Show number of documents when listing tracks
    Closes #291

--- esrally/track/loader.py
+++ esrally/track/loader.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0cfee1452256f255ee9f54b1042c9c24e5abc621d20188b6a46398b8dfd2a900
-size 35525
+oid sha256:dcb9b270fb05ad7332cadb0e8dcebfed66659f7e657063b52f978dff47d96153
+size 35593
