8af454e270c77020bf3aebbca0cb20350715a09d
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-07-04 16:20:50 +0200

    Show track file sizes when listing tracks
    Relates #291

--- esrally/track/loader.py
+++ esrally/track/loader.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dcb9b270fb05ad7332cadb0e8dcebfed66659f7e657063b52f978dff47d96153
-size 35593
+oid sha256:6b98aee9c21642d111aa33e8f695a55a3fd800e2870e33ea8334172aa8757997
+size 35769
