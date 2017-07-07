369cfbf8223b80bb41d1822fd0bea2460146f7b1
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-07-07 10:32:08 +0200

    Keep cancellation state if racecontrol has detected it
    With this commit we immediately cancel the benchmark if cancellation is
    detected in racecontrol (coordinator) instead of first forwarding the
    request to the driver and relying that it will respond correctly. With
    this change, Rally behaves more straightforward during cancellation even
    in situations where there is a problem with the driver.
    
    Relates #298

--- esrally/racecontrol.py
+++ esrally/racecontrol.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd33a3f39c6aafe993b3b98f210912fc213ff29501bbcc3642aaa7e6f31aeb7b
-size 15110
+oid sha256:f41436783f0dca083f127ae7fbf6dd69145e1ac91dc274c75090e221662a45a5
+size 15234
