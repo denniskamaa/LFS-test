dff353f2f764c2794fb73f503eeef484ea9b2c3a
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-07-10 13:26:10 +0200

    Slightly improve logging if ES process cannot be started

--- esrally/mechanic/launcher.py
+++ esrally/mechanic/launcher.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e894f21d06abecfeb1c5660c871fe6cb1d68c8d57c4e185b75a8cb40c93538b
-size 19820
+oid sha256:5a28acc20fe4024b7668fab16815904ec46d58bcb4c847d4458489dc7204ae7b
+size 20159
