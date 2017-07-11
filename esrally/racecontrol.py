22487936a13b2e5583e6285fb56981cb5c5ab7df
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-07-11 15:07:54 +0200

    Add ability to ignore unknown return values from mechanic

--- esrally/racecontrol.py
+++ esrally/racecontrol.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7536c448b506da53aae15d78a4478d80b2b0dc4644f5c59c8cafd5ddf126545c
-size 15482
+oid sha256:3215f66ec8bd0335922dd92d66aee3a642c67221ec1c968dc76b90f814279a46
+size 15754
--- esrally/rally.py
+++ esrally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50ef617a2b82c45d88e312bb45c167e5146f5d2a0ef9f69fef8c2ece5e33ba1f
-size 27083
+oid sha256:a2bedf524739db7a9438d81fc7864269c56ca3d3199454261e06a22fd7ce507d
+size 27419
