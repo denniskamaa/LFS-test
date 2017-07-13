17b84bdcde54df7b6578b2d28bd6fca392ca486a
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-07-13 14:49:38 +0200

    Add missing entry to DockerProvisionerTests

--- tests/mechanic/provisioner_test.py
+++ tests/mechanic/provisioner_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ea7e7c0219afddd00ebf83a3711365b4ee316ef5d3b9f459a473137cbc824ab
-size 6021
+oid sha256:7171920f6dda3b703c6ca30e347d8d5f1284320cb524740b8d6519fc34ec0779
+size 6072
