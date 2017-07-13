a07981ee56732bd0b72ff00e489aae422ef3aaab
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-07-13 14:32:36 +0200

    Don't fail benchmark if cleanup fails

--- esrally/mechanic/provisioner.py
+++ esrally/mechanic/provisioner.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c13717218cdc791aaae404fd37001e95a1206ee15bbeaac97770397fc4b5d7cc
-size 12300
+oid sha256:1f6288f29c04fe6dc076d63d5580ccc2cc5ebf584738c9535857fd7c2f61fa5c
+size 12602
