ea47ed6a17eccd0c22e73729dc428972e636aca8
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-07-18 14:14:16 +0200

    Separate Driver/DriverActor to make it easier to test

--- esrally/driver/driver.py
+++ esrally/driver/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac6272b8e72a783baeb30ba0f47c5e80be221e25e2ff041bc99f090a78611da1
-size 54583
+oid sha256:587982bb909ea38e70f25f6e1777c8dce5c2c6570367fedc32ae37ea3cb6b245
+size 56129
