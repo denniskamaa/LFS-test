76b8197f4bf1e4a406455abe0e070751acc9a7ee
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-07-26 13:25:57 +0200

    Add 'list elasticsearch-plugins' to integration tests

--- integration-test.sh
+++ integration-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d985217d02dac8158b9cf0208d7859159e4f4051b2315e4c85ff23600ae077b6
-size 4777
+oid sha256:1a4a2245fd9df39fdcff5251f611cdf57f0b068e942e144c8d2a2f8249965f2b
+size 4898
