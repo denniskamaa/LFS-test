217fc58c0914ea03e5f1314d8196879491ebd106
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-07-28 11:16:33 +0200

    Check that Rally is used with a supported Python version
    Closes #307

--- esrally/__init__.py
+++ esrally/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4382854b6e05fa66d4a21fc8812a33223a94ab40d1ae138bf3d8f09c455d35e4
-size 1526
+oid sha256:2bfe80fcfa9c13c6f3d8d27c80fd35388c6ee452cf772424758470353a0e16a6
+size 1739
--- esrally/rallyd.py
+++ esrally/rallyd.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f37ff1c181941315196e5ca135effa6b35a9f1cf057e0ea3354aa3d10e5cb377
-size 3767
+oid sha256:3afc1f13b7864f23299e56346b6732bb798d387b54f0601a94754b4493a6aa89
+size 3816
