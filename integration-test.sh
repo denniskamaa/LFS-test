29e2c3f1b94e8b8aba107dcdeaf209bfbc56167e
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-08-03 09:39:45 +0200

    Portable in-place file edits in integration tests

--- integration-test.sh
+++ integration-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2eccf7b9cb903e0142c65ca3eb633e16efba8fbb9a896f3a691ea1e39575a4ea
-size 5640
+oid sha256:bb732e2f72957b8bcdc38e14c75d3db879efff28da0e1d1bf2bb8c97fd8dfb38
+size 5748
