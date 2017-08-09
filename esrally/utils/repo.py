cd8b11615765ddf2617159562138fb3c515571f0
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-08-09 10:47:44 +0200

    Fix wrong parameter order in repo warning message

--- esrally/utils/repo.py
+++ esrally/utils/repo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ff8f0caa08d388df450933dbd5b490025ffa008d7ff3e5811576df62e7fc9d8
+oid sha256:4f376b6ecf8891bbfc084f995f8f97338ce85a508633874bbd22f6a46efa2006
 size 3588
