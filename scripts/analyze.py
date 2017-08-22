052a17c9007c60d3931c1ba27e6fd0fd46f18c20
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-08-22 17:43:41 +0200

    Allow to provide label key as command line parameter

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6bb33ef93d51d47a6f30b55949ea0bf12b5fd5fdea805acc4bb3a7e37b265f2
-size 5643
+oid sha256:6befb9d96f9fd4b4cdfeb0ce970d0f846bf612636ec27c358f82cb860d41d55c
+size 6408
