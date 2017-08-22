ad5c6515299b4694d2cef9589326288308a037cd
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-08-22 11:42:35 +0200

    Remove spill_to_disk also in tests

--- tests/metrics_test.py
+++ tests/metrics_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3b954a58631cff29254d602bd37aeae5e2d708035ff40a5515e3528d094fe8f
-size 39911
+oid sha256:49e86b9e9ac4c487504d828ac30af21d549b6a7db25586fe9a4d8cc05e08e983
+size 39833
