82a5cba80c7984a2b6bc23c433379dcab6296d7a
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-08-22 14:02:37 +0200

    Fail fast if we cannot determine the distribution version

--- esrally/mechanic/mechanic.py
+++ esrally/mechanic/mechanic.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe86d8308039341480daf3d0da689603434aa12c7aa1e2d9aa5005c2ef5a6e14
-size 27867
+oid sha256:4ca1ba701f73fc873a8a6e2c41b5cbcc227d4008396db64a3a110d4327964dc3
+size 27731
