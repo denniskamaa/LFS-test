31cc2d479e81ae6b97c477ec9af9cca33781f000
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-08-24 09:43:41 +0200

    Allow to skip indices and templates for search-only tracks

--- esrally/track/loader.py
+++ esrally/track/loader.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31643b23fe220b90de4eb86b4377becc44a670dffe609bc734af323adf26c4c6
-size 34680
+oid sha256:2b548a88aed84edf69354d1aec7d865e0ccc53f65d1817ab2b722ff5e6c72d4a
+size 34745
