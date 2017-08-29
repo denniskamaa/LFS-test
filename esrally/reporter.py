efc3f9318bf6b2868cf255b367869aa32b14dc56
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-08-29 13:54:53 +0200

    Guard against missing values in throughput formatting

--- esrally/reporter.py
+++ esrally/reporter.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0bf026f113334db46b0beef7d39e25a7b371d08effc0c3670e7938bce4748585
-size 37990
+oid sha256:f3ab5f803302d52c667db4fa835b0118aa1958668849e6624970e33daa1dfe5d
+size 38110
