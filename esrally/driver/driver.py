3c431dbac9028040f60fa0860cabc2b921fe9606
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-09-14 14:49:29 +0200

    Upgrade to thespian 3.8.0
    With this commit we upgrade to thespian 3.8.0 and replace a workaround
    to use multiple timers per actor with the now available proper solution.
    
    Closes #318

--- esrally/driver/driver.py
+++ esrally/driver/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e750e680eadf12301c873d5aceb57aafa03adee440e0f0137c0c0ed7c45bd24
-size 62620
+oid sha256:771f30dbd979f7a2b8585da932eb0d6cc57abfa523e9e4b94dcf876681b71396
+size 62316
