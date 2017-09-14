429bbdc4c97c49ad3d29a53b5ad432c9ad6f330a
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-09-14 14:52:05 +0200

    Get Docker node log dir from node config

--- esrally/mechanic/launcher.py
+++ esrally/mechanic/launcher.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b489e4c1e283bca2d0630d3cb0bcf4ad0c2630c2fe883789b8ea8aa3f854b699
-size 16275
+oid sha256:8baab96a841676657cb0272b78ecdaeb0057ad10e63a624c43bd477f650c5585
+size 16264
