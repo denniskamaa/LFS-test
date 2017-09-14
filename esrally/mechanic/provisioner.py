909bfd6ff4c33cbecc1c1ce508a88b2f34d31e6d
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-09-14 15:58:50 +0200

    Disable unused features in Docker provisioner

--- esrally/mechanic/provisioner.py
+++ esrally/mechanic/provisioner.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76e11a2b062ed8b0198598480949d9c540ee90c95d286680d9edd64ce006a33b
-size 20724
+oid sha256:e587c937bb1fb52fb0120ef34ca4d4d6d394007a530e80572f0e6595dbb59e88
+size 21019
