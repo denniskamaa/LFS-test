8d03165ff318eacb72faacb890154d133bf591f0
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-09-18 13:09:39 +0200

    Don't log body when creating a new index
    Closes #321

--- esrally/driver/driver.py
+++ esrally/driver/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:879fc248b2379e088e0cbdf6ff2c50fff40dc65c2cfcc1d83e755fec5f1ec6a2
-size 61955
+oid sha256:001cbb0f80cce480b638d0c0a9673a613518f41bf671957efbff4cf9b69ef5f9
+size 61945
