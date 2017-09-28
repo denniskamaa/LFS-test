1cb3a30df379f9af6bb12a471e715797e6a5390f
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-09-28 12:58:31 +0200

    Formatting fixes

--- esrally/mechanic/telemetry.py
+++ esrally/mechanic/telemetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68e0ef94c2fa90312dd829516d0ee68cfa4e3074a19efda305547afe4fc92c71
-size 32379
+oid sha256:453c75bd79dd2efaf90f5486b743875df464ff25c7d4444cd7231e32b39d09cc
+size 32373
