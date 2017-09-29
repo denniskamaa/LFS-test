200f8fc73d24e5879254ece905f4844e3b7dd8c5
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-09-29 10:15:46 +0200

    Allow child actors of mechanic to exit also after cluster has stopped

--- esrally/mechanic/mechanic.py
+++ esrally/mechanic/mechanic.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7e649ec9332103c47b607c411ca1e49804675dfe293f64a13af7093d6cbbb53
-size 27308
+oid sha256:1e687ff362497f7fdfd6b9cf305c2752687edee8a389732a1611a8ee84826ff2
+size 27340
