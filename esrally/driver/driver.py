57f03cff04194c876bc14edc0f6482a094ba04fa
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-10-05 12:47:34 +0200

    Catch BaseException in all actors

--- esrally/driver/driver.py
+++ esrally/driver/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c82e00cc1855bf1509f4188001f013b9941a4bf41a0a493428379d1b62b2d1ed
-size 62616
+oid sha256:0e750e680eadf12301c873d5aceb57aafa03adee440e0f0137c0c0ed7c45bd24
+size 62620
