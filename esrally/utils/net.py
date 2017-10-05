471b2d6f64240f43f49d4fa95420b01e54f144f6
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-10-05 12:56:01 +0200

    Revert "More robust DNS resolution"
    This reverts commit 5e3c9e8c70e5809cf295fd2b3cca63a87c8c3cc8.

--- esrally/utils/net.py
+++ esrally/utils/net.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:550674497c8bf81ce4b787b7e6722cc873f634adff63aaa360cadbf13b468684
-size 5313
+oid sha256:16b6d52643ea34386f75c65eb40e71d1997122c82b3c4e2e7af35cdba9bbb275
+size 5437
