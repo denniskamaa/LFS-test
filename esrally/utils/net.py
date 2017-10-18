d4a30d013f4ccdac1db3f46631ecc40710b241bf
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-10-18 16:17:58 +0200

    Check HTTP response status when downloading
    Closes #333

--- esrally/utils/net.py
+++ esrally/utils/net.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16b6d52643ea34386f75c65eb40e71d1997122c82b3c4e2e7af35cdba9bbb275
-size 5437
+oid sha256:36b27e3a3a3a8a2983b6fcd09450b8b909153a8c2a4fccd5404678c6e8b0db0a
+size 5564
