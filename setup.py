c106af71c34a9b115904e4479217553d762d7109
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-10-26 16:19:22 +0200

    Upgrade to psutil 5.4.0
    Closes #336

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62a247ee16797016ad4cefcbb89dc596ecc45d93137cb8c8fd01d925623e2e83
+oid sha256:7b10785e7a8f51ca97d69e2d8aba49692af2b5052afe3d07d5b2fa27fe4709f8
 size 2470
