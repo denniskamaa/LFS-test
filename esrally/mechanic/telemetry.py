c2e141cf11b3a6e71ecd7b8e4ad0191324a5231e
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-10-26 15:40:04 +0200

    Silence CPU sampling errors on shutdown

--- esrally/mechanic/telemetry.py
+++ esrally/mechanic/telemetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:453c75bd79dd2efaf90f5486b743875df464ff25c7d4444cd7231e32b39d09cc
-size 32373
+oid sha256:85fc3541013941f1ec92b1ab7dc025b64289a175dbed3ea535f6e2a75e0e8b4e
+size 32572
