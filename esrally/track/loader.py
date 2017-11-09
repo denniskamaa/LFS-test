443536fb861d71e6165e5fc2f20834dff9887ec0
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-11-09 13:26:44 +0100

    Allow to define operations inline
    Closes #326

--- esrally/track/loader.py
+++ esrally/track/loader.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:06001da1dbb3e64b49fea97be8fdaff19087d002721e964f8caeca350e100511
-size 39268
+oid sha256:bf788f724a2548edad6c42fc5fd7ec197dabec1cad754c77bcd71da6d47c6395
+size 39761
--- tests/track/loader_test.py
+++ tests/track/loader_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:510fde149cc32a61cf9ba7aaefac1f6a3d523b458b73f87f1dd2386a6b7b71b1
-size 69436
+oid sha256:aa42f921ea42cd30a41b220868a3934c483eb025788d786d66e3f6382607e6f4
+size 70950
