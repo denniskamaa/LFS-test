10e0eaec03843896c067b47351a6798acd218947
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-11-10 13:34:47 +0100

    Raise SystemSetupError for invalid track paths

--- esrally/track/loader.py
+++ esrally/track/loader.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ffe98893c58bf4635de056ae34762fdc086d06b41f0d92b81dcaff9cffaba175
-size 40599
+oid sha256:e46a6adeffcb664b60bc2dd5a05c2a4af13ec518497d1ea3c5f659a1b0e702c5
+size 40619
--- tests/track/loader_test.py
+++ tests/track/loader_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84bf3324d6c2368f9f355687a0a0bb60dd21a32048c2b629b35ed696ed33e523
-size 71101
+oid sha256:7858141f8f4cf9f44ef17c14d04ae37e71f165d6428d794cec0deb707c56109f
+size 71121
