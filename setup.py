d4793c06264cc57fa04e55b0713f2ff70d83bcd0
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-11-10 16:15:44 +0100

    Upgrade to elasticsearch-py 5.5.0
    Closes #357

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a4597406a75b38fd3400e78a05e734f2ee4c31c3c99bed3ec1dba35f754f627
+oid sha256:044ae8917099cbb81e578bb2445249483e81d996f8a46de61a5b769953adfcab
 size 2825
