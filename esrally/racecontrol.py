f5ba61de612caaedf6486d6c442cc35bcc873376
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-11-13 15:40:00 +0100

    Show ES distribution version upon race start
    Closes #360

--- esrally/racecontrol.py
+++ esrally/racecontrol.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a7ea2e56cb403dab22b52213aa174984f84cebf9849498188385b443c85b1bf
-size 18180
+oid sha256:0c23f4defa522e38228b68a894fea960c5b9ab581dc24155b7f2a0b606c0e2a5
+size 18239
