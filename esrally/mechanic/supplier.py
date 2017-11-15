d00c343c89c5b4944d5791177f46d69ca7fca6ff
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-11-15 10:35:34 +0100

    Make plugin remote URL optional
    Closes #362

--- esrally/mechanic/supplier.py
+++ esrally/mechanic/supplier.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48dcf5f5e1e26859c3ba565dff3bbd143865ecdf4cc893dbec3c026e5ead2748
-size 13830
+oid sha256:a0820b3261b0c048a529caa1bddde5203213d0323503483f9bcad8bab35a55c2
+size 15120
--- tests/mechanic/supplier_test.py
+++ tests/mechanic/supplier_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82ee3aceeb797ab4794ccbc6d4c5bde52d65d7c33d52ee5d2ec4b91b3cd0a173
-size 10526
+oid sha256:59f52e5a5fd29009c2a9bf84634145ea3d08c1ff7fd07cc1ff03de971e72c4bf
+size 11522
