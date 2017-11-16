e3ec9cc1ed4262c9f4bc07cde2535c79a826fbf5
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-11-16 13:05:07 +0100

    Retry metrics store operation on gateway timeout

--- tests/metrics_test.py
+++ tests/metrics_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3aa58a48e2f015274ad06347c0b083395d6c1206b8104a4abf3cd2b4886f69a
-size 40471
+oid sha256:ee4eb0e83dc015f464394aded9ff0a0be9c2aa57b104e06615afb720837a84ea
+size 41641
