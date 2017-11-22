1f8f593b79bf87beea4aafeab616cc5592b7e926
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-11-22 13:18:59 +0100

    Allow shorter revision syntax for plugins too
    Relates #345

--- esrally/mechanic/supplier.py
+++ esrally/mechanic/supplier.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28543475d6239778defeef7b507bd66b0d1acd5f59fc7156e4f55f460b31ca1b
-size 24228
+oid sha256:d22d250cd684355921dab88378609be7c3e8afb07900493b40e75a6c0a5b26cf
+size 24154
