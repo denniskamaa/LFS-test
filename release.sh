8c4bbe4f060993d58b64df8985bd509577dc7706
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-11-23 08:56:53 +0100

    First clean on release, simplify docs build

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d67fca623bfc6552903d0feb31ff8581e629d9628ffe03231aaebcc2107db5fe
-size 2720
+oid sha256:2c09dc0c0b76f1630edc07f1fd5719b80083744dc4684fe116cfea7f32cfc154
+size 2734
