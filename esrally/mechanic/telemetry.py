95bdb39b316ea26229204da23dfe9b26f72abf59
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-11-29 13:47:37 +0100

    Correct index time calculation for multiple laps
    With this commit we correctly determine the indexing times related
    statistics even when running for multiple laps. Previously we determined
    the values at start and end and subtracted them. The problem with this
    approach is that right after we got the value at the start, Rally
    recreated the test indices thus resetting all indexing times stats to
    zero. Then we subtracted the end value from the high value from the
    start and constantly got a negative value.
    
    Now we skip this subtraction and warn the user instead about a
    inconsistent cluster state at the beginning of the benchmark if these
    values are not zero.

--- esrally/mechanic/telemetry.py
+++ esrally/mechanic/telemetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85fc3541013941f1ec92b1ab7dc025b64289a175dbed3ea535f6e2a75e0e8b4e
-size 32572
+oid sha256:507acbd800c9b8a7e612875a8857494a903197a562bf8cd253be887daab86860
+size 33010
