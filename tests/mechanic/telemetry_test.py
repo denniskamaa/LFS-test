b9aeacd1a1fae8746a394cf5d0bcbbffc30e22de
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-12-01 14:59:59 +0100

    Fix test_index_stats_are_per_lap

--- tests/mechanic/telemetry_test.py
+++ tests/mechanic/telemetry_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aab6e8795b6f9d269d5d893c44e8d5edfa89ad47cedddc87d9b72bff277fff33
-size 37322
+oid sha256:a19bf48091ad4e800ebb32ef3a4f636ee053ab6c5d07f5f88758360c6dd5bf2d
+size 37324
