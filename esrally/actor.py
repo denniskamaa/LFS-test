d1802cff0e503ef095377275c524087358bcc999
Kevin Quick <quick@sparq.org>
2017-12-03 01:13:04 -0700

    Propagate init arguments to base classes.

--- esrally/actor.py
+++ esrally/actor.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ee4692d7eb619e62f1e9e27239a8fa3a71a5dde57d25d906d3d428149fd057b
-size 10763
+oid sha256:f95b9de70ac57f777b9369e80962c18e6b6e1b9f6f89a5dd960978c1991adb76
+size 10787
