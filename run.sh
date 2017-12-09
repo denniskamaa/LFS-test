d1bcf898331c6c3bfc97005c2486d5fcd9a52488
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-12-09 17:27:50 +0100

    Call correct binary in virtualenv

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae81db08c2fcfdd0f528093c6a023a73f95ee9a835ed65cfc86bf9a4db2b386f
-size 3486
+oid sha256:306a76ac1c2abdd7404a2c25b425ad85a25d806b56d6155bbf81c0c2d4819c8d
+size 3574
