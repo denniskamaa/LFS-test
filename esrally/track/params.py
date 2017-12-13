1bf5655e68c7018c7c2b230830d9a32c2654286c
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-12-13 12:52:23 +0100

    Add a hint to the migration guide in deprecation warnings.

--- esrally/track/params.py
+++ esrally/track/params.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7bca4d46fbaac201ffebbe9e1f16b0fdae24c85f82067abcedf839b5c08ed87c
-size 33681
+oid sha256:9761a96a13e0308f0e0b0e83538c32c43601a030dbcad38ea828f6063a73da4a
+size 34146
