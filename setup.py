c90ec0bbc1bfcb77a31dd707b4988b00e533e4e2
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-12-15 12:59:57 +0100

    Upgrade to thespian 3.8.3

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:044ae8917099cbb81e578bb2445249483e81d996f8a46de61a5b769953adfcab
+oid sha256:7789ffd2ebcceca1ae46683d26e8a97182d2966d2352489742b0658b71caafde
 size 2825
