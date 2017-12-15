42e5fd8bf85052d03b473ce4f186001888f77400
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-12-15 12:40:09 +0100

    Kill lingering Rally processes before each test

--- integration-test.sh
+++ integration-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6142567aafa7ecffcd5b65739742892c803fe46953916c2a91b5607b6d37250
-size 5994
+oid sha256:4be1edfc9ae58a29b1f86788b5b0d481ac29d4b2b0a35af3b3984e37e8572fd2
+size 6297
