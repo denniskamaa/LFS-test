fc7102b75f65aa710a47962a167fc5c7d97db69c
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-12-15 08:15:57 +0100

    Fix TrackPostProcessingTests

--- tests/track/loader_test.py
+++ tests/track/loader_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6417e8b7f85b99b6d457ef895ccee319f5302a338424665c7d5f7cecfd8825c4
-size 79416
+oid sha256:2d4c6b00e675b9331d49d8e0506ba2f11e8b92b9185187c238701bf4b833a778
+size 79398
