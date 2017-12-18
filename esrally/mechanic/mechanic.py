8e9293a62990b333f668a168480bf83c1dfd973c
Kevin Quick <quick@sparq.org>
2017-12-17 22:53:25 -0700

    Type-specific entrypoints in MechanicActor.
    Uses the receiveMsg_TYPE entrypoint for message handling to improve
    clarity and readability for the overall sequence of events.
    
    Removed handling of BaseException: this was viewed as too broad.
    Also, this does not send back BenchmarkFailure to the RaceControl
    actor if a communication with a child results in an exception or there
    is a similar problem communicating with a child: these failures will
    be more explicitly identified, and the RaceControl will handle
    PoisonMessage and ChildActorExited as the generic failures, so this
    did not need to be identified here as well.

--- esrally/mechanic/mechanic.py
+++ esrally/mechanic/mechanic.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b04390e32c5c2d747a64d02cfdc07ebe264ad1fb088d2896237a9097e145e460
-size 27745
+oid sha256:4f80381e7863ccb632b1f7c950fd90ff9d91711ff499ed73e7344fb3239d0703
+size 26608
