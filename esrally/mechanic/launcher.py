2fdbd90c9224ede4ff59a167bb1f99fbee1f7567
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2017-12-18 09:11:04 +0100

    Wait until process termination on launch errors

--- esrally/mechanic/launcher.py
+++ esrally/mechanic/launcher.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e41f2551c8416404612bc906f93278bf3dc49462e0c2c7bca24d6a2a3b46f22
-size 17176
+oid sha256:39720ec6373ded4b4b4c5ce0b6fd9c7cde3e57db5b7f0c70f7a018f86bbc23d9
+size 17482
