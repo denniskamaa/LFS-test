5cbb3e4ae035039f47b111f2aaa9e9abdd2bc15b
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-01-16 16:42:31 +0100

    Also use data directory when --track-path is given
    Closes #348

--- tests/track/loader_test.py
+++ tests/track/loader_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93772092b513abb9b635add6542c7ab01f907d66885c12cf1124094426fff331
-size 84744
+oid sha256:8fe227da9da364dcf5aa64e39b7ebb37452fea61ddaba28ec12e19510f09feb4
+size 90934
