5e557ce987db74606b359191aee4046094067e54
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-01-16 15:01:16 +0100

    Initialize team repo only when needed
    Closes #396

--- esrally/mechanic/mechanic.py
+++ esrally/mechanic/mechanic.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb3860f4ed614638120e80b00df26f0429c0400e53bb4cfe687a0093fe46bd24
-size 26047
+oid sha256:dc30ae1f83afa55b17bfb10c7a084f10cf7d9519c9e140d2012c81522ecfcb52
+size 26051
