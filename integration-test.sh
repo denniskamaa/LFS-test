a0c968f739d20227650b460bd2723debbfedeff9
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-01-18 08:21:34 +0100

    Kill ES in integration test only if started

--- integration-test.sh
+++ integration-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4be1edfc9ae58a29b1f86788b5b0d481ac29d4b2b0a35af3b3984e37e8572fd2
-size 6297
+oid sha256:840a61653289bad4b1a73a2fcbe9a8572ff503d3a4152ac5b37c9fb79b799140
+size 6345
