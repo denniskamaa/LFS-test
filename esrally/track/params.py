dfec3b4ca2db8d7be1434af7f62f52e7ffd4faac
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-01-19 08:34:10 +0100

    Add usage example for body-params

--- esrally/track/params.py
+++ esrally/track/params.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b14ff67a8349753b10293ec25bf23714653839747991630aba495206aff5ce32
-size 34774
+oid sha256:8a564addefffcee2043ed7c0f09d02ac4146ce493a142407b329ef019a6a3ec9
+size 35705
