c0098b640c3382c8dacfeb447968828f885f73aa
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-02-02 14:31:01 +0100

    Prevent thespian resending messages on launch errors

--- esrally/mechanic/mechanic.py
+++ esrally/mechanic/mechanic.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01048c90c75fb0f00943909307e578c11b1c3f5561c0f5c2396d220e5f8170fb
-size 27339
+oid sha256:bdd12ce052997504c795ba48135a7cd954f5079baebbd0e3bc2ea4c719b9441b
+size 27847
