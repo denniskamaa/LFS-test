aea88ec030b7310ad1ba3f31ce75be822397a90c
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-02-02 15:14:09 +0100

    Cleanup logging

--- esrally/actor.py
+++ esrally/actor.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9841b09b883614523d36df719b36a8ffe9ad565b754d917d26c8fbd376259a9
-size 10815
+oid sha256:37369c9bcd3dec82680d06e3e3ff6bdee1ff3a538fdd5a42bfc848a26dc183bc
+size 10917
