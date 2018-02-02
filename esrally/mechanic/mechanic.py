a731b5dfde62d350a909b5160b300139907fbcb3
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-02-02 16:24:33 +0100

    Fix node 'startup' for benchmark-only

--- esrally/mechanic/mechanic.py
+++ esrally/mechanic/mechanic.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bdd12ce052997504c795ba48135a7cd954f5079baebbd0e3bc2ea4c719b9441b
-size 27847
+oid sha256:1fdcb2905b6c9c732309831e1fddf3695fe88669088e19407da7bcf73a85238a
+size 27867
