36add066d1e6046f6a8e53981f8189e9b688544f
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-02-05 13:56:50 +0100

    Create all integration test configs in setup
    Previously we have only created the ES config in the integration test's
    setup routine and have relied on `test_configure` to provide the
    in-memory configuration.
    
    However, this leads to problems when we need to override `JAVA_HOME` and
    `RUNTIME_JAVA_HOME`. Hence, we now create all configurations in the
    setup routine.

--- integration-test.sh
+++ integration-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:36f0a24a835e20ea4385c559dbee482afa9f035ee35fb7dcaacc50bcfff82133
-size 6796
+oid sha256:2834563fe11460647ed808a041b3c048f1908c955d3e67881ae5e2f7817aa405
+size 7341
