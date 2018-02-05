cd504731c4dad03c8b4e706672f96f52129b792d
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-02-05 13:14:29 +0100

    Store Rally version in results meta-data
    Closes #411

--- tests/metrics_test.py
+++ tests/metrics_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b2894d33224c82b4f22a64875a85cd44f57ec010522a0b206d12a766feb027d
-size 42641
+oid sha256:89272455afb6bb9581f294ae2fa2c291cbdbb797474edf4dca00988d9b977985
+size 42767
