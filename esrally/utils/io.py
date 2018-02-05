d566474f7af292e70c708064fb37ba3f1676e808
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-02-05 08:11:53 +0100

    Document issues to check for Python 3.7

--- esrally/utils/io.py
+++ esrally/utils/io.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7324c769bbe65421dc5b895c205f6fa33d9a806554cac32f0b6d5cc8c72a1d1a
-size 14847
+oid sha256:60848e74de8ef26a35a157bffd65efbe31237da6aa8049093926aa746a604cc5
+size 14981
