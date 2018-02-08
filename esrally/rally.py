0055b51d2846d3463e2bffb7b45551aee5e7d56b
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-02-08 13:41:03 +0100

    Remove unused code

--- esrally/rally.py
+++ esrally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80a3e8c7a6a389a621e4cd292b2296f7822d50f3ebcd6b105f375fc14b2cfa91
-size 36211
+oid sha256:cb87c634a4e372329aef16010426517d66691df35efbcdf416f904546e7b5633
+size 35953
