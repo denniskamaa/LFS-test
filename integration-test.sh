a34197d1d581f3a17b8c8a4752113ae1d1452800
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-02-13 11:02:32 +0100

    Print integration test config to stdout

--- integration-test.sh
+++ integration-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27b4dfc6af18a44608b0bb1189f978d7175b46049babfa9c0121f04dd7296093
-size 7737
+oid sha256:3977454253815e83d0eef329b13a821ea439b350dcd9d5bbb27b2ac154128ebd
+size 7937
