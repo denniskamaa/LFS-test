b94fbd7eeec9f10266ff6834f882512dcfed8710
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-02-13 10:51:07 +0100

    Fix ElasticsearchInstallerTests

--- tests/mechanic/provisioner_test.py
+++ tests/mechanic/provisioner_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f9d9220077b856b86ebbcaae3f18dca6a7540da622c18b997734fc136ee4803b
-size 17904
+oid sha256:895839387311380420bbc49742a26f4dc1bdd5b1953c197776f66cf9e81bc61f
+size 18058
