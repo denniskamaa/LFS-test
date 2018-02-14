0cd45682ffda9a64160f33d298a8929350ff860a
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-02-14 09:10:50 +0100

    Upgrade versions in integration test

--- integration-test.sh
+++ integration-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a78b2c015d962cf92c1bb31c1d69f62a52752fa41041a27f1ccaff5417a27878
+oid sha256:fe735ecc668939fbf94bf2a057adccfe918b67e0429ec65e3d38c4ec06d9e7df
 size 7605
