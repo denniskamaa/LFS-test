567b93ab2c45128bea5a5cfa13d0e4199909749c
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-02-17 13:42:33 +0100

    Fix query bar chart

--- esrally/chart_generator.py
+++ esrally/chart_generator.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a576c8a2071b8c2c468cdd2de12b7fc8f0e0f43be3dc19e5529b9d4fe9cd0382
+oid sha256:8930b5b4a8a036365cba6d4fcc609f890b6db3bc057363ae29f794d090c98aaa
 size 50802
