d2e32b8f2f56ce20f6bf3dfa93dad8d3da69dcf9
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-02-20 15:37:36 +0100

    Check user-tags instead of user-tag in analysis

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:235acdefd552121d4d77385d0488872e3b76dbd7527197cf4ad65ecf60932eb2
-size 6450
+oid sha256:49f8e1dc3df6de1aa035641876d390a30c3a458812db0fff8a87d2b0d0f58ba6
+size 6451
