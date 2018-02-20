1b3d4442f1e209aff7fed05706a44d8df019c71b
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-02-20 09:51:55 +0100

    Additional logging when gathering indices stats
    Closes #416

--- esrally/mechanic/telemetry.py
+++ esrally/mechanic/telemetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ab4820d188b54822279573d7d61881a21fd7452a296861740541bdf069eca7d
-size 35414
+oid sha256:3eb59dbf77fb20c1df20ad1765bc87d6240b7e99ac30e55e6ade859053a3cc0d
+size 35639
