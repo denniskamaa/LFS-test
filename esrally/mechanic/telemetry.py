5d728bcd8485363f97f26c50e12edebb701995f2
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-02-20 09:34:23 +0100

    Add comment for JFR settings

--- esrally/mechanic/telemetry.py
+++ esrally/mechanic/telemetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:898009996aa7aea7fdbaffb532a9b8008327bb66b066b3224f92461fcdc80d9e
-size 35252
+oid sha256:8ab4820d188b54822279573d7d61881a21fd7452a296861740541bdf069eca7d
+size 35414
