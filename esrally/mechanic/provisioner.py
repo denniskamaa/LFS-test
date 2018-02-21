4dca04cd57fcc69f80328cb257d44376b6bd8c70
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-02-21 10:08:28 +0100

    Revert "Use more specific name for mandatory plugin check"
    This reverts commit 62c57237fcfe62fccf6cc40527e76a982f125291.
    
    Relates elastic/elasticsearch#28710

--- esrally/mechanic/provisioner.py
+++ esrally/mechanic/provisioner.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6bfc20383751e235be86139335899f6e5c0993309741ccfb5a2420378a01faf3
-size 22646
+oid sha256:d70f402ce105f3d494d72f5574af4e84d88cfb52ab329c6dd5db8c764cf34573
+size 22434
