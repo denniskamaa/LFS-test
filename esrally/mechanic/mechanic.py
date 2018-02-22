22a360c3c1adff716a0e94655f1484f3ca6c3ada
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-02-22 17:58:52 +0100

    Don't store children per-host for external cluster
    With this commit we store only one child actor for an externally
    provisioned cluster instead of one actor per host. The latter is only
    needed if Rally provisions the cluster.
    
    Closes #420

--- esrally/mechanic/mechanic.py
+++ esrally/mechanic/mechanic.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:00f5db6392ff76391f3e90b8ef9a8df7061e0f9901d21401991d551ee1770943
-size 27903
+oid sha256:e114b08100d01b8c901b7efdbd700e2cac3e7b34769464284e4e85ce38e8ec67
+size 27915
