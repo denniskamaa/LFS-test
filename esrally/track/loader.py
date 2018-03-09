6ca12c02d53760c65b17715515fb4f92dc1e28ab
Daniel Mitterdorfer <daniel.mitterdorfer@gmail.com>
2018-03-09 08:35:21 +0100

    Allow to use track parameters in index (templates)
    Closes #429
    Relates #430

--- esrally/track/loader.py
+++ esrally/track/loader.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f9d19297dcbfd34c80327d554491df8ea89920e076964fe44f9b53afa92f295
-size 62222
+oid sha256:b5263aebcea3126976cad91969a1dbc02a625121d01b198c65fe4f5f0b9673f5
+size 62959
--- tests/track/loader_test.py
+++ tests/track/loader_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8fe227da9da364dcf5aa64e39b7ebb37452fea61ddaba28ec12e19510f09feb4
-size 90934
+oid sha256:5f2803887d7eb3f35c27b01ec3489553ba24332a8ad68ebb8bd0efcc44db9080
+size 91838
