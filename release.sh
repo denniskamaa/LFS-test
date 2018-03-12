09baae38bcce32c4f64a8761fa69f932c02d7ddb
Dimitrios Liappis <dimitrios.liappis@elastic.co>
2018-03-12 14:18:10 +0200

    Update milestone link in post release notes in release.sh

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ca4204c22dd95e8a6557d42469c27824674dfb7e399ca04741cbf53b56e2b9f
-size 2588
+oid sha256:742c99809319e96e735e106a669004bc258c12c85589dcbba04c87f1dda37015
+size 2571
