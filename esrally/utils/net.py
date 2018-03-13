9fdee98bf6d996136797b6ca9b79b99663f5b837
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-03-13 10:37:26 +0100

    Use Content-Length header as size-check fallback

--- esrally/utils/net.py
+++ esrally/utils/net.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e93f2118aad6b6cb950c2747d6c6c47c562ae375dd503c125922d9866b8faed
-size 5501
+oid sha256:aa19d27439c2b387ec1b5d0b335036475963cc0285ba986974fa7b654749b711
+size 5622
