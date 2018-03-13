3ec4d4a15755734dd4d377d0d1f521fbb8773965
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-03-13 10:28:49 +0100

    Combine installation and config into one method
    With this commit we combine installation and configuration of
    Elasticsearch and plugins into one method. Previously these were two
    separate steps but the installation scripts rely that the config
    directory is present when a plugin is installed. Creating the
    configuration directory immediately after installing Elasticsearch
    avoids this error.

--- esrally/mechanic/provisioner.py
+++ esrally/mechanic/provisioner.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d70f402ce105f3d494d72f5574af4e84d88cfb52ab329c6dd5db8c764cf34573
-size 22434
+oid sha256:9ff098d0274eb48548526f9fa92395f7cd209098cd67d85feb47cee7969cafdf
+size 22327
