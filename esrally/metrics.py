7462c27c48050bf41728bf0cdba85a393f24d480
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-03-15 08:01:51 +0100

    Fix indentation

--- esrally/metrics.py
+++ esrally/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50af66511a3d66eeef0bd4d9e1c02723b0113061fb10f745cb1d626cfd29d3c9
-size 61335
+oid sha256:aa82026029cac80332681d76c9bfbacd0d5413f1cc16ced52083f9a74a4a4bd3
+size 61336
