a666863e88ff0b0d5d7aaf254989255797a02c68
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2018-04-03 14:52:35 +0300

    Fix venv detection with venv and Rally execution on non master
    This commit fixes two issues. Firstly, let `./rally` start when
    executed for the first time with self update disabled (e.g. from a non
    master branch in a fork) by ensuring the `esrally` binary has been
    installed with setuptools.
    
    Secondly, fix virtualenv detection when pyenv is in use. On some
    configurations when pyenv is present, creating a Python3 virtualenv
    uses pyvenv instead of Python3's own virtualenv[1] which fails the
    current check for a virtualenv in the `run.sh` script.
    
    [1] https://legacy.python.org/dev/peps/pep-0405/
    
    Relates  #449
    Closes #447
    Closes #448

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:306a76ac1c2abdd7404a2c25b425ad85a25d806b56d6155bbf81c0c2d4819c8d
-size 3574
+oid sha256:6cb78ac073723bb9eec8fae5487946d3c3bb3072cc6992e6c187daececcca77e
+size 3969
