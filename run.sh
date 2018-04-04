b4ece07b210c835ffbd261794b733df4c5a699f2
Dimitrios Liappis <dimitrios.liappis@elastic.co>
2018-04-04 10:01:17 +0300

    Resolve more incompatibilities with function definitions
    Follow bash's advice[1] strictly in how to declare a function name to
    avoid issues with parameter passing. Avoid the `function` keyword when
    declaring a function that is sourced from another shell script.
    
    [1] https://www.gnu.org/software/bash/manual/bash.html#Shell-Functions

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d582beac9e2553255522876a090bf65ab633326977daefa13889edb9c352f2a
-size 3973
+oid sha256:de5d29bea9cf5dc33c79860fc88dfc6173e63aafef63f0dfc62cab953c53061e
+size 3967
