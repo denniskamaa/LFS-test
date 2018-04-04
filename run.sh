8a8741a974b146f5bde4e6aec1bb094fcc3bffa6
Dimitrios Liappis <dimitrios.liappis@elastic.co>
2018-04-04 09:20:07 +0300

    Be even more backwards compatible with bash function parameters
    620db995218607c1677d6ec960d47c473e31f8be fixed incompatibilities with
    older bash versions such as the one found in macOS, however, it's not
    bullet proof and still fails e.g. with Ubuntu 16.04's
    bash (`4.3.48(1)).
    
    It seems that declaring functions[1] like
    `install_esrally_with_setuptools ()` fixes this issue, but for now we
    opt for the solution we had earlier using `${1}` for the parameter
    inside the function. Additional tests may need to be added in the
    future.
    
    [1]
    https://www.gnu.org/software/bash/manual/bashref.html#Shell-Functions

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:45f6277863b5cdd515c98ee1fb7cc34ce29ab6fa5b509894fbe6ee008ec4a1e9
-size 3967
+oid sha256:1d582beac9e2553255522876a090bf65ab633326977daefa13889edb9c352f2a
+size 3973
