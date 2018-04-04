ecee0c2390ca35c6f81f003bc4942821e7d5d1c2
Dimitrios Liappis <dimitrios.liappis@elastic.co>
2018-04-04 10:55:55 +0300

    Fix rally start when auto update gets triggered
    When auto update gets triggered (i.e. there are no local changes) the
    function install_esrally_with_setuptools() fails as no parameter gets
    passed at that point.
    
    Make optional parameter detection more resilient in bash function.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de5d29bea9cf5dc33c79860fc88dfc6173e63aafef63f0dfc62cab953c53061e
-size 3967
+oid sha256:a8a4718eaf1913c20ec66a563fcbcfb138761b30cf992109a933563eb2bcde41
+size 3976
