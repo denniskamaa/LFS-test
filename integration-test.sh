cb37e08575bcdbb956f5dac44c832c71214361f7
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-04-06 12:46:18 +0200

    Revert "Temporarily disable integration tests for older versions"
    This reverts commit 24a59d7ff8c4aa96cec4e529c897b3215c226dfa.

--- integration-test.sh
+++ integration-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0aa3deeeadd000247221368de4a1753695fdecb4da79ad90fb340be394a2a5e
-size 8685
+oid sha256:acd6bb9568016ec453f7ab07013cc636a109375855d597477535ccd94e24fcc5
+size 8653
