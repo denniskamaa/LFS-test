dac0b8f500948584bc7c90c20fe33311ac84aaa5
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-04-06 12:50:48 +0200

    Remove obsolete warning message
    This commit removes an obsolete warning message about a deprecated
    operation type which is a leftover from #435.

--- esrally/track/loader.py
+++ esrally/track/loader.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87a35d8e9f41bb5fc20e7535e22b26be574401ec7ef1201a34bddaa0d8a9c654
-size 52267
+oid sha256:0583fbdcd266a984f2cde4a0cf8b3c40867d6fa61b8ceb0af5a31d3eec568537
+size 51549
