1bf79ed06dd36abdf30adb7e20258626f3c4e1db
Daniel Mitterdorfer <daniel.mitterdorfer@gmail.com>
2018-04-11 13:45:40 +0200

    Require at least Rally 0.7.3 for config migration
    With this commit we remove outdated config migration code. As our tracks
    require at least Rally 0.9.0 now as of the recent cleanup (see #380), we
    remove all migration code for config version 11 and below (that is
    versions older than Rally 0.7.3).
    
    Relates #468

--- esrally/config.py
+++ esrally/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4baee1a26a2b4b7d916580454fd6e9c058d508f876ed30de709a1769a120735
-size 45104
+oid sha256:6e12ff573149de1eb542c74106da1a4d127b1214627e38222c432b0af76ea3ca
+size 35625
