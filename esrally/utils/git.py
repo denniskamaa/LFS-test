2d7bde8708ec0fc44af2598ebb7b6fd12f39c57e
Daniel Mitterdorfer <daniel.mitterdorfer@gmail.com>
2018-04-11 16:57:23 +0200

    Don't require git
    * Don't require git
    
    With this commit we silence a spurious log message when git is not
    present and document under which circumstances it is ok *not* to have
    git installed.
    
    Closes #339
    Relates #466

--- esrally/utils/git.py
+++ esrally/utils/git.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c569653e288c4c5c6bcc8dd3375f5d0a4a4a2537ef86102307122ebcf64e50a7
-size 3867
+oid sha256:4c221600d7580eafe72e7a811517ca3956b5de5dfad8e0e3dd8ec7018f019aa6
+size 3912
--- esrally/utils/process.py
+++ esrally/utils/process.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e0210556a4889b47bdc28ff36debd34cd957148bcc57808e73e0ae58dfe6fd2
-size 4598
+oid sha256:ecccc09aa2f41083c2797283c6d9fdf5f05a843c51a4556117039900bd37153d
+size 4693
