92a0f8cc9f48296f2ff824f61fec783a037014f4
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-04-17 10:28:36 +0200

    Warn about deprecated bulk params only if needed

--- esrally/driver/runner.py
+++ esrally/driver/runner.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e0c76eabedec92f9616bf8ca38b7104d90092d370f41101f2f1d1325f1fe8011
-size 35398
+oid sha256:08c5cf2ae2928b88fa11499aff8320f18c8123a9ba846e1fa99ba750b0dea2f8
+size 35523
