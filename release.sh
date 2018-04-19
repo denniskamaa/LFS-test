1701ea616a362e44719c7bc2e064849ea7267e15
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-04-19 12:57:00 +0200

    Activating docs is not necessary

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d3546268099b701e80305195f4136abfdc6f41274b05f8bb0b1aa279e3e9f23
-size 2518
+oid sha256:a89d866710d746b19c53b3d5a319cf058c62289806cb99ff0814d673c0e2af4e
+size 2406
