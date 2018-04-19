b8d7c611868959f2ed54a49453a5f66d221822d6
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-04-19 10:20:42 +0200

    Don't modify client options input parameter

--- esrally/client.py
+++ esrally/client.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b1d4bd958af9069dfb9c898b5108afbf22dbea2989fdbea693214a88855232d
-size 4247
+oid sha256:faa7f9acd6cd5b361e152ed71eaef6797865405ddde8c998a1e07c35e20bc45d
+size 4306
