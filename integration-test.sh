0b71fee44abe699f0c441455cc4d734d7ed00250
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-04-19 12:31:41 +0200

    Skip integration test of ES 2.x

--- integration-test.sh
+++ integration-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:acd6bb9568016ec453f7ab07013cc636a109375855d597477535ccd94e24fcc5
-size 8653
+oid sha256:d4ae74906f9956ab1e564c0e3a62760e1754f1581da4e48b271935fa82ed4876
+size 8793
