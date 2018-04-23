03db9901e241f426801318441f9e380d539eaee2
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-04-23 13:55:26 +0200

    Periodically flush system metrics on coordinator

--- esrally/mechanic/mechanic.py
+++ esrally/mechanic/mechanic.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ccb803e6040ad87ca8a320dccd9d99ae3921215128ca4e2af7e1051b03feb749
-size 29109
+oid sha256:f78a102aa251d54c74c020f5f8120d5455733601ea305cb0aa2617ff24dad731
+size 29789
