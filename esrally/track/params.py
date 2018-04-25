915363225620d203e40f2186462aacac7a1d9764
Daniel Mitterdorfer <daniel.mitterdorfer@gmail.com>
2018-04-25 09:05:49 +0200

    Improve simulation of bulk-indexing conflicts
    * Improve simulation of bulk-indexing conflicts
    
    With this commit we introduce a new property `conflict-probability` for
    the bulk-indexing parameter source. Previously we had a hard-codded
    probability of 25% but now the user can control it.
    
    We also add a new property `on-conflict` which allows users
    to define whether the action-and-metadata line should use "index" or
    "update" on simulated id conflicts (the default is "index").
    
    Closes #422

--- esrally/track/params.py
+++ esrally/track/params.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64e2bae9f3c8fe8ea4e730ab9eb085f0cb28ab023ee165036547ed8c0fc0accf
-size 36012
+oid sha256:2bd1948f29726c4ee69f9bc4f6386d0ca4a027be71923d5a41077963ec12fc6e
+size 38810
--- tests/track/params_test.py
+++ tests/track/params_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6c63889d0e24fcc8223f0bbd7ed2522c3fcfa9e569df5852be5f240757cd453
-size 55327
+oid sha256:35021635a8c81b45460a5a42e9d0ee75b5112a4fda55945d07db63c6f856a7d9
+size 59176
