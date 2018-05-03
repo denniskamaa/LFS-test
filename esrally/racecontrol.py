ef62d48bdf91e108d3b56b62a339182e2688770d
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-05-03 10:10:49 +0200

    Fix logging when setting the default host

--- esrally/racecontrol.py
+++ esrally/racecontrol.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:959cee552e47b26479a1a2eff52ff0aeb70322df97b91143017382b7cb12fda1
-size 17146
+oid sha256:040ed161f6830baa8f6ce31a623a47b199e682ed2e00f9e9976c9409703a8c71
+size 17144
