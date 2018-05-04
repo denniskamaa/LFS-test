4baf14ad93b5b5337ec0aebafaac8ae6948ab2bc
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-05-04 08:15:42 +0200

    verbose_iw cleanups
    We have removed the car `verbose_iw` but there were still some leftovers
    in tests and docs. With this commit we remove all traces.

--- integration-test.sh
+++ integration-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4523f6b005ece54891113ba3505d507f3945f81808c55ddfd55bd150eff3d91f
-size 9417
+oid sha256:69aa39a08ce71a3ff2f0044784448ff09fa7e70cc0e679e8739222bd9a89e293
+size 9416
