79e1874edd38fa8e288cf1af4f30329a8cc259bf
Dimitrios Liappis <dimitrios.liappis@elastic.co>
2018-05-10 13:20:50 +0300

    Fix typo in NodeStats unit test class name

--- tests/mechanic/telemetry_test.py
+++ tests/mechanic/telemetry_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:32daff7d78c4f6d5410f8854c01389ce80ae3bcc1fe80a7d7a7e48d9f6c2f614
-size 59868
+oid sha256:ccb833841cb9977c4145ffa1b0b88dc2ee05bfe9f28ec20efbaa919e9c8281ee
+size 59869
