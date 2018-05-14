385a6958004d447ef6a75eb54fdf2b7a52e930be
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-05-14 09:42:44 +0200

    Avoid duplicate text in copyright

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0fdddac81b8f6647e8a597003285b94bfc9c055c62a3ce3a87dbfcbe9e50876
-size 4517
+oid sha256:50580c90811a4da0048bb99de38f424d4ee0b31cb44beea081014a400f8b480a
+size 4507
