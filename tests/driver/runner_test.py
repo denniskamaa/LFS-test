8d9679bb251ca0f3b083e7f0dba8b47bd816d4f1
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-05-17 13:51:00 +0200

    Fix deprecation warnings in test

--- tests/driver/runner_test.py
+++ tests/driver/runner_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5849f97526c5a2a05a507859d9a02f76a42c907d8d3a9b3f998f8a8293bdb54b
+oid sha256:dc9545d906c8b1ec245ae5fca3aadca9f3a035266dfa67b1fe6915787b0a5da8
 size 51812
