7c9c37bf0882f51d81adb0e4e1e8eaf917adc248
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-05-23 13:22:19 +0200

    Use dedicated logger in CcrStatsRecorder

--- esrally/mechanic/telemetry.py
+++ esrally/mechanic/telemetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b98e4fefeef0dda436f9af64ff37c1cb7708c754ca2681a2316ff84aafe245df
-size 52362
+oid sha256:66cb590e23cb4f9752caf77f3e8fad312728a362e8305893ccc5870c42228e9d
+size 52397
