1078e47ae3673aba9ea8cbafad4cfa83461252f7
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-05-30 10:19:32 +0200

    Stable iteration order in tests

--- tests/mechanic/telemetry_test.py
+++ tests/mechanic/telemetry_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12f36d683e05ea33f32990275a2935035af2fcfa015a1c67ce3efaf387e1c879
-size 84902
+oid sha256:736ca39772f4a8ac11793fb3f94fa5ac3b6c43ff57a5703c56b0b8c0e381958f
+size 85767
