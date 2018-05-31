27636ff5f1fffc8cf86b23d609681cb4c7da84b1
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2018-05-31 16:08:32 +0200

    Add multi-cluster support for NodeStats telemetry device
    Collect NodeStats from nodes in all clusters if user has specified >1
    clusters in target-hosts.
    
    Relates:  #516

--- esrally/mechanic/launcher.py
+++ esrally/mechanic/launcher.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2a759ef2ad1a7f4fbc673fdca05f46f6c346632f93b2af0490b846d8e9e5c36
-size 20206
+oid sha256:f415d575b4089dd0035906de907563c1fbc3b3c9e4117e6d425561c9da23b713
+size 20198
--- esrally/mechanic/telemetry.py
+++ esrally/mechanic/telemetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ddcfd9c6a7b958ba6c59755c9be7cbfee944ccee1b8f495a2ab9533aebbbf83a
-size 52295
+oid sha256:a86a644d1f7f39c9c2783f2bdd527ecd257bb15da69446972eed84b428fa5b23
+size 53117
