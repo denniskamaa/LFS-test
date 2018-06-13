c2852fbbfab15fdaa2ff7a984647b3ac602d0ca9
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-06-13 08:43:14 +0200

    Update development status from Beta to Stable

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:753aeb4aa56c3692acb17c4ec715484dedfe086c81550065f4e9d60d63df3dd0
-size 2825
+oid sha256:41f53c5ca78980fe58d5a7f2192c3c6fe6932b7d7fc247fb19b46df02fbcea62
+size 2838
