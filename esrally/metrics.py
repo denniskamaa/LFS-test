87f3cd0d99d4f395971531710e1c96e0dd7ecf8d
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-06-18 17:00:49 +0200

    Backoff on rejected execution in metrics store

--- esrally/metrics.py
+++ esrally/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50dd6d6c2dbccf204c21eab7898d65c3908a272f24e818914efe5786b1a676fb
-size 64279
+oid sha256:7d4950fb91c2e8a34f436329adef8e76a9b911653f20288d82854e13355bbdee
+size 64516
