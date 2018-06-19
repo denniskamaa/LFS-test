03d278cda9010600c777d3d76feb9e4fd79f59b5
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-06-19 10:56:29 +0200

    Store car params and plugin params for races

--- esrally/metrics.py
+++ esrally/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d4950fb91c2e8a34f436329adef8e76a9b911653f20288d82854e13355bbdee
-size 64516
+oid sha256:0299e9801367ecdfcc4fbbb6bad202fc0a8d05438c9b5b9603ed08dda5d33a15
+size 65183
--- tests/reporter_test.py
+++ tests/reporter_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c85dcb1ffbe601fc8b34fdd3a39e760a45d556b54b1b5b3703a409a6b93a41a3
-size 9605
+oid sha256:894d2067fdebb1f364d52d6eac6480b734ac7dd2d7701e7a410ea23397389e93
+size 9752
