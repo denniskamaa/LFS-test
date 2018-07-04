64d8ffab56b98e24c40fbbe30225f91061327b37
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-07-04 12:05:48 +0200

    Fix resource definitions in Docker Compose

--- tests/mechanic/provisioner_test.py
+++ tests/mechanic/provisioner_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f4b486f136a899727bb6ec5e6f36f72667ba6b8f7ebe5a948007f361e4c8fe18
-size 28016
+oid sha256:c7e767820fabfa67461919a990729b2225cfba6f7c7a9a2e60bd7fe354eb9b85
+size 28028
