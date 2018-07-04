8512b9bf9e1045e1ee4395a3df270aef85fdf53c
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-07-04 09:04:00 +0200

    Raise LaunchError on process termination

--- esrally/mechanic/launcher.py
+++ esrally/mechanic/launcher.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f57a47dd6c22c47a190486fcc36b27233959691d6c34d3aae3def64045e3d6a6
-size 20839
+oid sha256:a6a45ddd0507ef9d7cb55dee667f74e934c8834cc3d2bce38f57b2c64e8fae1b
+size 21154
