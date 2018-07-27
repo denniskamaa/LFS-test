e8985a335d7b36e556c809da980b6a8c27ce3c89
Daniel Mitterdorfer <daniel.mitterdorfer@gmail.com>
2018-07-27 08:41:47 +0200

    Warn when cluster settings cannot be applied
    With this commit we issue a warning on the terminal when a user runs a
    track that specifies cluster settings but uses the benchmark-only
    pipeline. Previously we issued a warning in the logs but this is not the
    first place user is looking (nor should they be required to in this
    case).
    
    Closes #541
    Relates #542

--- esrally/mechanic/mechanic.py
+++ esrally/mechanic/mechanic.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4fa5171a31c73f2a24edd127932593b90c04450592ebaf03e4b2e0d2ebbf6a9
-size 29489
+oid sha256:f566aadc88c6038f9989cbfe195b6de0514a73143f7b6f1ac7a2b02c86b3098e
+size 29561
