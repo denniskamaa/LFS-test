1d55bdfe53c1b1a9342d3693dcaaac4dcb64fd52
Daniel Mitterdorfer <daniel.mitterdorfer@gmail.com>
2018-08-01 15:32:35 +0200

    Use non-standard port for metrics store in tests
    With this commit we intentionally choose non-standard ports for the
    metrics store in integration tests to avoid that other ES instances on
    the same machine interfere with our tests.
    
    Relates #545

--- integration-test.sh
+++ integration-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82296a6ff527f5f621bf0e027d33b0e0de02ef0f1156e370e3dfc36660f2add0
-size 8777
+oid sha256:05ee8024b3babfdc29d0c64fc6e89feb02bdfd5850a085e253f825e5f9455dcc
+size 9172
