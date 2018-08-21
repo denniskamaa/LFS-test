ec8f6d245f0e00f3b14b437eec548e7bce2c47bc
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2018-08-21 17:31:30 +0300

    Fix HTTP TLS client certificate verification
    Currently Rally fails with "bad certificate" when Elasticsearch
    requires verification of client certificates.
    
    Fix bug, add tests and update documentation with more
    examples. Also decouple verification of server certs from
    presenting the client certs.
    
    Relates #551 
    Closes #550

--- esrally/client.py
+++ esrally/client.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f53e0f41bd920773c51873ac63f290e5f17eeb14eb7c8cff9a5fec3c83e84a89
-size 4404
+oid sha256:f40095ddbb508cdc1d78a75c3718405d12d921abde570dc37b4e674b3ee87915
+size 6670
--- tests/client_test.py
+++ tests/client_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:32f83848f64873d47b06fa34b643873c00cf71f0c7900482c232ca2bded8d664
-size 2770
+oid sha256:f5a432434641cf4894ce7fa5c966a243cd8f4e70f8e7d0a88d2ad969934c1eea
+size 11856
