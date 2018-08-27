0ef38fb848ad04b2582112b33598858064cc6432
Daniel Mitterdorfer <daniel.mitterdorfer@gmail.com>
2018-08-27 13:09:54 +0200

    Adjust flight recorder flags for JDK 11
    Closes #491
    Relates #557

--- esrally/mechanic/telemetry.py
+++ esrally/mechanic/telemetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23094845b1ee7eefdc45ba83ffb3e4350beb9c02db0660fe645c2f0d2adbd924
-size 51594
+oid sha256:a9cfd797d3cfa9c1e42c024b02a5db943a384ada9e01a27d73e240d01ef569f5
+size 51838
--- tests/mechanic/telemetry_test.py
+++ tests/mechanic/telemetry_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da5a79062bfd24b30ba5ff4f5790e5d88f682e0874f425d1066a4fb56e9bedcc
-size 89201
+oid sha256:5898ef16ac9dc43708507fb47b94861b704ac94578f6b3bff31e09d255850812
+size 90400
