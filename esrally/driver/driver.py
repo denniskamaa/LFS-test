30802a579ab0d2605382e4908d14903a5984945c
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-08-27 09:46:56 +0200

    Load track plugins eagerly
    With this commit we load track plugins eagerly when preparing the track.
    We need to do this because the track loader needs to query all parameter
    sources for the corpora that are in use.
    
    Relates #555

--- esrally/driver/driver.py
+++ esrally/driver/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c40c678ee7cd7b0ba3022bdef76c1fdc08cfac9306f2104bd796eb4de902ac25
-size 62595
+oid sha256:a0ad9185220efd729f9c98e68b67eaef9795b35b4a69fc0bf6204ca46fa9dcf9
+size 62798
