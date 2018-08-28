f164c2c365e93d648359a2bc071d1f6213b9769a
Daniel Mitterdorfer <daniel.mitterdorfer@gmail.com>
2018-08-28 15:12:38 +0200

    Write rendered track to a temporary file
    With this commit we write the full track to a temporary file instead of
    dumping it directly into Rally's log file. This helps users to interpret
    error messages later on that involve line numbers. It also avoids
    bloating Rally's log file.
    
    Closes #554
    Relates #559

--- esrally/track/loader.py
+++ esrally/track/loader.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99b26453a1ed9f4acc0f7e33b76c4333df5fcc0d7b2b1814fcca3139a8ac9a12
-size 52783
+oid sha256:bd7cb4be7387913f5799fd1835d5a3cc354abbc0bf29c2c60dc834c01b81487f
+size 53213
