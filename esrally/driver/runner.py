4a1f31dcdb1feb78c54136cda5fff7da5cd32419
Daniel Mitterdorfer <daniel.mitterdorfer@gmail.com>
2018-09-10 16:55:59 +0200

    Add shrink index runner
    With this commit we add a new runner to Rally that executes the shrink
    index API and all other relevant API calls in order to shrink an index.
    
    Relates #569

--- esrally/driver/runner.py
+++ esrally/driver/runner.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:00f61a44c9202a4594f57a594d826557f1705ee718134dd92af7f262c1c4a765
-size 35868
+oid sha256:a68ada7ad99d76f53fb08751ea5a94def2c16f4a527de8c5ac875803112b59e9
+size 39401
--- esrally/track/track.py
+++ esrally/track/track.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da626931f03cfd3360eb122e799cc22e1a8c7544ff9dfaadfa302f1a41183f31
-size 22964
+oid sha256:0878364292b75ca0d0302c638dd8eecdae77887b3ecb677a16e65f716ff0238a
+size 23066
