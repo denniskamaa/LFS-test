0991a763cbd34281625215c2549a39d84b3fa40d
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-09-10 08:11:01 +0200

    Fix wording and line break

--- esrally/mechanic/telemetry.py
+++ esrally/mechanic/telemetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9cfd797d3cfa9c1e42c024b02a5db943a384ada9e01a27d73e240d01ef569f5
-size 51838
+oid sha256:76559d79db53d44e8ac8325cd4a0715e35b09f11b079b384ce0266f4d27e05af
+size 51883
