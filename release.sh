4b44272736bdb381ffa3bfcb2428b9627ec2d12e
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-09-13 12:32:05 +0200

    Disable automatic preparation of offline distro
    With this commit we disable the automatic invocation of the script that
    creates the Rally offline distribution. We disable it because the new
    pypi API caches quite heavily and the Rally package will not show up
    immediately after the release. Therefore, creating this distribution is
    an additional manual step in the release process for the time being.

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a89d866710d746b19c53b3d5a319cf058c62289806cb99ff0814d673c0e2af4e
-size 2406
+oid sha256:3838561b1c176ece62bf34772fafe872dbb748b8017a187ddece8902cf980b2c
+size 2408
