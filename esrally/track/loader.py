966bbd945370abc5de65f9dd4bc029d7a95d33b1
Akhil Rane <rane.a@husky.neu.edu>
2018-09-21 00:40:33 -0400

    Improve formatting of document count in list tracks output
    With this commit we add thousand seperators to the document count in list tracks
    output. E.g. instead of showing 11396505, new change will show 11,396,505.
    
    Closes #327
    Relates #576

--- esrally/track/loader.py
+++ esrally/track/loader.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05ed23b4153b79b79fd08564d379bcec3e591c724a6c1fba68fcbd0d4c5f1161
-size 53417
+oid sha256:3734e22d4bdf04873357a31a1390f1261ad74c1d6068a025ac7185d7439cc8b3
+size 53465
--- esrally/utils/convert.py
+++ esrally/utils/convert.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f96f51c1fcf5c68b3c7e528ec720325cffad25a6b5fba1e2b52003a14865d021
-size 1397
+oid sha256:23051175a37d598b62c20e38573faedcc9fd909a8f8f0aad38dc80fb7f68e6d4
+size 1468
