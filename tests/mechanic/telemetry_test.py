d0f4bcee909f35080acff79cf7bf719ea5695699
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-10-19 08:06:09 +0200

    Use assertRaisesRegex in test

--- tests/mechanic/telemetry_test.py
+++ tests/mechanic/telemetry_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:611261e0a20c5374ba10934f6985d1651406519a44cdbb49150c025cbbd0bafa
-size 97112
+oid sha256:2fef2cb396121e9c177cf463b4e6ac6107a6d58148f6f4d966609f5e82596d29
+size 97109
