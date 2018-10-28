af090ddf3135f41c66730c14d3e2d273dd21e6ee
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2018-10-28 12:21:00 +0200

    Allow specifying Indices stats metrics to collect from Node stats
    Currently the metrics collected from node-stats indices are a fixed
    list.
    
    Introduce a new telemetry parameter to allow users explicitly specify
    which metrics to collect from the Indices stats section in Node stats.
    
    Relates #589

--- esrally/mechanic/telemetry.py
+++ esrally/mechanic/telemetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8e506c9fd1a1140588704e8cf984e92bc46cf9380b33abba9aca249eec7423c
-size 52672
+oid sha256:c64c8a8c42363e26f6667f9d39bc2751432dba666b15893da8dc8ad18967ceda
+size 53487
--- tests/mechanic/telemetry_test.py
+++ tests/mechanic/telemetry_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bcd8db72321fccc12a37d63d338576ba0aa1e7908ddbb2db1bef9fdf5ae1924b
-size 97116
+oid sha256:c4b25abc41eec313e00e66ec2682f8db1f319c2325e17184430f47625fc9ba1a
+size 108967
