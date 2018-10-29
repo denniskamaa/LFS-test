e80eb75557bd7c62692911de3ddbadb32e21cb92
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2018-10-29 19:53:28 +0200

    Update ccr-stats telemetry device
    The ccr-stats API endpoint has been updated in [1]; update the
    telemetry device and tests to align with the changes introduced.
    
    [1] https://github.com/elastic/elasticsearch/pull/34912
    
    Relates #590

--- esrally/mechanic/telemetry.py
+++ esrally/mechanic/telemetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c64c8a8c42363e26f6667f9d39bc2751432dba666b15893da8dc8ad18967ceda
-size 53487
+oid sha256:e33d556f434ef2d14aef6c66cbaf35ebe1ff7002376e36afa1ed087314b35ee8
+size 53732
--- tests/mechanic/telemetry_test.py
+++ tests/mechanic/telemetry_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4b25abc41eec313e00e66ec2682f8db1f319c2325e17184430f47625fc9ba1a
-size 108967
+oid sha256:fd05bf287616e4e9abf8ead39ea73c0b973a9f68249e81d696cf831e5224bf97
+size 111865
