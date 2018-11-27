2d40b3ae09b63a75990c86a9c20938e8b6cc578f
Evgenia Badyanova <evgenia.badiyanova@elastic.co>
2018-11-27 16:00:55 -0500

    Change logging level for esrally command line to DEBUG (#604)
    All command line arguments were logged in a generic fashion and among
    them can be passwords. We want to avoid the passwords being logged by
    default if they are provided but still keep the ability to configure
    the command line arguments to get logged if necessary.

--- esrally/rally.py
+++ esrally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3fc3174f230114b8af303d125c963bc19307389f66accf808514f657ab7c76e9
-size 31063
+oid sha256:80112ab9cde7a2edbf0c70f0b8291909b75b573ba7e5017732f1848dfdf857eb
+size 31064
