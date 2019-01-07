ca39e869bd4fc67c39bce54902cba31b9ca2e79a
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2019-01-07 10:08:34 +0200

    Fix timing issue with test
    Initial attempt to reduce errors produced by #600

--- tests/driver/driver_test.py
+++ tests/driver/driver_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d3b4f90e19aa89f6ba39f58a9e08bc8a381552ba10d38e973fd398c56b3f7b4
+oid sha256:602ce10af548ae5afc8e4852838116977c2a1660ada175048be0c62d6c4e2657
 size 42273
