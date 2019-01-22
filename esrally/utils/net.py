99f596e19ef3a572a93e29aba3fd6ce80f170064
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2019-01-22 13:50:22 +0100

    Improve logging in network connection probing
    With this commit we add a new debug log statement to better understand
    why Internet connection probing fails.

--- esrally/utils/net.py
+++ esrally/utils/net.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2380535c4b74d8a106b9e82d4bf998d0ff0dbbcfe70eda6a91fc27ef940e1b48
-size 6436
+oid sha256:a0ea090ec946c555da2a8e7ce7a2e3cc6679cf7df68a915c0d98b0661833d428
+size 6522
