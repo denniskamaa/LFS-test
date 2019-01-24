a4565fdf11e24e1859c4a62e82f75a6372a012a5
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2019-01-24 13:55:37 +0100

    Remove 'de minimis' attribution

--- scripts/offline-install.sh
+++ scripts/offline-install.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0eeac857927fbe997e7881cd6dd92277ae95563ef27f96f1a47b0c08e8589c4
-size 3984
+oid sha256:22dfc2b4b676939b4b08a18322f5e62676840b54ab9de18b3a626a7ddb9cc263
+size 3944
