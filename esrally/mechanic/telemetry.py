c68d3f9e54a062fb0302132bbeece04ac0379d90
Daniel Mitterdorfer <daniel.mitterdorfer@gmail.com>
2019-01-29 14:55:20 +0100

    Ignore JSON logs for merge parts analysis
    With this commit we ignore any JSON log files when analyzing merge
    parts. Because Elasticsearch writes both plain text and JSON log files
    with elastic/elasticsearch#36833, we need to ignore one of them in order
    to avoid double-counting. We choose to ignore the JSON files for now as
    this is backwards-compatible.
    
    Relates #637
    Relates elastic/elasticsearch#36833

--- esrally/mechanic/telemetry.py
+++ esrally/mechanic/telemetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:adaafe506284b48ab8a2dd45608d8dc2061e4d87b0d7d5080597561a1747913a
-size 55486
+oid sha256:b2cedf108249b06280665c83316193d6b94d7217de87354c2e6d1bc5cd249bb0
+size 55621
