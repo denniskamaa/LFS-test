de5b321834cf3859b80b2b72578fa19289e26cea
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2019-02-12 13:05:12 +0200

    Allow collection of jvm gc section in node-stats telemetry device
    We already collect the mem section under node-stats/jvm but GC stats
    can also be very useful for understanding the jvm memory behavior during
    a benchmark.
    
    Add `node-stats-include-gc` telemetry parameter (enabled by default) and
    add it to docs.
    
    Relates #644

--- esrally/mechanic/telemetry.py
+++ esrally/mechanic/telemetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:875a1786d10139f9cf623d7510b32bc7a871cbd1ee2cddebd653a170dd0ad342
-size 61115
+oid sha256:a4755879fd8d0ae686cd18403c7b8ad0126e0beb12e64f0c99c8bee8c73e74ca
+size 61464
--- tests/mechanic/telemetry_test.py
+++ tests/mechanic/telemetry_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e9b573121cdab3d505761d45ceb0c1b9d1e1d6a21e8ff6f269ecc5be9dd840e
-size 122848
+oid sha256:166c1ae597f143520c145418acbd044aab26899d97f5c376e6031b85f3bd0fbd
+size 125180
