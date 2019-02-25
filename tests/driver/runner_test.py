895f25d498e367caea8c68c95b1efa3280c7cf47
Evgenia Badiyanova <evgenia.badiyanova@elastic.co>
2019-02-25 11:24:01 -0500

    Update unit tests to reflect changes in #605

--- tests/driver/runner_test.py
+++ tests/driver/runner_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73255bcab0228bd1c118fe5d114020e5172b9ecf892f05090704102660395d90
-size 69530
+oid sha256:fe6d981516acdf254aad3e47414ebbaed832039a147ceeeed3d763fedcd16755
+size 69525
--- tests/metrics_test.py
+++ tests/metrics_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64d6540ecd3674ba93a1ce625ebd1ca9079d902e0cc4d998e844cffe8a1ee532
-size 53583
+oid sha256:abb7423d4ff6ec75fdc7f9579f534d96cb8d7fc539938b6b8417777d71cd05dd
+size 53568
