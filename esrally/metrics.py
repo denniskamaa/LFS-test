91c46b3dd3f9e4bff302bfae2d9808fb4017d3ca
Itamar Syn-Hershko <itamar@code972.com>
2019-02-25 17:50:08 +0200

    Updates to rally-* index templates to support 7.x APIs (#648)
    This fixes https://github.com/elastic/rally/issues/605

--- esrally/metrics.py
+++ esrally/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6cfdd011660cd4318041f8b57bdfbc78a14db5a26d16fbf0e10563687ff0be67
-size 65922
+oid sha256:312baaa7c51380b8a4976eff9a68e6f00bf46f780ba92ee7f06f177b578819ec
+size 65915
--- tests/metrics_test.py
+++ tests/metrics_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c529d7cc982782806f358680c325c5e320cc2972dd869f807c192a145609fc1
-size 53611
+oid sha256:64d6540ecd3674ba93a1ce625ebd1ca9079d902e0cc4d998e844cffe8a1ee532
+size 53583
