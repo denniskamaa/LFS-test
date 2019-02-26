cb2881e2c09eb253f4ed7ba524e95e939af5d13e
Daniel Mitterdorfer <daniel.mitterdorfer@gmail.com>
2019-02-26 15:08:57 +0100

    Add compatibility layer for existing metrics store
    With this commit we add a compatibility layer for existing metrics
    stores that ensures Rally will not create incompatible mappings. Without
    this change, Elasticsearch will complain that an existing metrics, races
    or results index contains multiple types. By detecting this condition
    upfront and instead writing to an index with the suffix `.new` we avoid
    this condition. Note that when a new index is created (i.e. after a
    month), it picks up the new mapping and is thus not prone to this issue.
    
    Relates #605 
    Relates #655

--- esrally/metrics.py
+++ esrally/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18d7b8de08d06ec2fcf04afba1e415d482914e8b6a7833d2587be26925f5ca05
-size 67064
+oid sha256:c5fae9c5b201abb3d7439e3637395a0b054ef8e7aae25e17d7b091f1fb5f0456
+size 69136
