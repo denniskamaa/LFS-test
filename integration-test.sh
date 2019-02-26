a273da4732b7b892d4dcb89fa8d59ead6fd1c41a
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2019-02-26 13:19:20 +0200

    Fix bug in integration-test after proxy PR #654

--- integration-test.sh
+++ integration-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d31aa560f55740398131e0e4e943db528cf9b012d763db0fc2b7712353ffbc60
-size 14290
+oid sha256:7cf49cf3e93e01e83c1fbdc4e084626c902b772da5e4bfd519e1110ec7ab6e95
+size 14425
