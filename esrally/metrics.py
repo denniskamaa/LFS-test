238d7399e70995970d4c15a74068aa89b1a12c9b
Alexandros Sapranidis <alexsapran@gmail.com>
2019-03-04 15:29:21 +0200

    Sort track params and user tags when listing races (#659)
    This change is affecting the list races subcommand.
    It sorts the track parameters in alphabetical order in order to make the output
    consistent and also sort the user tags with the same order.

--- esrally/metrics.py
+++ esrally/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c5fae9c5b201abb3d7439e3637395a0b054ef8e7aae25e17d7b091f1fb5f0456
-size 69136
+oid sha256:f3379082043e25f209af912e8203cd5732c255efb7097b63910bebe47077e6a6
+size 69168
