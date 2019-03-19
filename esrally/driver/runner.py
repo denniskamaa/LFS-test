f54e6d1b9bb5b0e1000c458cdbad76bafed2171f
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2019-03-19 12:38:26 +0100

    Pass timeout with generic params for optimize API
    With this commit we pass the request timeout via the generic `params`
    dictionary to the optimize API (which we call via the client's raw
    transport API) instead of as a dedicated kw parameter (which is plain
    wrong).
    
    Relates #669

--- esrally/driver/runner.py
+++ esrally/driver/runner.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c0ae1d4ed0c7e9009c493b185a15f610a9afb2ceeb816994f8d479a4acecb9e
-size 46050
+oid sha256:9c56c889f394c96ae16df69e51bce7239e55360d454bc6d88cf875462a7e4b82
+size 46092
--- tests/driver/runner_test.py
+++ tests/driver/runner_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0fdedfe405164a2162331ec757b3cc8d5e166ab212787da389d8d1432ec12066
-size 73585
+oid sha256:bec84a4ed88a8cb3578781f06c8b25b4b275551e369efe11901c237b42928733
+size 73686
