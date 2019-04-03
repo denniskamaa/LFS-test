29409d686f95c278b499986119016325f7164b70
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2019-04-03 17:47:25 +0300

    Chart generator segment memory and new track combinations
    With this commit we update the release-charts to cover the default benchmarks 
    and add codify segment memory visualizations.
    
    Relates #668

--- esrally/chart_generator.py
+++ esrally/chart_generator.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bcb9a8e522937f80e83a5265d3ffcb6f4a438062ad8dde4f45c7d3ea277dd6e7
-size 51662
+oid sha256:662fda7323f43ec78390a3a38e30c885b2134fd50232d5ba418339e06447520f
+size 62670
--- esrally/rally.py
+++ esrally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:45f5f84cfd8a71bb9a0a53f512e3a93868f1106fcea3c40eb6579e894911a44a
-size 32592
+oid sha256:1413e546d98584273a067a1c021e8341fb5b2bca986d0c9805794193eead2c57
+size 32663
