1d0476288b55a6e24a8350544016b0a3c3403ae9
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2019-04-12 10:22:04 +0200

    Revert "Allow shell redirects if possible"
    This reverts commit f48c5d85384b770726ee7ac7e873ef761c9a5f64.

--- esrally/utils/console.py
+++ esrally/utils/console.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3983f85b95261f7bec2b0b45b16ff6265e8cc54de78f6a4f74e1b6f046c611af
-size 6515
+oid sha256:5da13eda0dbf84df4046468266ce17caacf268d11dc8affc114e9b52e1b37364
+size 6008
