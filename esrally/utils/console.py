b580d2fb564a9cbea9cf7b34457f724ff71840ec
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2019-04-12 10:13:26 +0200

    Close terminal for all OSErrors

--- esrally/utils/console.py
+++ esrally/utils/console.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3983f85b95261f7bec2b0b45b16ff6265e8cc54de78f6a4f74e1b6f046c611af
-size 6515
+oid sha256:96087b9ee110ff29c3c6ae53181147e2892e0b5e05ffdc674e1afe86bd63b5a5
+size 6421
