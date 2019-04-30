86690a2c2da879717d2dc2271e92cff3c360d830
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2019-04-30 09:40:43 +0300

    Fix release charts after
    PR #684 is now passing es_license, but this is missing from the
    `format_title()` method signature in the BarCharts class.
    
    Relates #687

--- esrally/chart_generator.py
+++ esrally/chart_generator.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b94fcd886534042fa5c3cb9b4edbd12248c1fe45a4ed5284165898dd430a7244
-size 63124
+oid sha256:4ebdb5b8d34ef19a800903cec6adf2a6decad4f5662ef866b68f2fd12d28c704
+size 63141
