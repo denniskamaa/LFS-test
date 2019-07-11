ade9492d2343387ff22e03e69d368a64199e6fe3
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2019-07-11 14:05:27 +0300

    Don't check complete list of parameters in integration test
    To check the complete list of exposed track parameters in an integration
     test we rely on parameters in rally-tracks which can change over
     time[1].
    Be more lenient by just checking the unused track parameters.
    
    Relates #726
    
    [1] https://github.com/elastic/rally-tracks/commit/4080dc9850d07e23b6fc7cfcdc7cf57b14e5168d

--- integration-test.sh
+++ integration-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5906f2c5c22aa42451def3a74258fe45b68dfce90815540040c8400c7cd98d10
-size 21453
+oid sha256:f7a1c06334214f20804f57e0a9d5c3f9da410f2124d10dd541d9d3d81fadcb27
+size 21248
