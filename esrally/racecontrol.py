4f6ffe411ba808e165cc2579c9179f47299ad6b8
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2019-07-16 08:40:09 +0200

    Allow to set distribution version as parameter
    With this commit we allow (again) to set the distribution version as a
    command line parameter even in cases where it does not seem to be useful
    initially, like the `benchmark-only` pipeline. However, if Rally fails
    to determine the cluster version (for whatever reason) it can be useful
    to be able to override this on the command line. Similarly, it can be
    useful to override this when benchmarking a source build that is *not*
    based on master but rather of an older branch. In these cases, Rally
    would still take the right decisions with some guidance by the user.
    
    Relates #728

--- esrally/racecontrol.py
+++ esrally/racecontrol.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f7ce34c7f143d45712c93daefad4aaeb0dbe374f7512ebf0c6f08e86086fca8a
-size 20025
+oid sha256:d37015dd7a877d4ef8ee0b0bb1c0e5ae03f8c683525a706b3ee7742982b20d00
+size 19392
