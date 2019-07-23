40ec5434a5d7d2a80ddba29ce4fa6babf9ecb68b
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2019-07-23 07:33:08 +0200

    Assume string type for params by default
    With this commit we allow users to skip enclosing quotes for params
    (like `track-params` or `team-params`) when the value is clearly a
    string (i.e. it cannot be coerced to a number or a boolean value). It is
    still possible to surround a parameter with single-quotes to enforce
    treatment as a string.
    
    Relates #735

--- esrally/utils/opts.py
+++ esrally/utils/opts.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:998affa101ec82df1c6c8e287b5b841237c91f20856d729fe04a3052ce49aa73
-size 6083
+oid sha256:f94d8125619792b25bf3ed096bb4435e709f53c7193c3e7bd8a1e5d65197d518
+size 6224
