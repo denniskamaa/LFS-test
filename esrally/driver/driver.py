cc6d74b4f5d14d5295ac86e8285e5e9d6c9c18d9
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2019-09-02 13:15:19 +0200

    Improve logging of schedules
    With this commit we add more detailed info logging to schedules
    indicating how long / often a schedule is expected to run and when it
    has terminated.
    
    Relates #760

--- esrally/driver/driver.py
+++ esrally/driver/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8f87d4ab934ed9434ac1684c13482c3ae5e99aae81efe1ca79d3b4937d82ae8
-size 63817
+oid sha256:867a3ac969f41d6b8adada983287b66722db148727dca46944ab0dabbc19b9b2
+size 64858
