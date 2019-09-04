88c9ec9cd8a71248eebdd1317e59e716e926e46f
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2019-09-04 09:15:09 +0200

    Check that origin points to main repo for release
    * Check that origin points to main repo for release
    
    With this commit we add a new pre-release check that ensures that the
    git remote named `origin` points to Rally's main repository instead of a
    fork. This is necessary for releases because later during the release
    process, changes will be pushed automatically to the main repository
    (such as the release tags).
    
    Relates #762
    Closes #717

--- release-checks.sh
+++ release-checks.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4cec54f1367fe03c615b2e6d5ea402f45334e0d5af757d0bc30509b39a1d7242
-size 2007
+oid sha256:6da0546c48a16017ff284aeb7e756b1fe0be3e1b781f9189b8ebe3c42eba8d5f
+size 2237
