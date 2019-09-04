7027c1520e97861dc764a57a1f0bc106ef80ccc3
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2019-09-04 09:25:26 +0200

    Allow to retry until success
    With this commit we add a new parameter `retry-until-success` to all
    retryable operations (e.g. cluster health) that will allow to retry
    those operations until they report success. Note that this is actually
    just a convenience property that will increase the number of retries to
    `sys.maxint`.
    
    Relates #761

--- tests/driver/runner_test.py
+++ tests/driver/runner_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d640bb88b3b52c351bd24e315f709b5079e72ee12e53ef85fa1a5845b7dce08
-size 76440
+oid sha256:f39e0d18f5cdd5e968b658898ca8cfd49d7c3411f1a1485380ae5b6599b10986
+size 77199
