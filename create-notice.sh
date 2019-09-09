2007742bddc8dee4741cf6398777b3bc099a4755
Evgenia Badiyanova <evgenia.badiyanova@elastic.co>
2019-09-09 10:31:58 -0400

    Correct license url for jinja

--- create-notice.sh
+++ create-notice.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2298f9297e06114ae48e44eca5a93c9a904fc62ea7afb44a480a80c619dd6a34
-size 3275
+oid sha256:0d41b40e5faedf4714638d81d951b2aa2e7d723a88150773cb98b9525e3311c5
+size 3279
