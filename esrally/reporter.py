24dbd2db3a727ebb70da7df5a776a0bcc6975195
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2019-09-10 15:41:20 +0200

    Remove merge times from command line report
    With this commit we remove all merge time related metrics from the
    command line report as they are not measured anymore since #764.
    
    Relates #764
    Relates #767

--- esrally/reporter.py
+++ esrally/reporter.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8451a32fb6bd3d7269b8aec9041b76d58074abe014e0c022cf89d0635eaa3ec9
-size 45368
+oid sha256:a6cb9fee9d476665030c83e5a58fc4b1e1ae05fc740ae1be0ff229efc832b7c6
+size 42352
