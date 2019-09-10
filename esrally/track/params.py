1eee2f4a4d1085cb5b6e1665128a8a8eaa783de3
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2019-09-10 17:19:46 +0200

    Honor ingest-percentage for bulks
    With this commit we honor the parameter `ingest-percentage` again for bulk
    requests. Before #763, the method `size()` was used to determine upfront when a
    parameter source is exhausted. When we changed the API then in #763 the
    parameter source's progress indication was instead done with the
    `percent_completed` property but this is only informational and the parameter
    source is required to raise `StopIteration` to indicate completion. This means
    that `ingest-percentage` was not considered anymore for bulks but instead we
    always read the input file(s) completely and then terminated with
    `StopIteration`.
    
    Relates #763
    Relates #768

--- esrally/track/params.py
+++ esrally/track/params.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9387c0e55b94c38583669ff76f95c25dd356b645a8147ae7b7f7b0df02eadf73
-size 40600
+oid sha256:7bb5ecad801d1c9ef5966c408517d33af262d9ab5796e51e3c61afc9b7ba645e
+size 40994
--- tests/track/params_test.py
+++ tests/track/params_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c87d6aa75a33834839aff58d865b51cda81331cda89012cfbea32970c1caac2
-size 69224
+oid sha256:6915fb25f198dfc3c78b428608e1778e34be00c7fdfbbf686be4f160d71e27ff
+size 70186
