b966bf17e58e4d654d27a9b7a899093499e493b5
Dennis Lawler <4824647+drawlerr@users.noreply.github.com>
2019-09-23 09:08:32 -0700

    Change NodeEnvironmentInfo from a telemetry device into a function. (#775)
    Minor refactoring in preparation for further changes.

--- esrally/mechanic/telemetry.py
+++ esrally/mechanic/telemetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba2d5e85cc253f26d574b9a8928b5ee72f87cfc81294c1cd5bae717571ea7619
-size 58686
+oid sha256:65581e1e62d1642bcfeb515e4aadd858220b8f5de68adfa00f7bea8cf85fc89d
+size 58444
--- tests/mechanic/telemetry_test.py
+++ tests/mechanic/telemetry_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a076964b26b5d5adb82c39b46c62f0c383ce38a26e2b6853ce4edce1f3253e31
-size 121763
+oid sha256:7a8158a7edec1598e1d3c1f6f1890fe22a02d85aedbe41752257e70c35c8704e
+size 121718
