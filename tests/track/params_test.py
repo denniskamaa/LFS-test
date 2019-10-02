5aac1d8eb340f16757d4413f5b56789ca72e4b84
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2019-10-02 14:18:37 +0200

    Replace deprecated assertEquals with assertEqual

--- tests/track/params_test.py
+++ tests/track/params_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6915fb25f198dfc3c78b428608e1778e34be00c7fdfbbf686be4f160d71e27ff
-size 70186
+oid sha256:0e3b7f06d01881fc6f896b8d9cd54edd8c8ae88a72dcc56a6c75460c9fbc595d
+size 70184
