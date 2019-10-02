54ab18435ec2a528aa81e39ded1cb7c7a156a7ec
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2019-10-02 14:12:28 +0200

    Fix parameter name in microbenchmarks

--- benchmarks/driver/runner_test.py
+++ benchmarks/driver/runner_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3a339134a1f23cd0e229fc036c6db418ba122afe189a754f41d1effeff21a10
+oid sha256:994a4f750653e5bd16e9f5e82e056f4be7baa65502f08077d4e1f463c75ec62b
 size 2504
