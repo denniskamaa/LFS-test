3f53dc62b2406f3e9faf59917dcbf0e219f865a6
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2019-10-08 14:31:00 +0300

    Update Elasticsearch version of targets and metricstore in CCR recipe
    Relates #782

--- recipes/ccr/start.sh
+++ recipes/ccr/start.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:821f535e2f1bac19dbc3e6ac281e8ef8d735cd9999ca3afb077e54266f97bb59
-size 2742
+oid sha256:e40aa9865302660440cb09e601ff3b79662928acd88fba04821619f58d1ac4ad
+size 2761
