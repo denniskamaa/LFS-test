15c81476d7846bf394a933e424079b270d1e0d84
Daniel Mitterdorfer <daniel.mitterdorfer@elastic.co>
2019-10-08 14:19:22 +0200

    Don't attach telemetry devices for Docker
    With this commit we don't attach any telemetry devices for Docker
    because this is not easily possible to measure properly without
    considering we're running in a container. We keep the underlying
    infrastructure in place so we can easily add new telemetry devices if
    needed.
    
    Relates #785

--- esrally/mechanic/launcher.py
+++ esrally/mechanic/launcher.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:668e377268edd7036e3f35c052a2200646f552d99080de6a6fed11a6c8901e2d
-size 10470
+oid sha256:4ad38fae40c9efa7fff50024f6b49790f501ed61ade0a83dda2a3cc55fef7aa3
+size 10223
--- integration-test.sh
+++ integration-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48d808ffca63440f3c0d1a48b1147b7faa23a5219900b4a4c034bb559ec14744
+oid sha256:77ac9d92d9bb40e772ecd72d252f2732f32736da17260bd5a6086dce09ec558e
 size 22110
