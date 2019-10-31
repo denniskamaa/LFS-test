ed68951dedff42bc14378a14cffb159abc1576f6
Dennis Lawler <4824647+drawlerr@users.noreply.github.com>
2019-10-31 07:32:34 -0700

    Enforce coding conventions (#805)
    Closes #794

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0423148db0e738aa57fba7e031c9511a88f1f345fdbe070942a9fa87a72c6dc1
-size 4308
+oid sha256:07e0467b93bc3034eeb84df4aeefe610cbe85e85da682d2c042e6170c774fe41
+size 4357
