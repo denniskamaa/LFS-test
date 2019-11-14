91ef5b46e20d890e474f40410fcb224aeceafcf0
Dennis Lawler <4824647+drawlerr@users.noreply.github.com>
2019-11-14 10:27:43 -0800

    Add fallbacks for ML APIs (#812)

--- esrally/driver/runner.py
+++ esrally/driver/runner.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26bdff3889de0b82ef27c4b503a024c95cead2aeb10f581c05c266c83e4233ac
-size 52308
+oid sha256:978cb8cbd8ba6d186a118ae260f002b7c70eca8bfd6127cce37f29d79147486c
+size 55690
--- tests/driver/runner_test.py
+++ tests/driver/runner_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10b2a792c322b1fcf7a16c119c769b08044d36878e6017b502e8f55b4c9749f8
-size 85439
+oid sha256:9f9fb6e75b3c1d06a00dae478c4896ecf48d33b6625cad6f0e08e0dc4b324b5d
+size 91750
