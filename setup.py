db32a71d84a2fd81d179c064be6e070aeef86058
Michael Basnight <mbasnight@gmail.com>
2019-11-20 13:46:14 -0600

    Upgrade psutil to work with newer linux kernels (#820)
    The 4.18 kernel introduced a number of new fields into the disk io stats
    proc fileystem, and running tests on new versions of linux is causing
    failures because psutil cannot interpret the new fields. This fix
    updates the version such that it works w/ newer kernels.
    
    Relates giampaolo/psutil#1354

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1cfac08635a18710ed5d5bae5ac7f716ce7f1114b13a3db93ffd39a9437f6ed3
+oid sha256:11dfafe15f32bd69adc5208f5579b3bb1370bb62f4690074eaa7e1a2967de1c2
 size 4408
