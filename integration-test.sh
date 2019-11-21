fce724ccf0905c3e48c33072d557ec74fbd3837f
Michael Basnight <mbasnight@gmail.com>
2019-11-21 13:23:13 -0600

    Change remaining it tests to port 19200 (#825)
    The docker tests were using port 9200 because target_hosts was not
    specified. Additionally the tests that were using compose were also
    leaking a 9200 port on to a default bridge network that other containers
    might be using, which causes a collision and failure. This commit ensures
    we use a non default network so this collision will not happen.
    
    Closes #824

--- integration-test.sh
+++ integration-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77ac9d92d9bb40e772ecd72d252f2732f32736da17260bd5a6086dce09ec558e
-size 22110
+oid sha256:52afcf309b1cef162abadbf9eb948c7366b9ce09d1545d6801ad38c02e41ce49
+size 22141
