33e0f704e7b91e9f83c548d47e84c9e83f97975d
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2019-12-04 09:11:57 -0500

    Upgrade boto3
    With this commit we update the boto3 dependency to the latest released
    version so Rally includes the latest changes there.
    
    Relates #836

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11dfafe15f32bd69adc5208f5579b3bb1370bb62f4690074eaa7e1a2967de1c2
+oid sha256:e40a672f135960b0d4803307272234de84d10c11b50cc9590e02cdcf32149b59
 size 4408
