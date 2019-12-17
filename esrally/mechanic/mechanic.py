9c719935eb381d45c2dc392a6469ef54c7b119ec
Michael Basnight <mbasnight@gmail.com>
2019-12-17 12:45:14 -0700

    Fix lint issue C0122 (#843)

--- esrally/mechanic/mechanic.py
+++ esrally/mechanic/mechanic.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1aa618deb51ebab7fa8eba6d5523a946cf4ef7269d5822f8bc399bc273cc4e82
+oid sha256:48314fd11379f0a294069a144dd38249c57e2acb3652b292016fdc7466400bf6
 size 30508
