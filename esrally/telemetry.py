fbc8fc0924570903880a34c8ca0298286ca95b0e
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2019-12-17 02:12:11 -0500

    Store Disk I/O metrics if available (#841)
    With this commit we split the check for disk I/O metrics for reads and
    writes. This avoids the case when one of these metrics is either `None`
    or `0`, the other metric is still added to the metrics store (this
    scenario might happen if data is only written but never read so the read
    counter is zero). We also change the check to test for `None` explicitly
    because we even want to add metrics to the metrics store if the value is
    zero.

--- esrally/telemetry.py
+++ esrally/telemetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:354547021f07c42d30b01d0cf071eaecec03d1f54c7b47ffd9e1f7d808a84c8b
-size 54672
+oid sha256:59fef78f381d43a6ee0371d3dc60b3d9cfd558806430157fcd4abecb234363d2
+size 54704
--- tests/telemetry_test.py
+++ tests/telemetry_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0ac40aa5d24b5994c9a5ed68d4393609d0f4e8b47c5a05dc47aac1267c42afb
-size 116313
+oid sha256:1efce501345c193f4c3f967fc6b8f469a3bc9001c43dd04db0579fde67ab79f3
+size 117738
