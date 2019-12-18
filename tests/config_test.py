5fca5edcb1103a9aa597d39aa0ede90c4c550983
Michael Basnight <mbasnight@gmail.com>
2019-12-18 11:08:21 -0700

    Fix lint issue C0305 (#847)
    Relates #838

--- tests/config_test.py
+++ tests/config_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c64b95bfc842a59f5100353283f5f6a90abaffd1906ec07715047ea7c4acf66
-size 34904
+oid sha256:a88889d0d722284a4f6db76071b5f296005afc3d9a956eb6aeb9e855d3e8e0c0
+size 34901
--- tests/utils/versions_test.py
+++ tests/utils/versions_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90eb503a76242ab69b4e57de0b7c6f4e46d695bd16f220be5eef99ce08439aa4
-size 4328
+oid sha256:914ba5f9bad40a2bbf6610f0271de702b6737cb2419c5939f99f4cd1c34e84f0
+size 4327
