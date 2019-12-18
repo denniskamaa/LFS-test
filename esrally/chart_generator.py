6d1b53f20eb1d39450b41ff737c025a3ded88890
Michael Basnight <mbasnight@gmail.com>
2019-12-18 11:08:30 -0700

    Fix lint issue W0102 (#848)
    Relates #838

--- esrally/chart_generator.py
+++ esrally/chart_generator.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eac90e54f573f1340c0608326bcb9189417a6f35ba95725c83cc3004087216d3
-size 63915
+oid sha256:2fee26276276f8c14caaaeb80e90cf0b1713cac344a7cff51fc1c0acc01cfac4
+size 63966
