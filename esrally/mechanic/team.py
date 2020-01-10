774d65f194089e8146d4466c78c567f0bbf2a10c
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2020-01-10 12:01:09 +0100

    Pass plugin params for all plugins (#861)
    Previously plugin parameters were only passed to the plugin descriptor
    in certain configurations, especially Rally did not pass plugin
    parameters for core plugins. With this commit we ensure that plugin
    parameters are passed to all plugin descriptors.

--- esrally/mechanic/team.py
+++ esrally/mechanic/team.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50bae908804c9f8597e136374239abf91551ce504a2ae3606c3b803de9a0a87e
-size 22357
+oid sha256:c03401cae8786a546c353313a3c7411019ca30cfc6bbdc20cbc416e298bc2bbe
+size 22555
