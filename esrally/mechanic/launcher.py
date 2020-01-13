7e84073602011c1cd4bd622f5fd15886fe4ff6ac
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2020-01-13 07:35:39 +0100

    Detach Elasticsearch on startup (#859)
    With this commit we detach Elasticsearch on startup from its controlling
    process via the
    [setpgrp](http://man7.org/linux/man-pages/man3/setpgrp.3p.html) system
    call. Although we start Elasticsearch with the `-d` flag, this is
    necessary to ensure it does not receive signals like `SIGHUP` from its
    original parent process (Rally).

--- esrally/mechanic/launcher.py
+++ esrally/mechanic/launcher.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9581a9485ce8eb4adb658b922821cb876fd87fc4229193260350d16e54bd8ee
-size 10058
+oid sha256:30de9969dbc12e021cfa17477dc358dc4b341e190e8234c994a60f1a8c7ab4f2
+size 10071
