977e99a6e15d269b2313c1c109fb5aed2ca29ac9
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2020-01-13 11:52:31 +0200

    Allow piped stdin in run_subprocess_with_logging (#863)
    Sometimes we need to use as stdin the output from another command
    (e.g. pass secure settings to elasticsearch-keystore tool is
    one example).
    
    This commit optionally allows passing stdin from another subprocess
    to run_subprocess_with_logging.

--- esrally/utils/process.py
+++ esrally/utils/process.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6aa4e029668fca272e3935683f2c86ad5c585c99385c1986245caf15b58ebde6
-size 5027
+oid sha256:11e805c6c7fbec8fb053b7407f5415a39927f53d96d2c5450c693ead3f6cbe1d
+size 5188
