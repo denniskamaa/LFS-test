f55adbaeefd0186e19fc89e0a4171122719fd26a
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2020-01-15 20:20:29 +0200

    Really skip setup phase in IT tests during docker release

--- integration-test.sh
+++ integration-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f804b5d1c32b26d925b0fa2d572e796fc13f24d04edf0a1c20eacde0bbac881
-size 25461
+oid sha256:03e17512b09bf59a4dd2d5c9184b166c27f15b7260d3de968c1eb74d541bd800
+size 25467
