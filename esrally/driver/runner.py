bdc9bc4e3ad052ce72f8712606ca826ab6acece8
Dennis Lawler <4824647+drawlerr@users.noreply.github.com>
2020-01-16 06:59:21 -0800

    Fix W0143 (#867)
    * Fix W0143
    
    Open false positive, see https://github.com/PyCQA/pylint/issues/2306
    
    Disabling warning, so we can re-enable if/when fixed.

--- esrally/driver/runner.py
+++ esrally/driver/runner.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48237ff28d2e0e6b00d900ed2610e0364a67b2417f1433747c8355aab11a145d
-size 56135
+oid sha256:ed89fd7bcbbdd4dba4274d015dee6102f3c7e7e2a3c8dc65a591e8b0e010fd17
+size 56198
--- esrally/track/track.py
+++ esrally/track/track.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ecda7b6a5f9a23f2fd0794cca3a256867052acaf7087e81fd914153be9fc45a
-size 25508
+oid sha256:619f55d86f147b3080e50244a7c1b0e3542cfc3d9f94418b99208d7474bfe3e6
+size 25559
