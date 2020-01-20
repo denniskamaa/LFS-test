6d100066360c52d9af86f2ff39554d9f3a4fd645
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2020-01-20 18:33:17 +0200

    Adjust IT tests for elastic/rally-eventdata-track#67 (#876)
    In https://github.com/elastic/rally-eventdata-track/pull/67 we
    consistently switch to number_of_shards from shard_count, so adjust the
    IT tests accordingly.

--- tests/utils/opts_test.py
+++ tests/utils/opts_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:477c9024f7dfcdaa3cb52152abf7a10a6d76a22c5e1f434d023ce6ce5dc5c9ff
-size 10997
+oid sha256:fc51d3d25553de6584348d9c55e2e422cbcde787d2c676219b7e6f6fd50d9d20
+size 10970
