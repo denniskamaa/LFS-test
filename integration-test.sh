edfa93a6707d960634d021934372a8e3e8d18b04
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2020-01-28 09:01:18 +0100

    Show recent log output on integration test failure (#880)
    When a CI failure occurs in integration tests we usually have very
    little output to analyze the failure. Furthermore, CI workers are
    ephemeral so we also lose access to logs. With this commit we check
    whether there was a failure and dump the most recent Rally logs.

--- integration-test.sh
+++ integration-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03e17512b09bf59a4dd2d5c9184b166c27f15b7260d3de968c1eb74d541bd800
-size 25467
+oid sha256:626bd6fe85a6b10a2606cdb0cbdb9524cabe13479d7b466b7e68be1e0b9eade6
+size 25814
