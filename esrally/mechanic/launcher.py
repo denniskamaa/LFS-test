8cbd4ab15a19db50e90cc70d8c228b4043d949fc
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2020-01-30 15:54:33 +0100

    Detach ES using Python standard mechanism (#884)
    With this commit we use the parameter `start_new_session` to detach
    Elasticsearch from its parent process group (Rally) instead of the
    problematic (and low-level) `preexec_fn`. Under the hood this will use
    the `setsid` system call instead of `setpgrp`.
    
    See also:
    
    * https://linux.die.net/man/2/setsid
    * https://linux.die.net/man/2/setpgrp
    
    Relates #859

--- esrally/mechanic/launcher.py
+++ esrally/mechanic/launcher.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:884419d561f67358947f51753401233763a11effbb2d89461d732a7db8e14c54
-size 11254
+oid sha256:04bda9efc05165230ec7e3dd496b0bf0485ca52d8b0ad7c1ab4c598456c6aff6
+size 11201
