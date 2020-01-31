87466d472dbcda87f44a818229c7edd959b59387
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2020-01-31 14:21:34 +0200

    Fix race condition with old ES processes in IT tests (#886)
    Occasionally IT tests fail in a very subtle way when Rally spins up ES
    shortly after a previous test: while the previous ES process gets
    killed, the socket lingers and when Rally spins up the new ES process
    for the test, ES detects the used port and binds itself to the
    next one (e.g. 39201, for the HTTP API).
    
    Wait until the socket used by ES HTTP API is really closed across IT
    tests.

--- integration-test.sh
+++ integration-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:626bd6fe85a6b10a2606cdb0cbdb9524cabe13479d7b466b7e68be1e0b9eade6
-size 25814
+oid sha256:eb16148595cd56f7e5f2435e3d310d7c07fc70849354d9db32c7388d3adfb3d9
+size 26099
