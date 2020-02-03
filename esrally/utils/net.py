d8c30696afc4b1f87549cb10f71f676399f95c08
Dennis Lawler <4824647+drawlerr@users.noreply.github.com>
2020-02-03 09:49:09 -0800

    Fix issue with fetching test-mode -1k document corpora. (#885)

--- esrally/utils/net.py
+++ esrally/utils/net.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6868e48f07e2f2e2ab657d9351c32576d1a82750a1c23931eb44a769eac6c97
-size 8867
+oid sha256:1a74e3b971ba67b30cc99704ced0e53efdd7beaeef69394062e52852b766502a
+size 9059
--- tests/utils/net_test.py
+++ tests/utils/net_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95095e7a2741ba011028adf26ef2b35f2e92b3ab6a402fcaa9cd8f83e507cda9
-size 1657
+oid sha256:00114d0e2e4fed324cdb4ce4589da7072c3661519c25fe71b685f7caa34ce1c5
+size 1969
