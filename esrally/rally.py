044c7475e2371a30845960753ae854889e2c7c54
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2020-02-14 07:11:57 +0100

    Log race id on startup (#895)
    We have introduced the race id a while ago to uniquely identify a Rally
    invocation but we don't write it to logs which makes it harder than
    necessary to understand to which race a certain log output belongs to.
    With this commit we write the race id Rally's log file upon startup.

--- esrally/rally.py
+++ esrally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77a93bb7302f1705aa370eda6b3ab662523eab4d588d4f2beb1a6fbde8af2f20
-size 44771
+oid sha256:00f370cc50e0c3ace82409d5acac45a4a1d04a4005805cf42311afdb917dac81
+size 44817
