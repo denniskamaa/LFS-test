a9fbf99c1707d67ecb9cde4c2e5bb28290aa1e5b
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2020-02-17 08:00:10 +0100

    Only clear  metrics store on benchmark end (#893)
    With this commit we only reset the `metrics_store` field in the driver
    instead of completely removing the field from its class. This makes
    Rally more resilient in failure scenarios, e.g. when failure happens
    upon shutdown of Elasticsearch, which leads to the driver's `#close()`
    method being invoked, which in turn attempts to access the already
    removed field again.

--- esrally/driver/driver.py
+++ esrally/driver/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5dd80e484ff6d69e68a163e15879b586b73daae930dd598ce83946d6f792af13
-size 70216
+oid sha256:709f0080a44819b42a399b9153977d9e42cbfecd15b69e2086812ec7e111616b
+size 70219
