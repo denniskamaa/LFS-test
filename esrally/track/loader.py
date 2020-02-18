a75da0fa5f59a4391ba4ec902ab5efe910e3f092
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2020-02-18 08:19:55 +0100

    Show more user-friendly track loader errors (#903)
    With this commit we add more details when Rally cannot load a track. In
    all cases we include the path to the completely rendered track file. We
    also detect syntax errors separately and provide the specific context in
    which the error happens.

--- esrally/track/loader.py
+++ esrally/track/loader.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:afe7a4179da5d63ffd839a03a86f6363dbee18ced43b347e67047c6a6eb948ff
-size 64252
+oid sha256:0dcb15f8950b610d8271e7d5d74c20a6f359f08ee375035042925426b692a918
+size 65230
