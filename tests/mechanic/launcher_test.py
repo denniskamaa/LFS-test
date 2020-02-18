c10ea73c362bbd3e0d04064fd12d44a2a4235ebc
Dennis Lawler <4824647+drawlerr@users.noreply.github.com>
2020-02-18 09:32:39 -0800

    Fix race in wait_for_pidfile. (#901)

--- tests/mechanic/launcher_test.py
+++ tests/mechanic/launcher_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e3bf3cd3bdcc053b5c5c366f7e01ef0487f86a0ff26271ac9df8c648d3750c64
-size 12926
+oid sha256:0cc3f8e5b00dd739461f4210bb82ae637dd051afd6bd738b5096ea52bea5129f
+size 14654
