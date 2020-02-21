cfddf98e6e9ea7e9b2934eab0402d5a59514eb12
Dennis Lawler <4824647+drawlerr@users.noreply.github.com>
2020-02-21 07:59:54 -0800

    Normalize handling of --preserve-install (#913)
    We have --preserve-install as an argument for 2 subcommands, `race`
    and `stop`.  In `stop`, it is an argumentless flag, but in `race`
    it defaults to a boolean value but expects an argument.
    
    I normalized the argument so it is handled the same in both cases,
    as an argumentless flag.

--- esrally/rally.py
+++ esrally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8496e9afd68fa05923492b98828e7c04c8f87973926b2958bb5039a8cee5451
-size 44937
+oid sha256:495dbcc8a770b19ce84b4b6d789dcb0c832cb62bcfd49fbae58bcda46d259160
+size 44970
