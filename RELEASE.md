618c0f79db9d2d4a72d3c59ed24b70044049c91b
Dennis Lawler <4824647+drawlerr@users.noreply.github.com>
2020-02-28 08:30:52 -0700

    Improve release docs and IT prechecks (#914)

--- RELEASE.md
+++ RELEASE.md
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f172510db6a0c039c90daa4e97553a97cbfcd8b62a6d4246b0f5bf0c9902c35
-size 2482
+oid sha256:2aa728e6048cb463d3333f2daca00d1e3e3ee63bdd8e17af40f24df479d77060
+size 2582
--- integration-test.sh
+++ integration-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cdac2abd0d90b1a5d83ee49913eae7ca9f8f59f953514b67697ad5e224113b6a
-size 26241
+oid sha256:9efa714613cd6ecb8779f4625e4f847863d4da484ccc7ead7d1f4b1c75f2a8e1
+size 26649
