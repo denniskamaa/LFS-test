72226feb7c9cab093eb54453d40c5e8cdd954330
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2020-03-05 07:08:11 +0100

    Add unit for all query charts (#927)
    With this commit we add a proper unit to all time series charts for
    queries.

--- esrally/chart_generator.py
+++ esrally/chart_generator.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b239b41c114d006fd32263e39e1816af9b277c70ca34fda21e1cbf4b28fd500
-size 64557
+oid sha256:2df47927eb97fc43c8bf6fe57d82d40d913a2db685755eab887597d832bd50d1
+size 64793
