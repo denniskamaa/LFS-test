4b7490e1fc1cfbfc540e9ee1c3bd28a63593dc38
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2020-03-10 15:49:46 +0100

    Improve layout of generated dashboards (#932)
    With this commit we adapt the dashboard layout in the chart generator
    that we also use in our nightlies, namely that the bulk-indexing charts
    are wider than all other charts.

--- esrally/chart_generator.py
+++ esrally/chart_generator.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2df47927eb97fc43c8bf6fe57d82d40d913a2db685755eab887597d832bd50d1
-size 64793
+oid sha256:92a482b9a5893515f76a4ca5e400188d378fdc1d39ddc9a06ea6009f20df3634
+size 65268
