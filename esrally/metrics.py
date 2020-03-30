c926d8d9157a57ace700b861edef141a10fbdfa6
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2020-03-30 08:47:13 +0200

    Store units in results (#938)
    With this commit we store units for all operation-related samples and
    system metrics also for summary metrics.

--- esrally/metrics.py
+++ esrally/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a832b1958a4349caaddbf93bd70b537cb7e6e225d0913720ad7091ff21a35fc3
-size 85031
+oid sha256:6298c47f21c9f54dc96700d7e8b4e753abbf851fd55f7d1218f433711cd73660
+size 85390
