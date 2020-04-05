f95447b1f4d78e93133b4e0be34b643d784b362b
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2020-04-05 08:48:37 +0200

    Upgrade Elasticsearch client to 7.6.0 (#949)
    With this commit we upgrade the Elasticsearch client to the most recent
    version 7.6.0.

--- esrally/client.py
+++ esrally/client.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e1ed022e4ce879e76c0c2a5e7d6f21d95b6692a01b74d2b6ef348a4c40ce75d
-size 13625
+oid sha256:70f6bbd78b167c9c02192866770e6114e7d192a3a2f675c972e2edadf4c593c2
+size 14610
--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb9704281bdb3ca237d1257e3d8aee0c18d285d1d300fde00f2712ebdf350ea7
+oid sha256:3e7835ff7d3219151335ceb6c2c94722f1ec799bca13ee4bf5cc6a3c6512a4f8
 size 5898
