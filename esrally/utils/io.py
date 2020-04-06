69a3fb5783f521cd5b9abf07593dcfddac985a61
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2020-04-06 15:54:25 +0300

    Use pbzip2/pigz to decompress corpora if available (#947)
    Decomporessing large corpora using the standard bzip2/gzip libraries
    can be a slow process as they only utilize one cpu core.  Take
    advantage of pbzip2/pigz, if available, to speed up the process by
    taking advantage of all cores.

--- esrally/utils/io.py
+++ esrally/utils/io.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1dcc59690fed052f84c3692065cabb994c8657ade1d1f021458aadbaf0f0d58
-size 17593
+oid sha256:249732ac40d139c755731cc816f0fbff00d076ab4c75887c79c656770b6dcbf1
+size 19421
--- tests/utils/io_test.py
+++ tests/utils/io_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:554cc53aaf2dd5b5cb0605a86166ae6e67aa32a2dd6ba8e8121634e85a26aaca
-size 5212
+oid sha256:6705807ce24dfb9609138e42fe9d98f8881f6490a445d8cffb0010f3337ab38d
+size 7482
