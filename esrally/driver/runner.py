f02a60349659947e1ac93d45c1f6a191726f7f9d
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2020-04-14 08:55:05 +0200

    Fix fallback implementation of ML-related runners (#956)
    With this commit we fix several issues of the fallback implementation of
    ML-related runners. This implementation is only used for older versions
    of Elasticsearch which still include the `_xpack` path component and use
    the raw transport API of the Elasticsearch client. However, especially
    the handling of request parameters did not conform to the API
    requirements.
    
    Relates #812

--- esrally/driver/runner.py
+++ esrally/driver/runner.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c4ab9b900a5028f8294b3a0d47ef47a1343c56c1cdf78ca3f2274ac5b330aa6
-size 63855
+oid sha256:901f9d4f639ab60eb07b9151003545b6b6026e872440609f825dabe010916d25
+size 64603
--- tests/driver/runner_test.py
+++ tests/driver/runner_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52dbc872b968a5161b9464792c8ce2af3ea8f57defb2cf404c0b93d492beaf7f
-size 112490
+oid sha256:2701a8e743e043d136bcd764fd84e016beac185c6d133a9ac12dd4eb63bfdf2d
+size 113000
