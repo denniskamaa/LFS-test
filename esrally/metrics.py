339b4d95ec1fd0100faaee77890c8e401e564d13
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2020-04-15 17:26:41 +0200

    Increase bulk size to metrics store (#962)
    With this commit we specify an explicit bulk size (called `chunk_size`
    by the Elasticsearch Python client) to improve performance when sending
    large amounts of metrics documents. After experimenting with different
    bulk sizes we have settled for a bulk size of 5000 documents per bulk
    (the default is 500 docs per bulk) which improved throughput by a factor
    of 2 in our experiments.

--- esrally/metrics.py
+++ esrally/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c79c5df172c229d7387615219e08a445cc302b51723ce6099a596bc6ee1ab560
-size 85534
+oid sha256:e54bee45084ed3923f6a25e8ddeb73067cec9237ea4106239a2f841ece83af31
+size 85568
