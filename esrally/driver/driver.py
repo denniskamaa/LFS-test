7760dd261dae9b5222b6ac55117fb92be2fde6df
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2020-04-16 11:11:41 +0200

    Determine most recent sample per client (#961)
    With this commit we properly determine the most recent sample per client
    by looping over all clients instead of extracting only one sample. The
    reason this was working before is that each processes only simulated one
    client but with #944 each worker can simulate multiple clients so we
    need to check all samples to determine the most recent one.

--- esrally/driver/driver.py
+++ esrally/driver/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49c802c8f41b8cd8abb8f687073bd6f262c11c3e1fe606491a909d382151ac62
-size 78811
+oid sha256:13ec5c2b39d3f080669495cac76b70161a3d3c422781b9a2b5fa6dc4ff012f93
+size 78761
