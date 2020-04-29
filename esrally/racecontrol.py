2035aae21aaa3da31ef1a60f701248dc292adf6d
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2020-04-29 09:05:47 +0200

    Remove warning about bogus results (#973)
    With the introduction of dedicated subcommands for cluster management it
    has become more usual to use the benchmark-only pipeline. Thus, we
    remove the warning about bogus results now.

--- esrally/racecontrol.py
+++ esrally/racecontrol.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40beb04eef83231a8605be2db69d6c538c51e82894745fd6bded5a364fa5d338
-size 18923
+oid sha256:6411c6e64a012248a0e3870c8c5ded011ad0d9459fe3debe5e20b230ba79f676
+size 17914
