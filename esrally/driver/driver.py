65360960c07a952304b6485be36792593375e49e
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2020-05-04 10:48:19 +0200

    Assing worker-id globally (#983)
    With this commit we assign the worker id globally instead per load
    driver host. This ensures that worker ids are unique even if there is
    more than one load driver host.

--- esrally/driver/driver.py
+++ esrally/driver/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:67799d6979a99db5990fcc843ea9556e147c07230336dcf340bab251c8853098
-size 79908
+oid sha256:b5ac272d835d0eea3299f5dbebc302e4d2174a4d5f50e94e11b038294ded0b61
+size 79943
