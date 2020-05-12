218f806b16ff730edabcbfd6f35140e76031b525
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2020-05-12 13:44:45 +0300

    Don't use range for http and transport port (#995)
    Currently we use ranges for http and tranport port. This is error prone
    if there a left over cluster forcing the Rally launched ES node to
    automatically pick the next free port.
    
    Use a specific port for http and transport port.
    
    Closes #940

--- esrally/mechanic/provisioner.py
+++ esrally/mechanic/provisioner.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc2cc47fa7e13900ef53f7c9c1ba37f46fc2753d40dcc4e09150e0a53453ad23
-size 22611
+oid sha256:ba1ae2171c8924f77e5e3ef6949089b690bce7c24059122fdf7818ec0acb33fb
+size 22495
