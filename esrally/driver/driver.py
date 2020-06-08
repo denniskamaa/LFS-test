caf645e2494ff744dd2829e025a76c4e0c70fa3d
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2020-06-08 15:15:57 +0200

    Avoid redefining arguments (#1010)
    With this commit we reenable the pylint check `R1704` which checks
    whether arguments have been redefined as local variables.
    
    Relates #838

--- esrally/driver/driver.py
+++ esrally/driver/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27defacb9148fc7eeb819795a0bcad60538f787965aecac9851931f9525c9e8b
-size 81675
+oid sha256:c0edca1b7139786ee1126d3e23c7b03c3d18719b08e6b2b3c1a5a4c2d1d51d0c
+size 81646
