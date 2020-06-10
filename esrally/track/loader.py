397a9c80855555a1da3e9bd25e9d5227e7367b4e
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2020-06-10 10:30:09 +0300

    Make exists_set_param macro available to tracks (#1012)
    Frequently it's useful to specify a setting based on the presence of a
    track parameter. To avoid repetition this commit makes a Jinja2 macro
    called `exists_set_param()` available for all tracks (if they import the
     rally helpers).

--- esrally/track/loader.py
+++ esrally/track/loader.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6acd3c5db968727180e3b51c009443eb4c50b4acbed098258b792f8de7b98a33
-size 65284
+oid sha256:a609e1d34a9080d2767b5fa48542fca809649dfbaf6f0e6dd8843eaa8f6c7865
+size 65844
