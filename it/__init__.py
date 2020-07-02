e8f077762208a67b27d4685194d4de0a2bbe192e
Michael Basnight <mbasnight@gmail.com>
2020-07-02 10:59:57 -0500

    Migrate configure test to new IT infrastructure (#1023)

--- it/__init__.py
+++ it/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b6c3ad04b4ff07f98795f5e28e72d54856a8824de9c823e8eae8bdf9be1c634
-size 7549
+oid sha256:49d216c58e38712ed85d2ac09fff933dad0df0d54ee967ef0393621c33f14b81
+size 7582
---
+++ it/configuration_test.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:583117684e31480200a09af7343e90beef5112e4e35ab2a8c312ef6dae2b8955
+size 1157
