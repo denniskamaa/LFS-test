82d3ccdaf84ef5455f1f41db159ca4ad20e23c1b
Vitor Anjos <bartier@users.noreply.github.com>
2020-07-20 03:01:21 -0300

    Migrate Docker tests to new IT infrastructure
    Relates #975

--- it/__init__.py
+++ it/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49d216c58e38712ed85d2ac09fff933dad0df0d54ee967ef0393621c33f14b81
-size 7582
+oid sha256:6ff0aad27326cbbe7fdd0918719639b6e7503910627dd176891da3c58016edb1
+size 8408
---
+++ it/docker_dev_image_test.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f44cb9091c9fb0cb3c23e88d9e85ac8edc79a1cc7ce3d080a94c6e8f67908617
+size 2527
