9d1dcd3826dca49c468848fed9ed0ca06a8564e9
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2020-07-28 06:55:48 +0200

    Improve Python 3.8 compatibility (#1030)
    With this commit we upgrade two third-party libraries (py-cpuinfo and
    tabulate) that produce the following deprecation warnings:
    
    ```
    .venv/lib/python3.8/site-packages/tabulate.py:415
      .venv/lib/python3.8/site-packages/tabulate.py:415: DeprecationWarning: invalid escape sequence \[
        _invisible_codes_bytes = re.compile(b"\x1b\[\d+[;\d]*m|\x1b\[\d*\;\d*\;\d*m")  # ANSI color codes
    
    .venv/lib/python3.8/site-packages/tabulate.py:977
      .venv/lib/python3.8/site-packages/tabulate.py:977: DeprecationWarning: invalid escape sequence \e
        """Format a fixed width table for pretty printing.
    
    .venv/lib/python3.8/site-packages/cpuinfo/cpuinfo.py:492
      .venv/lib/python3.8/site-packages/cpuinfo/cpuinfo.py:492: DeprecationWarning: invalid escape sequence \d
        if re.match('^i\d86$|^x86$|^x86_32$|^i86pc$|^ia32$|^ia-32$|^bepc$', raw_arch_string):
    ```

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7bf2c7b14253c91d1fdb5590ff15cd71b4494b591b289b2753f8a9cae839f557
+oid sha256:a964aae7f4bc1488933ec82c73beb397e3356bc40abea0a1edcc0764a0329622
 size 5835
