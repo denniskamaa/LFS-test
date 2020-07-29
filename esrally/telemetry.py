0376ee288c5f87d91f9ca91c810b3f87197ec2a9
Michael Basnight <mbasnight@gmail.com>
2020-07-29 11:55:57 -0500

    Add indexing_pressure to nodes stats (#1041)

--- esrally/telemetry.py
+++ esrally/telemetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bcc80d0bcf4e11ec418dab7a630f4509dd974b6fd85fa69b3be170e6076253dc
-size 66074
+oid sha256:c45b2c413d44bdf22783b274867e3355775d331966a733c6b23ed31c4ddf8ecb
+size 66485
