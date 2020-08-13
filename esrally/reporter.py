1dea839c16478a94b40c8dfad90bfa531c566e97
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2020-08-13 07:27:19 +0200

    Show processing time also in comparison reports (#1045)
    In #935 we added a new metric `processing time` to the command line
    report that can be used to determine Rally's internal overhead. While
    the regular command line report shows this metric if configured in
    `rally.ini`, the comparison report did not. With this commit we also
    show `processing time` in the comparison report as well.
    
    Relates #935

--- esrally/reporter.py
+++ esrally/reporter.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0249a88e026c78e53f60a67d57df20819f24423a5cb9dce37281fa4b694a598b
-size 32245
+oid sha256:d3ab26d98e212f5165d88e49d4c2500366e9fbc46bdb28dd87a290a4b0235174
+size 32965
