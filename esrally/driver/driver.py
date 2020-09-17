f78ad91bf9cf377f136a2fac0e0074ba0d737e28
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2020-09-17 10:12:30 +0200

    Include task name in log statement
    With this commit we log the task name for tasks that have completed in a
    `parallel` structure because the main task has finished.

--- esrally/driver/driver.py
+++ esrally/driver/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6b5ad2118d4b5fa8cc45c444e350d5d07f669eebbd822d9da582e24eff2519f
-size 84287
+oid sha256:3234b11c809217a9f08c4b72a1df546cf5531be3ee9458fd12c339182b342821
+size 84303
