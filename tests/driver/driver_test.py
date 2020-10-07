e1c995a268db5eaf8deb45e998f3e96293b04a02
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2020-10-07 17:38:19 +0200

    Ensure that request error meta-data is string (#1075)
    With this commit we ensure that the `error-description` property that is
    added to request metadata on errors always contains a string. This
    avoids serialization issues later on when these objects are written to
    the metrics store.

--- tests/driver/driver_test.py
+++ tests/driver/driver_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:892989886ac7dd2d4b1e85609de60716ca897311cddc19f8ac8f8de984926584
-size 68143
+oid sha256:3a23d9cd6b044c7010a625213ae958e227d48b98c25312bbd3f5a3d5da302046
+size 68838
