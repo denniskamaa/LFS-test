ca3a67c7689883410f75fd46ebc6bbdde37e7774
Michael Basnight <mbasnight@gmail.com>
2020-10-14 15:32:04 -0500

    Avoid using exit() or quit() (#1077)

--- esrally/rally.py
+++ esrally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c0b8d3017dbb82c00586939338ab61da8d05b5bece596cceaea6ec6e03d42be
-size 47719
+oid sha256:53bd73319e88f807d1e048360139435837c55e4ba640ff2e5d1fee2216c1db6f
+size 47735
--- scripts/analyze.py
+++ scripts/analyze.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2324c829dcfbb69b6ed3bd83451989217be2dd38369cfc9e9c88876fc6e9a179
-size 7769
+oid sha256:ee0c11d68c7e2cb6bb000d248e3712e5c4dce7647a66e608a30c9a177c5cd620
+size 7773
