a37109831278e6b1b19fe8a12c004fec7c23fdbb
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2020-10-22 11:42:23 +0200

    Upgrade google-auth to latest (#1097)
    With this commit we upgrade google-auth from 1.21.1 to the currently
    latest version 1.22.1.

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18b48ead1702a8c87fdbd6ec5051af3699aa04bcc35d656579e4a55c5e27a704
+oid sha256:ce97bc262af78210ae3f89203a765f0acf2bc9c24496153c4eaed0a7bded07c7
 size 5939
