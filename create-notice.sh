f2b899b02d2a9ca6c4cea719b143b40de4f254e7
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2020-10-27 10:45:36 +0200

    Fix failing create-notice.sh
    Julian/jsonschema has moved to main for the master branch.
    Adjust create-notice.sh accordingly.

--- create-notice.sh
+++ create-notice.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5905a90e07945c1f67c77fc8e1f53bb685b3a0a92d5774898ad124156c7e9c25
-size 4626
+oid sha256:02646400ea0c80abebb0611ed3d75cd3f038f27ed91334691ee107368d62e8ea
+size 4624
