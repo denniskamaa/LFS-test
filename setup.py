34b62c0e04834858c8cd7303f58c8297ddb34f5b
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2020-11-05 09:47:40 +0200

    Add CI for Python 3.9.0 (#1096)
    Closes #1074

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce97bc262af78210ae3f89203a765f0acf2bc9c24496153c4eaed0a7bded07c7
-size 5939
+oid sha256:22f9e114be226d8d8c74fc0b4c049af1730d5bab35a863e29f23c80033ffd2d6
+size 5947
