fb9de4b7cb2f963819e6f992f16ea4f0c8006dcd
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2020-11-10 07:28:38 +0100

    Add support for FOSSA (#1108)
    With this commit we add support in Rally to run license checks on third-party
    dependencies with a separate tool called `fossa`. Note that this work is only a
    preparation on the Rally side. This is intended to be run in CI and needs to be
    supplemented by a corresponding CI job.

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9136bfec23f23e31925f81e6b25202df7aba989ba7b792a449f08cf83dcf270
-size 2336
+oid sha256:018cc9eb2eb7bfbacb2ce73589eddb91cd8fdec2add40b7d695057447a83624a
+size 2670
