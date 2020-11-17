b41e2db54c275fecb89506243741e1f078ea0321
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2020-11-17 13:45:58 +0100

    Make chart generator aware of EQL
    With this commit we add a workaround to the chart generator to recognize
    operations of type `eql` as throttled tasks (requiring a latency /
    service time chart) even if they are not throttled.

--- esrally/chart_generator.py
+++ esrally/chart_generator.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c23b964c87a4728fa5e88c31bd10f8c045063a89cc8dea1eaab873d418d79e4
-size 65235
+oid sha256:eb5cb616c14f9e9d3ec44895fe72148ee2f57c0441fc0a4b18d22db58594077c
+size 65877
