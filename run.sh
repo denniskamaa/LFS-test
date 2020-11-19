30d226baf1bfbde52d520718f31aa7993e220573
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2020-11-19 06:50:00 +0100

    Error wrapper script on unclean local state (#1115)
    With this commit we exit the Rally wrapper script with an error if the
    local working copy contains uncommitted files. This avoids skipping
    automatic updates silently if uncommitted files are unintentionally
    present. Users that still want to continue running can do so by
    specifying `--skip-update`.
    
    Closes #911

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64dda7c220077810902cfa3001e9190bea0ed526e5e5ee9f9049bdab08214aef
-size 5095
+oid sha256:52f46fd6355ad7a5e43d56d5a50f3e01fb6876d06292c590271d125a26f03400
+size 5159
