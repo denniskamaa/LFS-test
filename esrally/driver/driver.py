83de00316732e7eccdecfcd8b18f540416df430a
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2020-12-17 14:15:33 +0100

    Add a configuration file reference (#1137)
    With this commit we add reference docs for Rally's configuration file
    `rally.ini`. We also move one configuration property from the `system`
    to the `reporting` section as it is more appropriate there.
    
    We intentionally placed this information on the existing configuration
    page instead of creating a new one. We did this to provide continuity in
    the future because we intend to remove the dedicated `configure`
    subcommand and instead rely on users editing the configuration file
    directly. When we remove this functionality, we can also remove obsolete
    sections from this page and move it to the reference documentation.
    
    Closes #991

--- esrally/driver/driver.py
+++ esrally/driver/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2010fe514afa05e456fb7c2e7437053d9aae9dfdaa5e34fd72a48748e6a5a52c
-size 85250
+oid sha256:6be2b4dbd4e7b10bf3a348cb47ae92db1509b14dcf0afe50ba661790449d7402
+size 85253
