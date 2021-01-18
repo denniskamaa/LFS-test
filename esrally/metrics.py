dd3db94be67c3b20012f7c9d97c7e898120c3393
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2021-01-18 14:21:07 +0100

    Only consider race id when loading a race (#1153)
    With this commit we drop the `environment` filter when searching a race
    by its it in the Elasticsearch race store and only consider the race id.
    The previous filter was a leftover from the times when we did not use a
    race id but only a race timestamp as the identifier of a race. In that
    case it was useful to also filter for the environment but as the race id
    is unique we can drop the additional filter. This is also consistent how
    the Elasticsearch metrics store works (see
    `EsMetricsStore#_query_by_name`). With this change it is possible to use
    the `compare` subcommand to compare races from differente environments
    as long as the results have been written to the same Elasticsearch
    metrics store. Note that we intentionally don't change the filter that
    lists races; it still considers the environment name in order to ensure
    we only show races that have been run in that environment.

--- esrally/metrics.py
+++ esrally/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f1ba1d70cf25ac0e369fc302f7769c04a35fef551fb8e57d47a66d386c95776
-size 81328
+oid sha256:8b4703f8ab3ecb858a3254b20521447e83c0d837dbc8853dd1f39ec81caba651
+size 81265
