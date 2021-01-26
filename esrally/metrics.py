064a9b8fd7635d215f1e10ff765845db9a70226f
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2021-01-26 16:43:30 +0100

    Include empty mean value in summary stats (#1160)
    With this commit we add `mean` also in summary stats when no value can
    be derived. This is consistent with the handling of all other summary
    metrics and ensures command line reporting can rely on stats keys always
    being present.

--- esrally/metrics.py
+++ esrally/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b4703f8ab3ecb858a3254b20521447e83c0d837dbc8853dd1f39ec81caba651
-size 81265
+oid sha256:e1c73dbd4c5af3a7accef1095edf0e500d3ff20c03f9a23b5b21297f683bf317
+size 81304
--- tests/metrics_test.py
+++ tests/metrics_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8557adde865e8bd84b038ab8a43ef7cc950b20a3c90f4a6afff5f6e1603bb065
-size 83851
+oid sha256:83b2eb9fb836329d634389cc99121c1031b91ef26efa65342c69f9eddf3d7e4f
+size 84987
