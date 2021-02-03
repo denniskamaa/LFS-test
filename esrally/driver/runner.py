c1a3b52c90faf47e298705b2b835993b0e1d8845
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2021-02-03 16:27:03 +0100

    Document meta-data returned by operations (#1169)
    With this commit we add a new `meta data` section to each operation in
    the track reference documentation which documents which meta data are
    returned. This information can be used to add assertions for custom
    tracks. We also enhance a couple of existing runners so that they return
    a `dict` structure instead of a tuple because only `dicts` are supported
    for assertions.
    
    Relates #1157

--- esrally/driver/runner.py
+++ esrally/driver/runner.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:807b1a7f512e09cdde3fa016e138e31b7ff1e305004f92cf13c71ec57c517e2e
-size 95794
+oid sha256:f4fc4a5870c78e08fa4ca17b2825d0c5956cc81c602db2da6806094d33fa29fe
+size 90922
--- tests/driver/runner_test.py
+++ tests/driver/runner_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91a87d57a6a3dd59060c5691f9e424ea0bdeafa0ffab190c611e8edfaf4f9fd6
-size 189847
+oid sha256:4035710e803aa65296640e3bedc09410b23c265e1711305dd0d3fcfbb2b21e03
+size 191292
