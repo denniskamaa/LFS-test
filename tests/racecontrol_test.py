81816a3b694ebad4bc78c4d38d044137b8fc774e
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2021-02-04 10:39:33 +0100

    Improve isolation in race control tests (#1172)
    With this commit we rely on pytest fixtures for proper cleanup even when
    a test case fails. Previously, cleanup was only working when a test
    succeeded leading to unwanted side-effects between tests.

--- tests/racecontrol_test.py
+++ tests/racecontrol_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24aa917b95097f9f7ace878a46b3dbbc5309d6141b97b7aa7fb1ac0b1dc9641a
-size 4438
+oid sha256:d14cd6922b42d268e1c6f44227555d5c3d45069cb37ad34773d5d228ff732faf
+size 4177
