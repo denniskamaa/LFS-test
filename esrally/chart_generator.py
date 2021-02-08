1606e8a2fe26d9dc3741142dd9034199783bbb49
Evgenia Badyanova <evgenia.badiyanova@elastic.co>
2021-02-08 10:07:45 -0500

    Modify chart generator to support Kibana 7.x dashboards (#1093)
    This commit changes the chart generator output to ndjson and addresses validation errors for child "query" on kibana 7.x.

--- esrally/chart_generator.py
+++ esrally/chart_generator.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:865d07ac9588043121e470bc7db1721828e184a35ee736a9e1bed30555bf2622
-size 66485
+oid sha256:dac09ed2010b6505a643fc849bc5f8db0643c31abc5e241e40b482dffc64c640
+size 66657
