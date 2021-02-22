2d5b4bd715a761a18a87de5b4d878df5537cf821
Rick Boyd <richard.boyd@elastic.co>
2021-02-22 10:05:29 -0500

    update elasticsearch-py client to 7.10.1 (#1194)

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35d91f0c54d9b0cf2537ddd0d8011c120aa0d18eb9daa03566f00b7d12fd66a9
-size 5947
+oid sha256:e3b4853a44305590be01fcacd6aed197c993b075730bc1328b2bc676dd107af3
+size 5948
