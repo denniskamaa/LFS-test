1e7fdd087a3d798b7bda8ef04e758319a86d8c98
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2021-02-22 07:31:28 +0100

    Use operation name consistently in metrics samples (#1177)
    With this commit we align the value that is used for the `operation`
    property in request metrics. Previously, only the `latency` samples
    contained the correct operation name but `service_time` and
    `processing_time` duplicated the task name instead.

--- esrally/driver/driver.py
+++ esrally/driver/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f29171e281513276a2fe8fcaa5e24e983883ddb5d59e6601f2c12a7488ef43a
-size 87040
+oid sha256:918104147eeceb7b8970ce79a5aded2ece02fb5a61982105373b4a1ac8cd86e1
+size 87050
--- tests/driver/driver_test.py
+++ tests/driver/driver_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f73225721bfeaf87d887f684eb415f8c9e5b5e7ac14dde4f63d7e30fe848014d
-size 72526
+oid sha256:5a9b9b581ee80a8dbc4d608ab9fef9b601c62dcfbd23e627a5b528d8586097b4
+size 72538
