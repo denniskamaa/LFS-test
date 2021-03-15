b6e280879290647179863298bca99ac647225bfd
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2021-03-15 15:14:47 +0100

    Expose an API to handle file offset tables (#1207)
    With this commit we encapsulate handling of file offset tables for data
    files in a dedicated class. This class implements the necessary
    low-level handling only exposing a higher-level API for consumers.
    
    Closes #1204

--- esrally/utils/io.py
+++ esrally/utils/io.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d138ab2e5fd0c47a2ca0486112533a17f065aef635037d09c93151111778c58f
-size 16192
+oid sha256:bbe7fa5492d246335303ccfcb04b00b071f54f12aa0ba45044d3e380a133df93
+size 19824
