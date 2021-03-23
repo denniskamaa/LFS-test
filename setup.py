53092708da795e600aee785f0cda2a6f888a703c
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2021-03-23 11:31:09 +0100

    Upgrade template engine (#1216)
    With this commit we upgrade the template engine Jinja to the latest
    release 2.11.3.

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e3b4853a44305590be01fcacd6aed197c993b075730bc1328b2bc676dd107af3
+oid sha256:273436888271e76c3e41a0eb0b4ebe2a3f35cde2f65cb98313c300ea9cd29850
 size 5948
