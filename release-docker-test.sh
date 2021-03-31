47cb29b3c52e22231a98c8a18c9f617d3c139a25
Evgenia Badyanova <evgenia.badiyanova@elastic.co>
2021-03-31 13:22:56 -0400

    Fix docker release (#1226)

--- release-docker-test.sh
+++ release-docker-test.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9cdb649ede8ba4dd005945d3b4a1359cefeeeae431c88b521917340677f99f03
-size 5563
+oid sha256:b492aa9d9db77314a696b1690a21a438641aa230e0daa7de09967dcf68bc5cda
+size 5573
