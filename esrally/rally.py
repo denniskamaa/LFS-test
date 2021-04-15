fc3ffe7d51f594e3bb4f43c3d97168c6dcd46238
Rick Boyd <richard.boyd@elastic.co>
2021-04-15 09:51:57 -0400

    make --track mandatory (#1239)
    As there is not really a good default workload for Elasticsearch, we previously deprecated running a race without specifying a track.  This commit finalizes the requirement to specify a track when we run a race.

--- esrally/rally.py
+++ esrally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d30385121351bba120bcb4effd81215923afb31b01db0c42293c3ac43c0f02e
-size 45687
+oid sha256:716f2403e92186d82fee4d68da253353c7d112a412eb65bd08e998f0f682295f
+size 45350
