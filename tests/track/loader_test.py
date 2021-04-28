3b3f1e2583019807ba6ca663f4cf4c172596abd0
Rick Boyd <richard.boyd@elastic.co>
2021-04-28 04:09:09 -0400

    inject cfg to DefaultTrackPreparator (#1253)
    In order to allow tracks to register custom track processors, but support also executing the DefaultTrackPreparator with the new API, we needed to pull the cfg out of the init() method

--- tests/track/loader_test.py
+++ tests/track/loader_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef2d06052044bd5767d6b82f3c68a91b5e4d7a22b2f321219d3772164ba98905
-size 154031
+oid sha256:b08504eb760d732b0623efc6c8660b8f011c05346d79e74d02cb7c8bfebeae3c
+size 154028
