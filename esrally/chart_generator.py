c23bfcccd2faacb70d44b69615c74009e42e2239
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2021-06-01 07:11:17 +0200

    Remove segment memory chart (#1275)
    As segment memory statistics are dropped in
    https://issues.apache.org/jira/browse/LUCENE-9387, we drop the related
    chart from the chart generator.

--- esrally/chart_generator.py
+++ esrally/chart_generator.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9bb4d9eff83eb108709defa91ba9a0ae4f463196cb21f8cc47b0130ff667ca1
-size 74888
+oid sha256:4e8e741271aa27228880972dae0536b58ce3b954e913c24722f42ec91f05cb5e
+size 69040
