286524b13d6e24013a697219e4d912ee4c480966
Evgenia Badyanova <evgenia.badiyanova@elastic.co>
2021-06-08 14:17:04 -0400

    Add support for ml_processing_time chart (#1280)

--- esrally/chart_generator.py
+++ esrally/chart_generator.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4e2f1330abcf51b667b724c57357336b5a42e2ddc7f3340f18df96d60c8b54f
-size 69705
+oid sha256:f70cd90acb3dd3594c64fc895d287ed4026ca3583db85f944bf5e6995c2ee731
+size 73979
