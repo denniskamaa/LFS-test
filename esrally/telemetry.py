14dd44ada1c319e8c67ea731b07ce343d42f56e8
Brad Deam <54515790+b-deam@users.noreply.github.com>
2021-06-09 16:42:04 +0930

    Set daemon attribute directly instead of using deprecated setter (#1283)
    Closes #1262

--- esrally/telemetry.py
+++ esrally/telemetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d99a95cda1a2db0fa0daa044d564b9b0a5225d4c2a9a155ad7b4eb0050370196
-size 79733
+oid sha256:b9f8859f58cc4fd67b4a2153b19fc61acd22d828e55476bae33b360a889d24c5
+size 79721
