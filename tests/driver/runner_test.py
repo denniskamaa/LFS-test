414979ec084a61d4a87703103255dee037f19fc8
Quentin Pradet <quentin.pradet@gmail.com>
2021-06-28 10:08:08 +0400

    Replace match-all with match_all

--- tests/driver/runner_test.py
+++ tests/driver/runner_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1da941c4f131a27ed4bc8f953a38e7e1b28cfa1edda0918efe813b72caf904e9
+oid sha256:3a3c295c4e3a4df45285bf242144999cafd07f2d172f1ad4176f7aa2acac5f99
 size 202665
