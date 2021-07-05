9cf50a6d3fd335b4136b881ae16f0df3402b86da
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2021-07-05 14:41:21 +0200

    Drop relative-time-ms metric (#1291)
    With this commit we drop the metric `relative-time-ms` which we have
    introduced for a transition period. This property is superseded by
    `relative-time` which contains the same value.
    
    Relates #1198

--- esrally/metrics.py
+++ esrally/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0671c2cb75361c73283b752e01155e49c5b6517aba1b998faac23f9f49d602b9
-size 85145
+oid sha256:559651bf837d320fc8b9dc99d0629091dfe2e12a906f430f0a962570f88a77ea
+size 84947
--- tests/metrics_test.py
+++ tests/metrics_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ea27ffb35fc482ecade81ce885883f6944bccf6fba2c30cd674323b270ee56d
-size 88792
+oid sha256:05aa380c374c2f607305add35c125849d8452453b8fd176eaa021c2d3762109b
+size 88568
