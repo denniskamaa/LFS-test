1080caa9c0af108108f56e989a35c4e7142e2c95
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2021-08-11 13:53:09 +0200

    Show race id in console (#1311)
    Users can retrieve details about a race from the metrics store or via
    Rally subcommands via the race id. While we log the race id at the
    beginning of a race such an important piece of information should be
    more visible to users. Therefore, we print the race id also at the
    beginning of a race to the console.

--- esrally/racecontrol.py
+++ esrally/racecontrol.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4bbeeefa9c896af0e69ea38ff64fcff1bce31fabca1fcf28a325d190a30d401
-size 17251
+oid sha256:5ca429209e439d7ab6ab5039fd74a4a2b6c035086888a44e1e2c8a4d5f30bbd8
+size 17269
