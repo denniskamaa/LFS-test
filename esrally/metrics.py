81839c8de9578c343963c59b98f3ad32bbd1bbce
Brad Deam <54515790+b-deam@users.noreply.github.com>
2021-08-16 22:12:13 +0930

    Update rally-* index template settings to default to Elasticsearch defaults  (#1312)
    This commit changes the defaults for the number of shards and replicas used in the Elasticsearch datastore first introduced in #1310:
    instead of hardcoding them -- when unset -- to `1p/0r`, we rely on ES defaults[1] and only attempt to override these index settings if they've
    been explicitly specified in the `ini` file.
    
    Additionally, we also remove the hardcoded `refresh_interval: 5s` setting, to allow us to take advantage of Elasticsearch's default refresh behavior ([2], [3]).
    
    \[1\]: https://www.elastic.co/guide/en/elasticsearch/reference/current/index-modules.html#_static_index_settings
    \[2\]: https://www.elastic.co/guide/en/elasticsearch/reference/current/indices-refresh.html#refresh-api-desc
    \[3\]: https://www.elastic.co/guide/en/elasticsearch/reference/current/index-modules.html#dynamic-index-settings

--- esrally/metrics.py
+++ esrally/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca466c9a0760cbc7c900c8e4ae6d4f4b60eee8fe9f3eac31a115f3c72041bd9d
-size 85469
+oid sha256:e89878c2d4c93fdfb99ba7c60c9c5241ca81c76c5ac79af1398342875177bc99
+size 85905
--- tests/metrics_test.py
+++ tests/metrics_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf1119ccd229df8a1520cdb57db71fc1937aa2df7afbecfecf202141de987d45
-size 90410
+oid sha256:cee13250da2b2bbe524126beea71a246b52b25b039460fe99f29fc60886eede7
+size 94565
