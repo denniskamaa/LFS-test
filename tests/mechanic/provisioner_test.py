2a159aa65f08c75c84b4ba379af81f25d1d08024
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2021-08-16 07:20:02 +0200

    Use bridge network in container mode (#1313)
    With this commit we configure a bridge network that is only visible from
    the IP address the Elasticsearch node should bind to.

--- tests/mechanic/provisioner_test.py
+++ tests/mechanic/provisioner_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9cda6b8591c107051eda4aa02dab98917bcf5ebba607a461cdefaf2cd577265d
-size 31933
+oid sha256:36fb0dd62bc0439a5fb1613497eaf227781dbd3e968e5ee5d52359b272d1f264
+size 32247
