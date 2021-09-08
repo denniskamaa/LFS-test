f78893321df5403a8de6e5933408fde123c589c3
Rick Boyd <richard.boyd@elastic.co>
2021-09-08 08:23:01 -0400

    remove coverage dependency (#1321)
    This commit removes the rarely used coverage dependency from Rally, perhaps to be re-introduced at a later date

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52ca0ef74df786c9ef71d94116a83a2c206444887164dc3169034a3e033c7dbc
-size 6044
+oid sha256:4b2be9bc318e68812557606a8aa48d0c595c6728a6c54ded739b697f6694a6aa
+size 6021
