5862055541d8496906a56747475b85ac977907cd
Quentin Pradet <quentin.pradet@elastic.co>
2021-09-28 17:33:43 +0400

    Bump Sphinx version (#1344)

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b2be9bc318e68812557606a8aa48d0c595c6728a6c54ded739b697f6694a6aa
+oid sha256:910e8a6df6f0b98dfe143042b53704bb536db8767e3c7d0136b961331c7d1885
 size 6021
