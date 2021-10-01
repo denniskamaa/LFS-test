a246353db7e4867e0b10b82682db2967c3c31538
Quentin Pradet <quentin.pradet@elastic.co>
2021-10-01 16:05:18 +0400

    Bump version to 2.3.0

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9365d0b0c93587dcc88cc4e411f26b757c11043fdc0eb0b59dfc63f58a1546b1
-size 3690
+oid sha256:5f83f68569bbcac943cb0025e835be119ac39ab6eedf14d309171f3057615ed7
+size 3433
