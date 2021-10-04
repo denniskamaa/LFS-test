e3983a29b3b2ff41eb55cb4da630e40b782f97cc
Rick Boyd <richard.boyd@elastic.co>
2021-10-04 11:03:47 -0400

    remove formatter function from percentage diff evaluation (#1347)
    This commit fixes a small correctness issue with some metrics in the new `Diff %` column.
    
    Before (sample):
    ```
    |                                                        Metric |         Task |    Baseline |   Contender |         Diff |   Unit |     Diff % |
    |--------------------------------------------------------------:|-------------:|------------:|------------:|-------------:|-------:|-----------:|
    |                    Cumulative indexing time of primary shards |              |   0.0355667 |      0.0667 |      0.03113 |    min |      0.00% |
    |             Max cumulative indexing time across primary shard |              |   0.0355667 |      0.0667 |      0.03113 |    min |      0.00% |
    |                       Cumulative merge time of primary shards |              |     0.11245 |      0.0516 |     -0.06085 |    min |     -0.00% |
    |                Max cumulative merge time across primary shard |              |     0.11245 |      0.0516 |     -0.06085 |    min |     -0.00% |
    |                                                    error rate |       search |    0.655664 |           0 |     -0.65566 |      % | -10000.00% |
    ```
    
    After (same sample):
    ```
    
    |                                                        Metric |         Task |    Baseline |   Contender |         Diff |   Unit |   Diff % |
    |--------------------------------------------------------------:|-------------:|------------:|------------:|-------------:|-------:|---------:|
    |                    Cumulative indexing time of primary shards |              |   0.0355667 |      0.0667 |      0.03113 |    min |  +87.54% |
    |             Max cumulative indexing time across primary shard |              |   0.0355667 |      0.0667 |      0.03113 |    min |  +87.54% |
    |                       Cumulative merge time of primary shards |              |     0.11245 |      0.0516 |     -0.06085 |    min |  -54.11% |
    |                Max cumulative merge time across primary shard |              |     0.11245 |      0.0516 |     -0.06085 |    min |  -54.11% |
    |                                                    error rate |       search |    0.655664 |           0 |     -0.65566 |      % | -100.00% |
    ```

--- esrally/reporter.py
+++ esrally/reporter.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75173aea620c386fd4fa089b59d6561dff789c25cf503cfd5fe556ee591da2c1
-size 38337
+oid sha256:eab6db98b983c5b857412405912828c26bfdcd193c6db4fe2efd7db92f62cdae
+size 38326
--- tests/reporter_test.py
+++ tests/reporter_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4bd79d11cd8c434f37fd9c5e76ff5d1a30e63f4a0f40ad9eb65d277f1c99dec
-size 2332
+oid sha256:488a1800f43550aa2e9dc5ded8fcfd9ab531323602a4c9e1f259509bb0b6bc91
+size 3008
