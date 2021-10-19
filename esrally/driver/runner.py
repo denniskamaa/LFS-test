c6bb41979e159d4926c5c8e5172168764d1f8013
Przemko Robakowski <przemko.robakowski@elastic.co>
2021-10-19 09:39:46 +0200

    Add timeout parameter to bulk operation (#1362)
    This change adds timeout parameter that can be used in bulk operation to define server-side timeout (for applying mappings updates, automatic index creation etc)

--- esrally/driver/runner.py
+++ esrally/driver/runner.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ec225f30fc5c9161b6b5ce78746fe5973d5af161889f321d8972a57f20b4c3f
-size 102393
+oid sha256:3bc8d326357eddf0d742578746c57a925d4ab0c6ac92db74aa2b290439b35caa
+size 102574
