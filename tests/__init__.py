af985be38945168d86070e69217aac86a795b128
Quentin Pradet <quentin.pradet@elastic.co>
2021-10-25 17:46:36 +0400

    Prefer AsyncMock to as_future (#1366)
    In Python, calling an async function `f()` can be done in two steps:
    
     1. call the function using regular function call `f()`
     2. `await` the result, possibly later
    
    The driver tests took advantage of this detail to make `MagicMock` work with async functions by using `as_future`. With that trick, mocked functions return an awaitable result, which are then awaited in the tested code.
    
    However, coroutines are best thought as an implementation detail, and it's better to always call an async function `f` using `await f()`, never separating the two steps mentioned above. Thankfully, Python 3.8 introduced `AsyncMock` that allows removing `as_future` by just specifying a return value, which avoids thinking about coroutines, which is what we want.
    
    There's just one wrinkle: while `mock.patch()` replaces the target with an `AsyncMock`, it does not work recursively. So while we would like to rewrite
    
    ```
    es.cluster.health.return_value = as_future({"status": "green", "relocating_shards": 0})
    ```
    
    to
    
    ```
    es.cluster.health.return_value = {"status": "green", "relocating_shards": 0}
    ```
    
    we need to use
    
    ```
    es.cluster.health = mock.AsyncMock(return_value={"status": "green", "relocating_shards": 0})
    ```
    
    which is still an improvement as it avoids the `as_future` code smell.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c5385625b452e3eb6f7e0b194d3d33cb2ee68fd94a36bbcfb4432a62d81f805
-size 1591
+oid sha256:61f276231917560841ba998b7a6643357b78aab705e27c5e856193fb6c12691a
+size 1052
--- tests/driver/driver_test.py
+++ tests/driver/driver_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66f120d226c0f2e2e705d53153e6f9993263a36027bcb77e548815b3c1ebf388
-size 78434
+oid sha256:df3be050ddf0431a55d7f91ec8da9d4e1a6e270b1840e4fa629c2e0544a2eac6
+size 78232
