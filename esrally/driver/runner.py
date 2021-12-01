bece04a9853035d870397f667a22310bf6b7c669
Quentin Pradet <quentin.pradet@elastic.co>
2021-12-02 00:25:17 +0400

    Allow paginated-search in CompositeContext (#1391)

--- esrally/driver/runner.py
+++ esrally/driver/runner.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c9c912a4d45859662170699ddca594d419a170c8922993e0efaf403fe2c5b38
-size 102845
+oid sha256:33fd87c25bb64a145cbc3eb62d364c4a601bb478db1ca3981b83469fc5301ee3
+size 102877
