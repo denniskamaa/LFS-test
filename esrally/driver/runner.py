57b0c83d0cefa8f9b58d2bd935b10fb9408cd508
Rick Boyd <richard.boyd@elastic.co>
2021-12-16 16:10:41 -0500

    Revert "Fix bulk index detailed stats and log failures (#1400)" (#1404)
    This reverts commit febbfe8ad6e673bdaa886461aa59c0d023330fc4.

--- esrally/driver/runner.py
+++ esrally/driver/runner.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9858b128565f99476ef09752cc337acbf51e07766acd17810ce8f6c877ca2efb
-size 102814
+oid sha256:33fd87c25bb64a145cbc3eb62d364c4a601bb478db1ca3981b83469fc5301ee3
+size 102877
