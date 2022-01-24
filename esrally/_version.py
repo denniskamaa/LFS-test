83cf47735f075f83aacdfca1fbcd4637240ef27a
Jason Bryan <jbryan@elastic.co>
2022-01-24 19:10:38 +0000

    Continue in 2.4.0.dev0

--- esrally/_version.py
+++ esrally/_version.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9de67178c9847e385564ceb17ad46292b05d1d6b794f97a12fbd9961d3ed27e1
-size 22
+oid sha256:5ea57a4646efbb77dc8bc3f340671989fce9b5fb91df3a4035baac5a2094efa6
+size 27
