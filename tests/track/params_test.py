02f528cc2f34794018f70668f91339e8fc3cb12c
Quentin Pradet <quentin.pradet@elastic.co>
2022-02-01 11:31:53 +0400

    Switch to native pytest in tests/track (#1423)

--- tests/track/params_test.py
+++ tests/track/params_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4259d555a97550c372d73597b3aaf53e64eed98143501578ef47feb0a3cb1c59
-size 104438
+oid sha256:9066bb565f2de0b90331cc0d800c8c4b9d18a362f2d080bf09aa00c78e924af0
+size 99599
--- tests/track/track_test.py
+++ tests/track/track_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e35bb00703359180c14c8d133f5cdcd4223729a6c25c2ff8525e849b971fcec2
-size 17442
+oid sha256:f961b93148a581ce95c171daebb052a1cb8a8f99955ddb451ec761cfbef8977d
+size 16728
