aabdcf7e1108b0de16dada39facae16b92dc8088
Nik Everett <nik9000@gmail.com>
2022-03-09 09:11:36 -0500

    Run disk usage telemetry one index at a time (#1453)
    Elasticsearch can only queue 16 shards worth of disk usage analysis at a
    time. If the track creates more than that many shard the disk usage
    telemetry would get shard failures back. This helps with that by running
    the indices one at a time.

--- esrally/telemetry.py
+++ esrally/telemetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b2f2f21d981216626424b28ac545482ea7fa4ff3ecd626d9ed42e51b6f885fe
-size 101647
+oid sha256:0d7cc1f0f6c0dfc5425a3bab45498c353d92333ed6e77e2338eb30b1b8abed4c
+size 102309
