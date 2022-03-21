8ef177a3fa14bb7ad55dba4d637f96e0e3b35967
Rick Boyd <richard.boyd@elastic.co>
2022-03-21 13:52:11 -0400

    Add 'root' attribute to Track class (#1461)
    This change adds a root attribute to the Track class, in order to provide an absolute path to original track files to any consumers, such as custom parameter sources.

--- esrally/track/loader.py
+++ esrally/track/loader.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d9d2f11f2d05424fe4d513e4840f435c892b685c1fe6f0fa46e809a98332cfc
-size 78735
+oid sha256:dc6556d3ba8ea466c56c6345a858bd0dc0f5a08ffffb5cd658656fe9e624947a
+size 78923
