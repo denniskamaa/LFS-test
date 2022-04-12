3b104b6cef76328fca0d64f5fb21cce4201b2bb6
Jason Bryan <jbryan@elastic.co>
2022-04-12 16:52:22 -0400

    Add raw-request op type for latency charts (#1475)

--- esrally/chart_generator.py
+++ esrally/chart_generator.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13a7fde5f1f4312d114c191703e55d773c3616b459891aec9205b07c55fc1d4d
-size 76882
+oid sha256:3b30019c0af3013e05461acc2a9d679c87299f20f1f143a1534b9fffbcf831ed
+size 76897
