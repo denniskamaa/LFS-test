4a9c430e6c2b9116dc7906ba956ce6cbdac88670
Domenico Andreoli <domenico.andreoli@elastic.co>
2022-05-17 07:17:11 +0200

    Not every composable template has a top-level `template` (#1489)
    * Ensure that filter template does exist
    
    * Not every composable template has a top-level `template`
    
    * Drop unused variables
    
    * Fix formatting

--- tests/track/params_test.py
+++ tests/track/params_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ae0c22afe1b134a72bca1fedbbfda18e88ffabe29cd42c23e13b8405813ce14
-size 99599
+oid sha256:8103539c8bfdc2780fb0a1a1466d69f868a5aa994160c614c2a7634896083a63
+size 101877
