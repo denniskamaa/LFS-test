3aca54402f7b38b11385c4bf13e64f79cea5bd13
Nhat Nguyen <nhat.nguyen@elastic.co>
2022-06-15 14:46:03 -0400

    Relax target-hosts check for multi clusters (#1518)
    This change relaxes the check of the cluster keys of the client_options 
    and target_hosts parameters. These parameters are dictionaries, and
    their keys can be in any order.

--- esrally/rally.py
+++ esrally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d7417fcb4dab2ff46f3d219166251e8f76be2d7143dc51321381c9f1bb9afc7
-size 48007
+oid sha256:923b050ff4faf311b0606f45f4de08c7f67ab4b394d4ae588b952afbf3703cc6
+size 48005
--- it/distribution_test.py
+++ it/distribution_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37750b8e017db23dbff38ff1769decad597e8234aa8d0085fcf3f75ce5f1a6eb
-size 5546
+oid sha256:a53e7796ecb945eed19b6dc6da9d88ae9f2500b809f76ccd2980160438a0fdd8
+size 6378
