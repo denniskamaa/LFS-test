a27b7d5956ac557d65cadce3df90b003bb43fd6b
Quentin Pradet <quentin.pradet@elastic.co>
2022-06-15 11:33:46 +0400

    Instantiate SubClient only with kwargs (#1505)

--- tests/telemetry_test.py
+++ tests/telemetry_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30847b29b0d31679ca279d98f9d708c6ee02e311b9a91da8c33ac1a2dc4b0605
-size 201882
+oid sha256:b6d648d9a27aeb3242dc5ba3a989eb5447c44b193706682063368e3288adad45
+size 201927
