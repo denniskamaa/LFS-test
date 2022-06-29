52bb4ec7907e84fb049c5fd52468a142e3e65aac
Quentin Pradet <quentin.pradet@elastic.co>
2022-06-29 17:52:20 +0400

    docs/conf.py: rename master_doc to root_doc (#1525)

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:857da056d3c78bce14281ed07ee2f449e37447aeefda360cff597ecde17b6306
-size 6231
+oid sha256:2f49697f2e0f4592511b6da397922d0b832680f4eb266d76800f58d72da717d6
+size 6223
