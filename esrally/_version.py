ffffe546b2aba1fc94bfb0ed4811d5b1e8694317
Quentin Pradet <quentin.pradet@gmail.com>
2022-07-04 13:47:25 +0000

    Bump version to 2.6.0

--- esrally/_version.py
+++ esrally/_version.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f930bde46032534b5d1512d6e25ebb369ab2ef5d7200b7f839e32ae4a6462ed
-size 27
+oid sha256:38489beb77b4c8f106961117cab584d4ec119e5791d1c1c8eca497ee864440bb
+size 22
