435fbd39dda7b95701beccd15349db35fe5e5c6f
Mike Baamonde <michaelbaamonde@users.noreply.github.com>
2022-07-08 08:54:14 -0400

    Correctly log how long it took to close transports. (#1539)

--- esrally/driver/driver.py
+++ esrally/driver/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fcc90b48a583a5003970551889f7086293e5e3d82b8eb77819a580e0fc4ae6a4
+oid sha256:b907f820acb3ffc4ad706ce90642746b933eaa7582a075b8e41a00d990dc702d
 size 106068
