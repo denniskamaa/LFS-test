0240ea373476cddb31acf1eb0ebc5ddaf5cd0964
Quentin Pradet <quentin.pradet@elastic.co>
2022-08-09 11:30:30 +0200

    Move most packaging metadata to pyproject.toml (#1548)
    In anticipation of move to flit.

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a059ad31e04e0f8af1b2d5fb816a75dc5f935d36bd72b8739c3f17254e098d6
-size 4981
+oid sha256:fc75726afd0b5fcafc98a84f5078e5ae395eb32210ee7cd7aacc928d0846cb01
+size 1239
