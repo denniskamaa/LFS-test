a8e1f9e6ea7c175d985ae799498c66b180c9fbf5
Quentin Pradet <quentin.pradet@elastic.co>
2022-09-12 11:18:10 +0400

    Stop trying to test Elasticsearch 6.8.0 on ARM (#1571)
    * Stop trying to test 6.8.0 on ARM
    
    This will allow developers with Apple Silicon hardware to still run
    integration tests.
    
    * Bump metrics store version for Apple Silicon hardware

--- it/__init__.py
+++ it/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c014ef18488e39f110b8c0a06a1ee5ddc83d4e711ae9aaaf99f139c5aae513f3
-size 8618
+oid sha256:c44a1d29acc5f09ce67d2d9205635415418b3db13c23df18216e4412f6274495
+size 8789
