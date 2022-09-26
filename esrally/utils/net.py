79a06676493b6e2521f3453f5ac4c6d1e52936bc
Quentin Pradet <quentin.pradet@elastic.co>
2022-09-26 09:20:19 +0400

    Use narrower exception to avoid pylint ignore (#1579)
    getheader() cannot throw an exception and now always returns a string so
    the only possible exception is ValueError.

--- esrally/utils/net.py
+++ esrally/utils/net.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a120f5ae43114357d481d2a55585d2fbc74a47f1b7c0adecce7d68ffac8ebf8c
-size 12473
+oid sha256:1f4e0c198d92ea21c48d676bed8066cc05aabcf004c4020e0201fd5693047b8b
+size 12434
