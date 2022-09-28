e26fdffc5a1fbadfd708ff44ebbe77e48e3f5f91
Rick Boyd <richard.boyd@elastic.co>
2022-09-28 11:02:06 -0400

    Revert "Add support for assertions on raw requests (#1584)" (#1590)
    This reverts commit ddad4b4e3740cac3a2127c3de3668ce734b54908.

--- tests/driver/runner_test.py
+++ tests/driver/runner_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a157818f953aeb5ccbfc2842e1b65a97d8f5a717c75a1fa89108500d68d12b3
-size 246649
+oid sha256:b9c3b1eb8b966c2bf865a51142b9b5013266c7e7726d23017c1dbeb9866f7cca
+size 244521
