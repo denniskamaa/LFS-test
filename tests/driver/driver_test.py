f38d397ab025adb494d1e1a1bf73916088fac4a0
Quentin Pradet <quentin.pradet@elastic.co>
2022-10-03 11:37:10 +0400

    Fix ijson deprecation warning (#1589)
    Like the json module from the standard library, ijson expect bytes.
    While the signature of `parse()` is type hinted correctly, types are not
    enforced and many tests were passing a StringIO instance incorrectly.
    This produced deprecation warnings locally, but not on CI for unknown
    reasons.
    
    Co-authored-by: Brad Deam <bradley.deam@elastic.co>

--- tests/driver/driver_test.py
+++ tests/driver/driver_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe553c76c2edf90a570a2d6b6a59af886d4d03f31097bd5cc58f9c5c0dc59d6d
+oid sha256:4414c3268be202ccba92d49384f81d63afd1f222904069d5fd964d9c085ba2fa
 size 78504
--- tests/driver/runner_test.py
+++ tests/driver/runner_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9c3b1eb8b966c2bf865a51142b9b5013266c7e7726d23017c1dbeb9866f7cca
-size 244521
+oid sha256:eae0f34e60161c40d1bfe451fdbe8a98bef1df30353cb4a1310672356d470d59
+size 244831
