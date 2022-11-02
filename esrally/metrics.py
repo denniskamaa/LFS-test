e3ec3e24963e1c5156edb0f1e868fbced316cea6
Kazuma Arimura <k.arimura96@gmail.com>
2022-11-02 21:09:41 +0900

    Add `--user-tags` as alias for `--user-tag` (#1599)
    * add --user-tags as alias for --user-tag
    
    * update docs
    
    * update docs
    
    * Clarify user-tag documentation
    
    Co-authored-by: Quentin Pradet <quentin.pradet@elastic.co>

--- esrally/metrics.py
+++ esrally/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d365ff2856dd99412eb4d2d53d0b890bbbbbda35465dae8ad95a989898e2ea7f
-size 88203
+oid sha256:09cc469a146ca27c4942ded49198b70806f0fb013cf998de6bcb45001006e09b
+size 88204
