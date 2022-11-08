ab8bbf836533541855952206c7c62607a8a7e6ed
Rick Boyd <richard.boyd@elastic.co>
2022-11-08 13:27:17 -0500

    Print errors when --quiet is used (#1615)

--- esrally/utils/console.py
+++ esrally/utils/console.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fffb668edf509c6fff465163fe7b220f4ab7d9a67eb8d1ce6aa69b603cd7760f
-size 7476
+oid sha256:e1da49cb538e825ecaa09537d05ab180d45c8308685142fde592582de244a932
+size 7475
---
+++ it/error_test.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8998b327c732aa874cebd0334e05140b670ccfb7cf54895e8b0d73e08780f025
+size 1089
