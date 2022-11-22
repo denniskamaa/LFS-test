50d491294142a716fb19de1520212f2c789a06f3
Brad Deam <54515790+b-deam@users.noreply.github.com>
2022-11-23 09:23:58 +1030

    Output last 20 lines of docker logs to stdout on failure (#1629)
    With this commit we print the last 20 lines when a container created by
    DockerBuilder fails.
    
    This brings the behaviour in line with the existing native Builder.

--- esrally/mechanic/supplier.py
+++ esrally/mechanic/supplier.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d4676cb5dbcc190aa783f778d6bbe3d6ed8e58207a41c979190a7f25d236a84
-size 43859
+oid sha256:9a0fd711d2ef2536d209dc789a78404aa826aaa380af8efbc1e0268398b35b56
+size 44485
--- tests/mechanic/supplier_test.py
+++ tests/mechanic/supplier_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:29be4f4b9e10eefa5c7d44ab8e88325cd91ac6c89e1ecb92a2999b2795a0d723
-size 55079
+oid sha256:59abdb52f4e8eef734a580031c9a9cda23d6cf3097dad72f5d3e2be1c03555ed
+size 55298
