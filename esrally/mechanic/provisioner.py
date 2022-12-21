6f790f8c0011afbdf6b61870ee157e9c768f2905
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2022-12-21 09:06:06 +0100

    Pass plugin-params also for modules (#1640)
    In #1624 we've ensured that plugins that have turned into modules are
    now skipped from the mandatory plugin check. However, we've also not
    passed any plugin parameters to the provisioner and this made post
    installation hooks fail, if they expected parameters to be passed on the
    command line via `--plugin-params` (e.g. `repository-gcs`). With this
    commit we make sure that `--plugin-params` is still honored even in
    these cases.
    
    Relates #1624

--- esrally/mechanic/provisioner.py
+++ esrally/mechanic/provisioner.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85c74852ae25b23ab6eecd65e89359295c144205a52ccd8d3c4e909eb7c51f6d
-size 22400
+oid sha256:a4c57fd3e35a594695c98041d261a7dd443046c67789a20250c84fada6bb1cca
+size 22396
