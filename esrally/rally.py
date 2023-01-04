e4250a6861f96c26130a9f657ca68266f253b5e6
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2023-01-04 17:11:23 +0200

    Fix to-date option for list subcommand (#1645)
    PR#1597 introduced new additional filters for list races including
    `--to-date`, but it doesn't work as expected (gets ignored) as the name
    of the key is different to what it should really be (and what we are
    testing for).
    This commit fixes the bug making `--to-date` applicable.
    
    It might make sense to refactor those hardcoded keys in a future PR, so
    that they are referenced from the same location in `rally.py` and
    `metrics.py`.

--- esrally/rally.py
+++ esrally/rally.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee0bace12a03eced5b48633ae50893c1001f5cf00d5616a97ca19e3451455e1d
-size 54417
+oid sha256:cea32f54507c8b25ee6f6424f13ec7052ed6580542dfb8f10e3ffe2d3fedd3a1
+size 54411
