ecf5cb4303adee186c5603915bc4f3c31eb8569e
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2023-01-04 16:22:06 +0100

    Skip response parsing for raw requests (#1643)
    With this commit we skip response parsing for raw requests. As users
    might leverage the `raw-request` operation type for any kind of request,
    it is possible that it produces arbitrarily large responses, which might
    introduce client-side bottlenecks. As we don't inspect the response
    body, we might as well just use the raw response internally and avoid an
    accidental bottleneck due to response parsing.

--- esrally/driver/runner.py
+++ esrally/driver/runner.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:057907a7f078223914b37d7b61b5a9066b0a3a956e0e95fd8dce2411add6de77
-size 120109
+oid sha256:6b181d7e5f92e06844525ee3efc32515a23181c0c4a383d8f61581ad8c1db3e0
+size 120230
