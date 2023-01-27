8092620c982f0159cd8c57bfe8c7adef5e252296
Quentin Pradet <quentin.pradet@elastic.co>
2023-01-27 08:17:08 +0100

    Cleanup params_test.py file (#1657)

--- tests/track/params_test.py
+++ tests/track/params_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23ed8a607f70c62c0573e4f69979ace0e0dfdcd131d323faaea9dab53ba4cc9d
-size 103705
+oid sha256:9d34c1579811d27d3f2dd54a893a98870284f41d62570aec63c71e2de43c4a2b
+size 104766
