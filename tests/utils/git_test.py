c31f21c44f76c91a4b6340c965cf7c128da54baa
Quentin Pradet <quentin.pradet@elastic.co>
2023-02-15 10:45:32 +0400

    Fix git tests with sparse Rally git checkouts (#1667)
    * The previous git tests relied on the Rally git checkout, but:
    
     * GitHub Actions does not checkout the whole history to make the git
       clone faster.
     * Recreating a dummy repo is now 10x faster , with `pytest
       tests/utils/git_test.py` now taking 0.40s instead of 5.98s on my
       laptop.
    
    I've also started using GitPython in tests which makes the
    setup/teardown easier to write and read in my opinion.
    
    Co-authored-by: Brad Deam <bradley.deam@elastic.co>

--- tests/utils/git_test.py
+++ tests/utils/git_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e91c52a994029b8751d02872b68bd357dadb304b864bd45607ad6ea9dc9f7f6
-size 10257
+oid sha256:b7eb30b966a8e6be7b9ad5ae320de8a4c8c1fe2797f6277e3b882f7d5ec8d481
+size 9146
