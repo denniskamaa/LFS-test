bfef2dc48f162d6ea35d263f2efc1c5eb27b00ee
Quentin Pradet <quentin.pradet@elastic.co>
2023-02-22 17:21:00 +0400

    Allow reading metrics store password from environment (#1674)

--- esrally/metrics.py
+++ esrally/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab8237a422ce615cbb1ba19480d8ed8129be036dd32ebdf18745f45808c57cff
-size 97209
+oid sha256:4a50380b567d716f4b15e4cf75ff5fcf6c861b4f39ba31139bcbc8480467a81e
+size 97601
