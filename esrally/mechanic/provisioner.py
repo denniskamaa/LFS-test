a0afa84cff09ef0ffe574c916b432457045241ba
Quentin Pradet <quentin.pradet@elastic.co>
2023-03-02 15:30:08 +0400

    Revert "Add a variable in the provisioner for the seed node count (#1647)" (#1680)
    This reverts commit ebd4d40c0294bd17c5b459c942a24792c8b0763b.

--- esrally/mechanic/provisioner.py
+++ esrally/mechanic/provisioner.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6671239d13299525b6eeb8b8946b2ea7f4d4d23d5ddebecb3457ca16a734bfb
-size 22438
+oid sha256:07b18a2c93080d0399c77a55f8dfbb403882bf75cca6de8527412301882744d9
+size 22380
--- tests/mechanic/provisioner_test.py
+++ tests/mechanic/provisioner_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a1b55b6af12f176cd3df36f8d5f58fcc9604a10607d8abab1617a42bad91af2
-size 30291
+oid sha256:b69b52ba4e038d74282455c5724ca0734d61f681181c200e5359da1105ddf457
+size 30143
