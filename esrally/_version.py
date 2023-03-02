100296830286fbfb12b03db2402b0f2d49712934
Quentin Pradet <quentin.pradet@gmail.com>
2023-03-02 12:42:52 +0000

    Continue in 2.8.0.dev0

--- esrally/_version.py
+++ esrally/_version.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c91a521fa9816f804981b1654fbaedf0c4a30945b5ed8731d11ce22dffa540b0
-size 22
+oid sha256:d67a60fd812988a4065b098cba82e3f93899fd522ed2d06cb4131e98e90870f8
+size 27
