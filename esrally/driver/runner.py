0ee87ce99f78be4357cb552161852b32af142338
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2023-03-29 11:24:54 +0200

    Use correct params for create component template (#1690)
    The Elasticsearch Python client does not accept a request body but
    rather requires parts of the body to be passed for the create component
    template API call. With this commit we adapt Rally to these changes.

--- esrally/driver/runner.py
+++ esrally/driver/runner.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c95e06eb420fc298bc2954ec4d42b66e1a9f01746960590a180dcc630d7fee0
-size 118732
+oid sha256:3dfc8fb8647bebc5a5a44462286ea839f265894c80bdeedf842907df02a11a5b
+size 118740
--- tests/driver/runner_test.py
+++ tests/driver/runner_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:36a07c2d93a787fdf6cd7e25331a023a8090a4c84ae6da3060508d956324c772
-size 248324
+oid sha256:696403d84fb8a281d78a7928b108a3ec22b4f752d2354e7cff7bcd21d3d78f62
+size 248304
