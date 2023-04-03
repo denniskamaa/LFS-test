7a055982a650f5f59f5858f971bc6d6c9a1f746d
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2023-04-03 08:38:19 +0200

    Update progress after GCS download finished (#1698)
    With this commit we ensure that the progress indicator is updated once
    again after a download from a GCS bucket has finished.

--- esrally/utils/net.py
+++ esrally/utils/net.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7d160d7e4a2b3a27192af7776454d827bb0f91caa26f1387c97b8279731b7af
-size 12389
+oid sha256:b3d773ad41b4c176fed7447d579027a812dbf25507e51e97aab765a0a0d28b3d
+size 12648
