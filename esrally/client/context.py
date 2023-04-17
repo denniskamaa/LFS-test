0e0665ed4b42b00913eadd370b5a96d0303ecd66
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2023-04-17 17:07:03 +0200

    Propagate request start/end lazily (#1687)
    With this commit we only propagate request start and end time to the
    parent context if there actually is one. Previously we unconditionally
    propagated the request context which could lead to misleading errors
    when there was an open request context but no request was issued. This
    can happen when a mandatory parameter is missing and the runner was
    throwing exception. That exception would get masked by a `KeyError`
    because no request start was present yet and thus obfuscate the root
    cause.

--- esrally/client/context.py
+++ esrally/client/context.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea1c5b41c353fe9dd6b1e353c40b131d28375aa38966bb7fa6585797b226f788
-size 3562
+oid sha256:a88ca54c2a37dd26785f8800797b83f1d7b1fdd16364f72b20f75e752271294f
+size 3502
--- tests/client/factory_test.py
+++ tests/client/factory_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b411d5462584407d777bf37bfff41a27df156739ce88d6ca078f07d064b3123
-size 27394
+oid sha256:f778e83fa8083704f620a50cdc04ae0c92a43d5fd35c6b0b0c062cecf8fd5abd
+size 28005
