abd7e692613fb788457af3a9706777708ddbe5d0
Quentin Pradet <quentin.pradet@elastic.co>
2023-06-08 16:58:56 +0200

    Allow configuring logging to use local time (#1727)
    Co-authored-by: vaibhavsolanki1193 <33081360+vaibhavsolanki1193@users.noreply.github.com>

--- esrally/log.py
+++ esrally/log.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb1deece4ab225735af62debc13bd72b0c197898592b03e58823e448b8f9af63
-size 6525
+oid sha256:0d16fc1248f52e62b6320b697fb90063f2e3b8b7a96bdbc0cc1daa73de527dfc
+size 6626
--- tests/log_test.py
+++ tests/log_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d918ceb49afe3308790ff1e937756826beec7f256ab237c0bdcda3923b1f99a
-size 2296
+oid sha256:1a6c4b479c5bca75351816f227bb0f1f512e34e95baccb5b1ae1c92e67805a03
+size 2775
