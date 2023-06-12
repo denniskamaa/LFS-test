b74b6479ed825e12b656e43c059f52114a80fc5e
Grzegorz Banasiak <grzegorz.banasiak@elastic.co>
2023-06-12 12:58:53 +0200

    Fix license URLs in create notice script (#1732)

--- create-notice.sh
+++ create-notice.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca225a14de923174e3e238b17e3c36e5c83f147d12f04745eb8bf9459a474306
-size 4609
+oid sha256:cc37838881ae54f83fbec0f4564113c91e10b5df14fa9a55b3ca7f95243dc521
+size 4579
