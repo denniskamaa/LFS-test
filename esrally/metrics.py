2127503c9ff72b63fef44e0deb39f5f8b2a91849
Grzegorz Banasiak <grzegorz.banasiak@elastic.co>
2023-07-13 10:08:10 +0200

    Fix rally-annotations index creation (#1747)
    The `rally-annotations` index settings body template lacks `index`
    attribute. Also, upgraded Python Elasticsearch client is more strict 
    when evaluating `body` parameter in index `create` method. This
    breaks `rally-annotations` index creation in Elasticsearch metric
    store.
    
    This commit fixes `rally-annotations` index creation adopting the
    approach from other Rally indices which all use index templates.

--- esrally/metrics.py
+++ esrally/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40295a7ad26862201838783b317436edc598b8320e2950d4c9ab990a1c98f322
-size 99422
+oid sha256:9c3ad76edbc221706216e9ab391d9ce8eb237ed05d1e4ca024f57bfa13b95489
+size 99683
