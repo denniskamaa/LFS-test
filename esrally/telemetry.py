7ac460e09336b2815ff47d58246052fe28bf4025
Brad Deam <54515790+b-deam@users.noreply.github.com>
2023-08-09 06:40:56 +0930

    Add blob-store-stats telemetry device and tests (#1755)
    This commit adds a new blob-store-stats telemetry device that based on 
    the sample rate collects per-cluster, and per-node blob store statistics 
    as they are reported by the respective API.

--- esrally/telemetry.py
+++ esrally/telemetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d0eadf2fd97f251088eb8decc8ff9e8b4fca820874db7ffb65350266eca2b30
-size 104139
+oid sha256:ed2abc22a0af8428a70f2140edd388c8ab87ab0711be6532ff1c39b79f39e96f
+size 109579
--- tests/telemetry_test.py
+++ tests/telemetry_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:470456407147ed9fcca2d91a39f4f6bca43fa15b2b7d05a0f3efbc04b20fcfd5
-size 220357
+oid sha256:5778104d4444db1189556d12e081b5a0f3b3d55fe6aabdee7905ad232fae14dd
+size 226363
