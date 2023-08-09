7de2bc5408669dd96b5a1eb1c5792718e7338e67
Quentin Pradet <quentin.pradet@elastic.co>
2023-08-09 14:11:06 +0400

    Ensure serverless.mode/operator are always set (#1759)
    And invert one condition to avoid using `not`.

--- esrally/racecontrol.py
+++ esrally/racecontrol.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a74bf4112bbffa3f8c7a3f35951bbe3ade5a923727b3ef33a9c6b23d7dfa1c7
-size 18122
+oid sha256:452d09f0b42ad06a288d3900c597f27bdccd38fe88e17eddbf117d72e0331aed
+size 18280
