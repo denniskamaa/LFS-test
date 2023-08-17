dd72fd9bb2d310fc0d7e0d1356d8bed8ae37d0ef
Quentin Pradet <quentin.pradet@elastic.co>
2023-08-17 06:07:08 +0400

    Sort AUTHORS file using en_US.UTF-8 (#1762)

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7052aee4896c0f24a03ce32d4a7d0371ba2c402e4f4d6fd372e1995550c6012c
-size 2926
+oid sha256:12b66d2bc23d6c6da625e23169cf97b385555e06f951dfc753d0e83dbf76be52
+size 2945
