d2af6d1ca32f70e1db0eb839afa64257d729ce23
Brad Deam <54515790+b-deam@users.noreply.github.com>
2023-08-17 09:54:35 +0930

    Update blob store stats telemetry device (#1761)
    * Split out 'flatten_stats_fields' helper from 'NodeStatsRecorder'
    
    * Update 'blob-store-stats' telemetry device to support new response format
    
    * Fix docs

--- esrally/telemetry.py
+++ esrally/telemetry.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed2abc22a0af8428a70f2140edd388c8ab87ab0711be6532ff1c39b79f39e96f
-size 109579
+oid sha256:073792266556935e566083cf613622177ebeaff210501c7d1cbd85c3da0685b4
+size 109610
--- tests/telemetry_test.py
+++ tests/telemetry_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5778104d4444db1189556d12e081b5a0f3b3d55fe6aabdee7905ad232fae14dd
-size 226363
+oid sha256:6c1f25b098d9247da21c354d8be308b8f13b3f170f8c4d6fe2c89336a66f31ac
+size 229690
