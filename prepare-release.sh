3f4874088242867d3cb8b115d0f4934521c38496
Quentin Pradet <quentin.pradet@elastic.co>
2023-08-24 09:29:02 +0400

    Upload Rally to PyPI using Trusted Publishers (#1765)

---
+++ prepare-release.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8ec10935813bbead2f1990a9a71c4c367c9a7253fa660faa851d8ca945a343fc
+size 2169
--- release.sh
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:12b66d2bc23d6c6da625e23169cf97b385555e06f951dfc753d0e83dbf76be52
-size 2945
