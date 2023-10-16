4ec114622ca0f3d80cd50646d3d4c54dbb0899ba
Grzegorz Banasiak <grzegorz.banasiak@elastic.co>
2023-10-16 10:13:21 +0200

    Add serverless-aware runners (#1789)
    This change passes configuration context to `Query`, `DeleteIndex` and
    `DeleteComposableTemplate` runners to make them serverless-aware and
    change their behaviour if Rally targets serverless cluster.
    
    Details:
    * `Query`, `DeleteIndex` and `DeleteComposableTemplate` stop getting and
      setting cluster settings through `set_destructive_requires_name()` as
      cluster settings are not available to serverless users.
    * The `DeleteComposableTemplate` runner starts to ignore
      `delete_matching_indices` template option. Wildcard deletes are not
      available to serverless users.
    * Request cache becomes turned on by default for serverless non-operator
      use in the `Query` runner (`search`, `paginated-search`,
      `composite-agg` and `scroll-search` operations). In non-serverless use
      we typically rely on `"index.requests.cache.enable": false` index
      setting for this purpose but it is not available to serverless users.

--- tests/driver/runner_test.py
+++ tests/driver/runner_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fae0a35d7f4d011214a825098c32a72110adb8cf3c8081fea51ffae1819ce6a7
-size 255790
+oid sha256:2ed5350797a98f5ec5fc9e16a18c54cceb1c1f91fb0ac7a761083312407dc38e
+size 267076
