10c9e5f98a79ac067a49f035500c3b4f08e6872b
Grzegorz Banasiak <grzegorz.banasiak@elastic.co>
2023-10-16 09:07:30 +0200

    Retrieve cluster info in serverless public mode (#1790)

--- esrally/driver/driver.py
+++ esrally/driver/driver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b7c3ae2105539e80901fc2f0acec40dfc9ed72e10c5b72842c9f92d51ffb3bf
-size 110956
+oid sha256:7879f515a02aea7460ff6b345bd7d150b53615b99c6634023c0272c43e145815
+size 110984
