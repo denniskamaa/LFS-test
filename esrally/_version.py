ffadcf799e7ef202af0ce74b2379db41ad054367
Grzegorz Banasiak <grzegorz.banasiak@elastic.co>
2023-11-06 16:02:52 +0100

    Continue in 2.9.1.dev0

--- esrally/_version.py
+++ esrally/_version.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ebd7274c62a968b5ec6dc022e4945b7a8e37c48789c953242283c64df03be0e6
-size 22
+oid sha256:91dc52ca6647daca483b1105ed619b406f637aa811c29489271a570c60f23246
+size 27
