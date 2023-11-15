f905a2fcd47ae8e65c253a2fc797fa42da99e123
Brad Deam <54515790+b-deam@users.noreply.github.com>
2023-11-16 10:24:05 +1030

    Raise error if 'policy' field missing in create-ilm operation body  (#1801)

--- esrally/driver/runner.py
+++ esrally/driver/runner.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2be671dda0b46eb65d59d94ff05223bf796ad88bd8f52870870619f63b7bb979
-size 123796
+oid sha256:2775183f13b80dec4e2ff6c0558e87c9953b292c596b7cba3ba7ede27ae0f7ed
+size 124348
