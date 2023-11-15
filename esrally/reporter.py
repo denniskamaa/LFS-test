f2cc2e6d03b4f67e1e5fcfd86d952e659a1cdb13
Gareth Ellis <gareth.ellis@elastic.co>
2023-11-15 10:03:59 +0100

    Skip metrics that are missing in either contender or baseline (#1803)
    * Skip metrics that are missing in either contender or baseline
    
    * Remove warning

--- esrally/reporter.py
+++ esrally/reporter.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30d20ca1973996f57ef7bf3296607d53ac8a2a5e59d71680ca01969c59d5caf4
-size 44396
+oid sha256:8d6941cb45c1967eeeba56270650a946d4bad4efd0f06c67ba946800672ac06f
+size 44542
